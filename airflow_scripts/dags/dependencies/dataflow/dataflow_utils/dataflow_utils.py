885ce9b88e2b1c624912171e28e6e29963aa84b1
James O'Toole <jtotoole@gmail.com>
2019-11-27 14:43:01 -0500

    refactor project structure + registered businesses tasks (#2)
    * start on airflow utils
    
    * struggling back from git clean, god help me
    
    * refactor dataflow dependencies
    
    * clean up import format
    
    * update gitignore

---
+++ airflow_scripts/dags/dependencies/dataflow/dataflow_utils/dataflow_utils.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+
+import os
+import scourgify
+from scourgify import normalize_address_record, exceptions
+
+from avro import schema
+from google.cloud import bigquery, storage
+
+
+bq_client = bigquery.Client()
+storage_client = storage.Client()
+
+
+# monkey patch for avro schema hashing bug: https://issues.apache.org/jira/browse/AVRO-1737
+def hash_func(self):
+    return hash(str(self))
+
+schema.RecordSchema.__hash__ = hash_func
+
+
+def download_schema(bucket_name, source_blob_name, destination_file_name):
+    """Downloads avro schema from Cloud Storage"""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+
+    blob.download_to_filename(destination_file_name)
+
+
+def clean_csv_string(string):
+    return string.strip('"')
+
+
+def clean_csv_int(integer):
+    return int(integer.strip('"'))
+
+
+def normalize_address(address):
+    normalized_dict = normalize_address_record(address)
+    try:
+        for k, v in normalized_dict.items():
+            if v:
+                normalized_string += (str(v) + ' ')
+        normalized_string = normalized_string.strip()
+        return normalized_string
+    except scourgify.exceptions.UnParseableAddressError:
+        return ''
---
+++ airflow_scripts/dags/dependencies/dataflow/registered_businesses_dataflow.py
@@ -0,0 +1,110 @@
+from __future__ import absolute_import
+
+import argparse
+import json
+import logging
+import os
+
+import apache_beam as beam
+import avro
+import fastavro
+
+from apache_beam.io import ReadFromText
+from apache_beam.io.avroio import WriteToAvro
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from avro import schema
+
+from datetime import datetime
+from google.cloud import storage
+from dataflow_utils import dataflow_utils
+from dataflow_utils.dataflow_utils import hash_func, download_schema, clean_csv_int, clean_csv_string, normalize_address_record
+
+
+class ConvertToDicts(beam.DoFn):
+    def process(self, datum):
+
+        acct_no, name, trade_name, desc_of_business, business_type, address_type, city, state, zip, data_created, \
+        business_start_date_in_pgh, address1, address2 = datum.split(',')
+
+        address_full = clean_csv_string(address1) + ' ' + clean_csv_string(address2)
+
+        return [{
+            'acct_no': clean_csv_int(acct_no),
+            'name': clean_csv_string(name),
+            'trade_name': clean_csv_string(trade_name),
+            'desc_of_business': clean_csv_string(desc_of_business),
+            'business_type': clean_csv_string(business_type),
+            'address_type': clean_csv_string(address_type),
+            'city': clean_csv_string(city),
+            'state': clean_csv_string(state),
+            'zip': clean_csv_int(zip),
+            'date_created': clean_csv_string(data_created),
+            'business_start_date_in_pgh': clean_csv_string(business_start_date_in_pgh),
+            'address_full': address_full
+        }]
+
+
+class AddNormalizedAddress(beam.DoFn):
+    def process(self, datum):
+        datum['normalized_address'] = normalize_address_record(datum['address'])
+        yield datum
+
+
+def run(argv=None):
+    dt = datetime.now()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--input',
+                        dest='input',
+                        default='gs://pghpa_finance/{}/{}/{}_smart_trash_containers.csv'.format(dt.strftime('%Y'),
+                                                                                          dt.strftime('%m').lower(),
+                                                                                          dt.strftime("%Y-%m-%d")),
+                        help='Input file to process.')
+    parser.add_argument('--avro_output',
+                        dest='avro_output',
+                        default='gs://pghpa_finance/avro_output/avro_output',
+                        help='Output directory to write avro files.')
+
+    known_args, pipeline_args = parser.parse_known_args(argv)
+
+    #TODO: run on on-prem network when route is opened
+
+    pipeline_args.extend([
+        # Use runner=DataflowRunner to run in GCP environment, runner=DirectRunner to run locally
+        # '--runner=DirectRunner',
+        '--runner=DataflowRunner',
+        '--project=data-rivers',
+        '--staging_location=gs://pghpa_finance/staging',
+        '--temp_location=gs://pghpa_finance/temp',
+        '--job_name=registered-businesses-dataflow',
+        '--subnetwork=https://www.googleapis.com/compute/v1/projects/data-rivers/regions/us-east1/subnetworks/default',
+        '--region=us-east1',
+        '--service_account_email=data-rivers@data-rivers.iam.gserviceaccount.com'
+        # '--subnetwork=https://www.googleapis.com/compute/v1/projects/data-rivers/regions/us-east1/subnetworks/pgh-on-prem-net-142'
+    ])
+
+    schema.RecordSchema.__hash__ = hash_func
+
+    download_schema('pghpa_avro_schemas', 'registered_businesses.avsc', 'registered_businesses.avsc')
+
+    SCHEMA_PATH = 'registered_businesses.avsc'
+    avro_schema = json.loads(open(SCHEMA_PATH).read())
+
+    pipeline_options = PipelineOptions(pipeline_args)
+
+    with beam.Pipeline(options=pipeline_options) as p:
+        # Read the text file[pattern] into a PCollection.
+        lines = p | ReadFromText(known_args.input, skip_header_lines=1)
+
+        load = (
+                lines
+                | beam.ParDo(ConvertToDicts())
+                # | beam.ParDo(AddNormalizedAddress())
+                # TODO: ^add this step once we are on python3
+                | beam.io.avroio.WriteToAvro(known_args.avro_output, schema=avro_schema, file_name_suffix='.avro', use_fastavro=True))
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()
