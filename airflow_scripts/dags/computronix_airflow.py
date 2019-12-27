72b15ae003be6883a769f44a96aef479362ebafe
James O'Toole <jtotoole@gmail.com>
2019-12-27 16:20:26 -0500

    Computronix businesses tasks (#9)
    * clean column names in computronix trades/contractors dataflow jobs
    
    * working computronix pipelines

--- airflow_scripts/dags/computronix_airflow.py
+++ airflow_scripts/dags/computronix_airflow.py
@@ -10,6 +10,7 @@ from airflow.operators.docker_operator import DockerOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.operators.dataflow_operator import DataFlowPythonOperator
 from airflow.contrib.operators import gcs_to_bq
+from airflow.contrib.operators.gcp_container_operator import GKEPodOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.trigger_rule import TriggerRule
 from dependencies import airflow_utils
@@ -44,6 +45,16 @@ gcs_load = DockerOperator(
     dag=dag
 )
 
+# gcs_load = GKEPodOperator(
+#     task_id='computronix_gcs_load',
+#     cluster_name='us-east1-data-rivers-205ba4d7-gk',
+#     image='gcr.io/data-rivers/pgh-computronix',
+#     env_vars={
+#             'GCS_AUTH_FILE': '/root/odata-computronix/data-rivers-service-acct.json'
+#         },
+#     dag=dag
+# )
+
 # dataflow_task = DataFlowPythonOperator(
 #     task_id='trash_cans_dataflow',
 #     job_name='trash-cans-dataflow',
@@ -63,6 +74,14 @@ contractors_dataflow = BashOperator(
     dag=dag
 )
 
+businesses_dataflow = BashOperator(
+    task_id='computronix_businesses_dataflow',
+    bash_command='python {}'.format(os.environ['COMPUTRONIX_BUSINESSES_DATAFLOW']),
+    dag=dag
+)
+
+#TODO: change to GCSBigQueryOperator
+
 trades_bq = BashOperator(
     task_id='trades_bq',
     bash_command='bq load --source_format=AVRO computronix.trade_licenses \
@@ -81,13 +100,22 @@ contractors_bq = BashOperator(
     dag=dag
 )
 
+businesses_bq = BashOperator(
+    task_id='contractors_bq',
+    bash_command='bq load --source_format=AVRO computronix.business_licenses \
+    "gs://pghpa_computronix/business/avro_output/{}/{}/{}/*.avro"'.format(dt.strftime('%Y'),
+                                                                dt.strftime('%m').lower(),
+                                                                dt.strftime("%Y-%m-%d")),
+    dag=dag
+)
+
 beam_cleanup = BashOperator(
     task_id='computronix_beam_cleanup',
     bash_command=airflow_utils.beam_cleanup_statement('pghpa_computronix'),
     dag=dag
 )
 
-# gcs_load
 
 gcs_load >> contractors_dataflow >> contractors_bq >> beam_cleanup
 gcs_load >> trades_dataflow >> trades_bq >> beam_cleanup
+gcs_load >> businesses_dataflow >> businesses_bq >> beam_cleanup
