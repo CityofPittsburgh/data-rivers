df807eaf5346ce36249f5c4b8d1d1f435b070a3e
James O'Toole <jtotoole@gmail.com>
2020-02-07 13:54:59 -0500

    gcs helpers to scrub pii (#16)

--- airflow_scripts/dags/dependencies/gcs_loaders/311_gcs.py
+++ airflow_scripts/dags/dependencies/gcs_loaders/311_gcs.py
@@ -2,7 +2,7 @@ import requests
 import logging
 import os
 
-from gcs_utils import YESTERDAY, now, storage_client, json_to_gcs
+from gcs_utils import YESTERDAY, now, storage_client, json_to_gcs, scrub_pii
 
 
 bucket = '{}_311'.format(os.environ['GCS_PREFIX'])
@@ -10,7 +10,7 @@ payload = {'key': os.environ['QALERT_KEY'], 'since': YESTERDAY.strftime('%s')}
 # qscend requires a value (any value) for the user-agent field  ¯\_(ツ)_/¯
 headers = {'User-Agent': 'City of Pittsburgh ETL'}
 
-request_keys = ['id',
+REQUEST_KEYS = ['id',
                 'master',
                 'addDate',
                 'addDateUnix',
@@ -23,7 +23,6 @@ request_keys = ['id',
                 'streetId',
                 'streetName',
                 'streetNum',
-                'comments',
                 'crossStreetId',
                 'crossStreetName',
                 'district',
@@ -36,7 +35,7 @@ request_keys = ['id',
                 'priorityToDisplay',
                 'resumeDate']
 
-activity_keys = ['actDate',
+ACTIVITY_KEYS = ['actDate',
                  'actDateUnix',
                  'code',
                  'codeDesc',
@@ -49,25 +48,25 @@ activity_keys = ['actDate',
 
 response = requests.get('https://pittsburghpa.qscend.com/qalert/api/v1/requests/changes', params=payload, headers=headers)
 
-cleaned_requests = []
-cleaned_activities = []
+trimmed_requests = []
+trimmed_activities = []
 
+# filter responses to take out unnecessary keys, preserving only those we've defined in request/activity_keys
 if response.status_code == 200:
     for request in response.json()['request']:
-        cleaned_request = {k: request[k] for k in request_keys}
-        cleaned_requests.append(cleaned_request)
+        trimmed_request = {k: request[k] for k in REQUEST_KEYS}
+        trimmed_requests.append(trimmed_request)
 
     for activity in response.json()['activity']:
-        cleaned_activity = {k: activity[k] for k in activity_keys}
-        cleaned_activities.append(cleaned_activity)
-
+        trimmed_activity = {k: activity[k] for k in ACTIVITY_KEYS}
+        trimmed_activities.append(trimmed_activity)
 
 json_to_gcs('{}/{}/{}_requests.json'.format(now.strftime('%Y'),
                                             now.strftime('%m').lower(),
                                             now.strftime("%Y-%m-%d")),
-            cleaned_requests, bucket)
+            trimmed_requests, bucket)
 
 json_to_gcs('{}/{}/{}_activities.json'.format(now.strftime('%Y'),
                                               now.strftime('%m').lower(),
                                               now.strftime("%Y-%m-%d")),
-            cleaned_activities, bucket)
+            trimmed_activities, bucket)
