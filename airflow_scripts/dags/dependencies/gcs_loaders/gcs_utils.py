5f642c95c41ee3beeb3f5711f245f3dfa8988935
James O'Toole <jtotoole@gmail.com>
2020-03-14 16:27:11 -0400

    load initial data from OTRS to GCS (#22)

--- airflow_scripts/dags/dependencies/gcs_loaders/gcs_utils.py
+++ airflow_scripts/dags/dependencies/gcs_loaders/gcs_utils.py
@@ -92,6 +92,25 @@ def regex_filter(value):
     return value
 
 
+def upload_file_gcs(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_file_name = "local/path/to/file"
+    # destination_blob_name = "storage-object-name"
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
+    os.remove(source_file_name)
+
+
 def json_to_gcs(path, json_object, bucket_name):
     blob = storage.Blob(
         name=path,
