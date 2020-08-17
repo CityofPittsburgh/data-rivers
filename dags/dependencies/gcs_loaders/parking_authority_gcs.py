1111d747977937810ac915fc0d04886860c8ab83
James O'Toole <jtotoole@gmail.com>
2020-08-17 18:11:11 -0400

    add parking transactions to gcs load

--- dags/dependencies/gcs_loaders/parking_authority_gcs.py
+++ dags/dependencies/gcs_loaders/parking_authority_gcs.py
@@ -7,6 +7,8 @@ from gcs_utils import json_to_gcs, get_wprdc_data
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--execution_date', dest='execution_date',
                     required=True, help='DAG execution date (YYYY-MM-DD)')
+parser.add_argument('-p', '--prev_execution_date', dest='prev_execution_date',
+                    required=True, help='Prev DAG execution date (YYYY-MM-DD)')
 args = vars(parser.parse_args())
 
 bucket = '{}_parking'.format(os.environ['GCS_PREFIX'])
@@ -16,7 +18,18 @@ parking_meters = get_wprdc_data(
     fields_to_remove=["_geom", "guid", "_the_geom_webmercator"]
 )
 
+parking_transactions = get_wprdc_data(
+    resource_id='1ad5394f-d158-46c1-9af7-90a9ef4e0ce1',
+    where_clauses='"utc_start" >= \'{}\''.format(args['prev_execution_date']),
+    fields_to_remove=["_geom", "_the_geom_webmercator"]
+)
+
 json_to_gcs('meters/{}/{}/{}_meters.json'.format(args['execution_date'].split('-')[0],
                                                  args['execution_date'].split('-')[1],
                                                  args['execution_date']),
             parking_meters, bucket)
+
+json_to_gcs('transactions/{}/{}/{}_transactions.json'.format(args['execution_date'].split('-')[0],
+                                                             args['execution_date'].split('-')[1],
+                                                             args['execution_date']),
+            parking_transactions, bucket)
