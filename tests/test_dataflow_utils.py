a2724000c7dcb74253d8f724687f11bd9f971303
James O'Toole <jtotoole@gmail.com>
2020-10-06 18:53:34 -0400

    fix dataflow_utils test

--- tests/test_dataflow_utils.py
+++ tests/test_dataflow_utils.py
@@ -5,19 +5,20 @@ import future.tests.base  # pylint: disable=unused-import
 
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import unittest
-import mock
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from dags.dependencies.dataflow_scripts.dataflow_utils import dataflow_utils
 
 
 class TestDataflowUtils(unittest.TestCase):
 
-    def columns_camel_to_snake_case(self):
+    def test_columns_camel_to_snake_case(self):
         datum = {'exampleColumn': 'foo', 'anotherExample': 'bar'}
         expected = {'example_column': 'foo', 'another_example': 'bar'}
-        self.assertEqual(dataflow_utils.ColumnsCamelToSnakeCase.process(datum), expected)
+        ccsc = dataflow_utils.ColumnsCamelToSnakeCase()
+        self.assertEqual(next(ccsc.process(datum)), expected)
 
 
 if __name__ == '__main__':
