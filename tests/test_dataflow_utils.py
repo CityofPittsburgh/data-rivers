6ca513a01e2193384cc5e906e2cda3657bab5ffb
James O'Toole <jtotoole@gmail.com>
2020-10-15 15:52:12 -0400

    dataflow_utils_tests cleanup

--- tests/test_dataflow_utils.py
+++ tests/test_dataflow_utils.py
@@ -15,41 +15,26 @@ from dags.dependencies.dataflow_scripts.dataflow_utils import dataflow_utils
 class TestDataflowUtils(unittest.TestCase):
 
     def test_columns_camel_to_snake_case(self):
-            datum = {'exampleColumn': 'foo', 'anotherExample': 'bar'}
-            expected = {'example_column': 'foo', 'another_example': 'bar'}
-            ccsc = dataflow_utils.ColumnsCamelToSnakeCase()
-            self.assertEqual(next(ccsc.process(datum)), expected)
+        datum = {'exampleColumn': 'foo', 'anotherExample': 'bar'}
+        expected = {'example_column': 'foo', 'another_example': 'bar'}
+        ccsc = dataflow_utils.ColumnsCamelToSnakeCase()
+        self.assertEqual(next(ccsc.process(datum)), expected)
 
 
     def test_columns_to_lower_case(self):
-            datum = {'Example_Column': 'foo', 'anotherExample': 'bar', 'With a Space': 'foo'}
-            expected = {'example_column': 'foo', 'anotherexample': 'bar', 'with a space': 'foo'}
-            clc = dataflow_utils.ColumnsToLowerCase()
-            self.assertEqual(next(clc.process(datum)), expected)
+        datum = {'Example_Column': 'foo', 'anotherExample': 'bar', 'With a Space': 'foo'}
+        expected = {'example_column': 'foo', 'anotherexample': 'bar', 'with a space': 'foo'}
+        clc = dataflow_utils.ColumnsToLowerCase()
+        self.assertEqual(next(clc.process(datum)), expected)
 
     
     def test_change_data_types(self):
-            datum = {'count': '1', 'zip': 15213}
-            type_changes = [("count", 'int'), ("zip", 'str')]
-            expected = {'count': 1, 'zip': '15213'}
-            cdt = dataflow_utils.ChangeDataTypes(type_changes)
-            self.assertEqual(next(cdt.process(datum)), expected)
+        datum = {'count': '1', 'zip': 15213}
+        type_changes = [("count", 'int'), ("zip", 'str')]
+        expected = {'count': 1, 'zip': '15213'}
+        cdt = dataflow_utils.ChangeDataTypes(type_changes)
+        self.assertEqual(next(cdt.process(datum)), expected)
 
-
-    def test_swap_field_names(self):
-            datum = {'exampleColumn': 'foo', 'anotherExample': 'bar'}
-            name_changes = [('exampleColumn', 'newExampleColumn'), ('anotherExample', 'newAnotherExample')]
-            expected = {'newExampleColumn': 'foo', 'newAnotherExample': 'bar'}
-            sfn = dataflow_utils.SwapFieldNames(name_changes)
-            self.assertEqual(next(sfn.process(datum)), expected)
-
-
-    def test_get_date_string(self):
-        datum = {'unix_timestamp': 1602619169}
-        date_column_names = [('unix_timestamp', 'string_timestamp')]
-        expected = {'unix_timestamp': 1602619169, 'string_timestamp': '2020-10-13 15:59:29'}
-        gds = dataflow_utils.GetDateStrings(date_column_names)
-        self.assertEqual(next(gds.process(datum)), expected)
 
     def test_swap_field_names(self):
         datum = {'exampleColumn': 'foo', 'anotherExample': 'bar'}
@@ -64,13 +49,6 @@ class TestDataflowUtils(unittest.TestCase):
         expected = {'unix_timestamp': 1602619169, 'string_timestamp': '2020-10-13 15:59:29 EDT'}
         gds = dataflow_utils.GetDateStrings(date_column_names)
         self.assertEqual(next(gds.process(datum)), expected)
-
-    def test_normalize_address(self):
-        datum = {'address': '123 fifth Ave pittsburgh pa'}
-        address_key = 'address'
-        expected = {'address': '123 fifth Ave pittsburgh pa', 'normalized_address': '123 FIFTH AVE PITTSBURGH PA'}
-        na = dataflow_utils.NormalizeAddress(address_key)
-        self.assertEqual(next(na.process(datum)), expected)
 
 
 if __name__ == '__main__':
