5b33d1ddce5524a5141d7115d373de1250260ba3
woodjt <37873653+woodjt@users.noreply.github.com>
2020-10-15 13:51:31 -0400

    this commit adds units tests for dataflow utility functions and some … (#66)
    * this commit adds units tests for dataflow utility functions and some minor changes to dataflow_util.py
    
    * changes to doc strings
    
    * changes to doc strings

--- tests/test_dataflow_utils.py
+++ tests/test_dataflow_utils.py
@@ -15,10 +15,41 @@ from dags.dependencies.dataflow_scripts.dataflow_utils import dataflow_utils
 class TestDataflowUtils(unittest.TestCase):
 
     def test_columns_camel_to_snake_case(self):
-        datum = {'exampleColumn': 'foo', 'anotherExample': 'bar'}
-        expected = {'example_column': 'foo', 'another_example': 'bar'}
-        ccsc = dataflow_utils.ColumnsCamelToSnakeCase()
-        self.assertEqual(next(ccsc.process(datum)), expected)
+            datum = {'exampleColumn': 'foo', 'anotherExample': 'bar'}
+            expected = {'example_column': 'foo', 'another_example': 'bar'}
+            ccsc = dataflow_utils.ColumnsCamelToSnakeCase()
+            self.assertEqual(next(ccsc.process(datum)), expected)
+
+
+    def test_columns_to_lower_case(self):
+            datum = {'Example_Column': 'foo', 'anotherExample': 'bar', 'With a Space': 'foo'}
+            expected = {'example_column': 'foo', 'anotherexample': 'bar', 'with a space': 'foo'}
+            clc = dataflow_utils.ColumnsToLowerCase()
+            self.assertEqual(next(clc.process(datum)), expected)
+
+    
+    def test_change_data_types(self):
+            datum = {'count': '1', 'zip': 15213}
+            type_changes = [("count", 'int'), ("zip", 'str')]
+            expected = {'count': 1, 'zip': '15213'}
+            cdt = dataflow_utils.ChangeDataTypes(type_changes)
+            self.assertEqual(next(cdt.process(datum)), expected)
+
+
+    def test_swap_field_names(self):
+            datum = {'exampleColumn': 'foo', 'anotherExample': 'bar'}
+            name_changes = [('exampleColumn', 'newExampleColumn'), ('anotherExample', 'newAnotherExample')]
+            expected = {'newExampleColumn': 'foo', 'newAnotherExample': 'bar'}
+            sfn = dataflow_utils.SwapFieldNames(name_changes)
+            self.assertEqual(next(sfn.process(datum)), expected)
+
+
+    def test_get_date_string(self):
+        datum = {'unix_timestamp': 1602619169}
+        date_column_names = [('unix_timestamp', 'string_timestamp')]
+        expected = {'unix_timestamp': 1602619169, 'string_timestamp': '2020-10-13 15:59:29'}
+        gds = dataflow_utils.GetDateStrings(date_column_names)
+        self.assertEqual(next(gds.process(datum)), expected)
 
 
 if __name__ == '__main__':
