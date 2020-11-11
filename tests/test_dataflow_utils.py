d2fd6b2811f6a0ac094689a50e4b8f5f7968822c
James O'Toole <jtotoole@gmail.com>
2020-11-11 14:21:30 -0500

    refactor test_dataflow_utils to import dataflow_utils package

--- tests/test_dataflow_utils.py
+++ tests/test_dataflow_utils.py
@@ -2,12 +2,9 @@ from __future__ import absolute_import
 from __future__ import division
 # patches unittest.TestCase to be python3 compatible
 import future.tests.base  # pylint: disable=unused-import
-
-import os
-import sys
 import unittest
 
-from dags.dependencies.dataflow_scripts.dataflow_utils import dataflow_utils
+from dataflow_utils import dataflow_utils
 
 
 class TestDataflowUtils(unittest.TestCase):
@@ -15,6 +12,7 @@ class TestDataflowUtils(unittest.TestCase):
     def test_columns_camel_to_snake_case(self):
         datum = {'exampleColumn': 'foo', 'anotherExample': 'bar'}
         expected = {'example_column': 'foo', 'another_example': 'bar'}
+        # ccsc = ColumnsCamelToSnakeCase()
         ccsc = dataflow_utils.ColumnsCamelToSnakeCase()
         self.assertEqual(next(ccsc.process(datum)), expected)
 
