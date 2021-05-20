56f8b4aaf5dfb4f28d95ca24ca1e5c6ea92c37c4
Pranav Banthia <pranavbanthia28@gmail.com>
2021-05-20 11:49:58 -0400

    Extend the standardizeTime test case to include API calls
    The current unit test function for the standardizeTime dataflow utils class uses a parser to parse hard coded datetime strings. This additional test case has a dynamic datetime input with different formats for each run cycle. We make an API call to fetch the Unix timestamp, UTC and EST times. The API response is then verified against the values retrieved from the dataflow utils time standardize function.

--- tests/test_dataflow_utils.py
+++ tests/test_dataflow_utils.py
@@ -1,11 +1,16 @@
 from __future__ import absolute_import
 from __future__ import division
 # patches unittest.TestCase to be python3 compatible
+import datetime
+
 import future.tests.base  # pylint: disable=unused-import
 import unittest
 import numpy as np
+import requests
 
+from dateutil import parser
 from dataflow_utils import dataflow_utils
+import pytz
 
 
 class TestDataflowUtils(unittest.TestCase):
@@ -16,14 +21,12 @@ class TestDataflowUtils(unittest.TestCase):
         ccsc = dataflow_utils.ColumnsCamelToSnakeCase()
         self.assertEqual(next(ccsc.process(datum)), expected)
 
-
     def test_columns_to_lower_case(self):
         datum = {'Example_Column': 'foo', 'anotherExample': 'bar', 'With a Space': 'foo'}
         expected = {'example_column': 'foo', 'anotherexample': 'bar', 'with a space': 'foo'}
         clc = dataflow_utils.ColumnsToLowerCase()
         self.assertEqual(next(clc.process(datum)), expected)
 
-    
     def test_change_data_types(self):
         datum = {'count': '1', 'zip': 15213, 'temp': 72, 'day': 31.1,
                  'bool1': 'TRUE', 'bool2': 1, 'nan_float': np.nan, 'nan_int': np.nan, 'nan_str': np.nan}
@@ -81,6 +84,54 @@ class TestDataflowUtils(unittest.TestCase):
         expected = {'state': 'pa'}
         ff = dataflow_utils.FilterFields(relevant_fields, exclude_relevant_fields=False)
         self.assertEqual(next(ff.process(datum)), expected)
+
+    def test_standardize_times(self):
+        datum = {'openedDate': 'Fri July 19 03:21:55 UTC 2019', 'closedDate': '2021-05-01 01:44:00-04:00'}
+        params = [('openedDate', 'Mountain'), ('closedDate', 'UTC')]
+        expected = datum.copy()
+        expected.update(dict(openedDate_UTC=parser.parse('2019-07-19 09:21:55+00:00'),
+                             openedDate_EST=parser.parse('2019-07-19 05:21:55-04:00'),
+                             openedDate_UNIX=1563528115.0,
+                             closedDate_UTC=parser.parse('2021-05-01 01:44:00+00:00'),
+                             closedDate_EST=parser.parse('2021-04-30 21:44:00-04:00'),
+                             closedDate_UNIX=1619833440.0))
+        tst = dataflow_utils.StandardizeTimes(params)
+        self.assertEqual(next(tst.process(datum)), expected)
+
+    def test_standardize_times_with_api(self):
+        datetime_ = datetime.datetime.now().replace(second=0, microsecond=0)
+        time_zones_ = ['Mountain', 'Eastern', 'Universal', 'EU', 'New York', 'GMT', 'MST', 'Britain']
+        dt_formats = ["%Y-%m-%dT%H:%M:%S", "%Y/%m/%d %H:%M:%S %z", "%Y.%m.%d %H:%M:%S", "%a %B %d %H:%M:%S %Z %Y",
+                      "%a %B %d %H:%M:%S %Y", "%c", "%d %b, %Y %H:%M:%S", "%m/%d/%Y, %H:%M:%S"]
+
+        date_to_unix_ = "https://showcase.api.linx.twenty57.net/UnixTime/tounix"
+        unix_to_timezone_ = "https://showcase.api.linx.twenty57.net/UnixTime/fromunixtimestamp"
+
+        for i in range(0, 8):
+            datetime_ -= datetime.timedelta(minutes=5000)
+            conv_tz = dataflow_utils.TZ_CONVERTER[time_zones_[i]]
+            loc_time = pytz.timezone(conv_tz).localize(datetime_, is_dst=None)
+            formatted_loc_time = loc_time.strftime(dt_formats[i])
+            datum = {'openedDate': formatted_loc_time}
+            param = [('openedDate', time_zones_[i])]
+
+            api_request_unix = requests.get(url=date_to_unix_,
+                                            params={'date': loc_time})
+            api_request_est = requests.post(url=unix_to_timezone_,
+                                            json={"UnixTimeStamp": api_request_unix.json(), "Timezone": "-4"})
+            api_request_utc = requests.post(url=unix_to_timezone_,
+                                            json={"UnixTimeStamp": api_request_unix.json(), "Timezone": ""})
+
+            utc_time = datetime.datetime.strptime(str(api_request_utc.json()['Datetime']), "%Y-%m-%dT%H:%M:%S%z")
+            est_time = datetime.datetime.strptime(str(api_request_est.json()['Datetime']), "%Y-%m-%dT%H:%M:%S%z")
+
+            expected = dict(openedDate=str(formatted_loc_time),
+                            openedDate_UNIX=float(api_request_unix.json()),
+                            openedDate_UTC=str(utc_time),
+                            openedDate_EST=str(est_time))
+
+            tst = dataflow_utils.StandardizeTimes(param)
+            self.assertEqual(next(tst.process(datum)), expected)
 
 
 if __name__ == '__main__':
