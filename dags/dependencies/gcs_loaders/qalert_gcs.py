276815ca81742461e28223671b09660bac67bdf8
Jesse Wood <woodjt.mail@gmail.com>
2021-08-16 16:22:56 -0400

    in the initial draft of the GCS Loader we were planning on having the activities dataset in the DAG. Upon further reflection, it seems like this is an unnecessary dataset and we will instead deprecate/remove all of the code relevant to activities. This commit updates the script to cover removal of the activities-related code

--- dags/dependencies/gcs_loaders/qalert_gcs.py
+++ dags/dependencies/gcs_loaders/qalert_gcs.py
@@ -23,54 +23,36 @@ response = requests.get('https://pittsburghpa.qscend.com/qalert/api/v1/requests/
 
 # convert the utf-8 encoded string to json
 full_requests = response.json()['request']
-full_activities = response.json()['activity']
 
-# Comments must be scrubbed for PII from both 311 requests and activities.
+# Comments must be scrubbed for PII from 311 requests.
 # Comments do not follow strict formatting so this is an imperfect approximation.
 # Steps: extract fields, detect person names that are followed by hotwords for exclusion (e.g. park or street),
 # place an underscore between the detected words to prevent accidental redaction, redact PII the user information in
 # activities needs to be scrubbed, but does not need to be screened for hotwords
 
 
-pre_clean = {"req_comments": [], "act_comments": [], "act_user": []}
+pre_clean = {"req_comments": []}
 for row in full_requests:
     pre_clean["req_comments"].append(row.get("comments", ""))
-
-for row in full_activities:
-    pre_clean["act_comments"].append(row.get("comments", ""))
-    pre_clean["act_user"].append(row.get("user", ""))
 
 # convert list to string that is seperated with a delimiter sequence (allows full list to be processed as one string,
 # as opposed to mapping it to each element which is 100x slower)
 delim_seq = os.environ["DELIM"]
 pre_clean["req_comments"] = delim_seq.join(pre_clean["req_comments"])
-pre_clean["act_comments"] = delim_seq.join(pre_clean["act_comments"])
-pre_clean["act_user"] = delim_seq.join(pre_clean["act_user"])
 
-# # scrub pii
+
+# scrub pii
 scrubbed_req_comments = replace_pii(pre_clean["req_comments"], retain_location = True)
-scrubbed_act_comments = replace_pii(pre_clean["act_comments"], retain_location = True)
-scrubbed_act_user = replace_pii(pre_clean["act_user"], retain_location = False)
 
 # convert delim-seperated string back to list of strings
 split_req_comments = scrubbed_req_comments.split(delim_seq)
-split_act_comments = scrubbed_act_comments.split(delim_seq)
-split_act_user = scrubbed_act_user.split(delim_seq)
 
 # overwrite the original fields with scrubbed data
 for i in range(len(full_requests)):
     full_requests[i]["comments"] = split_req_comments[i].strip()
-
-for i in range(len(full_activities)):
-    full_activities[i]["comments"] = split_act_comments[i].strip()
-    full_activities[i]["user"] = split_act_user[i].strip()
 
 # load to gcs
 json_to_gcs('requests/{}/{}/{}_requests.json'.format(args['execution_date'].split('-')[0],
                                                      args['execution_date'].split('-')[1],
                                                      args['execution_date']),
             full_requests, bucket)
-json_to_gcs('activities/{}/{}/{}_activities.json'.format(args['execution_date'].split('-')[0],
-                                                         args['execution_date'].split('-')[1],
-                                                         args['execution_date']),
-            full_activities, bucket)
