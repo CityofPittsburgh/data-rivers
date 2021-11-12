c286cc1e6e9753c993dd992feaa7b101d7ba664a
Jesse Wood <woodjt.mail@gmail.com>
2021-11-12 11:18:13 -0500

    adding the backfill code to this branch

--- dags/qalert_backfill_airflow.py
+++ dags/qalert_backfill_airflow.py
@@ -13,7 +13,6 @@ from dependencies import airflow_utils
 from dependencies.airflow_utils import get_ds_year, get_ds_month, default_args, \
     format_dataflow_call, build_city_limits_query, build_revgeo_time_bound_query
 
-
 # TODO: When Airflow 2.0 is released, upgrade the package, sub in DataFlowPythonOperator for BashOperator,
 #  and pass the argument 'py_interpreter=python3'
 
@@ -51,7 +50,6 @@ last_action_unix, last_action_utc, closed_date_est, closed_date_unix, closed_dat
 cross_street_id, city, address_type, anon_google_formatted_address, neighborhood_name, council_district, ward, 
 police_zone, fire_zone, dpw_streets, dpw_enviro, dpw_parks, anon_lat, anon_long"""
 
-
 # TODO: change interval to 5 min. Alter bucket/table declarations appropriately
 # This DAG will run every 5 min which is a departure from our SOP. the schedule interval reflects this in CRON
 # nomemclature. The 5 min interval was chosen to accomodate WPRDC's needs.
@@ -62,37 +60,42 @@ dag = DAG(
         user_defined_filters = {'get_ds_month': get_ds_month, 'get_ds_year': get_ds_year}
 )
 
+# TODO: The GCS loader was/can be run outside of the DAG for testing purposes. (Execution time for 04-2015 through
+# 11-2021 bacfill was approx 1 hour). Uncomment this if it necessary to re-run.
 
 # Run gcs_loader
-qalert_requests_gcs = BashOperator(
-        task_id = 'qalert_gcs',
-        bash_command = F"python {os.environ['DAGS_PATH']}/dependencies/gcs_loaders/qalert_gcs.py",
-        dag = dag
-)
+# qalert_requests_gcs = BashOperator(
+#         task_id = 'qalert_gcs',
+#         bash_command = F"python {os.environ['DAGS_PATH']}/dependencies/gcs_loaders/qalert_gcs.py",
+#         dag = dag
+# )
 
 
 # Run dataflow_script
-df_cmd_str, date_direc, ts = format_dataflow_call("qalert_requests_dataflow.py", "qalert", "requests", "requests")
+# df_cmd_str, date_direc, ts = format_dataflow_call("qalert_requests_dataflow.py", "qalert", "requests", "requests")
+py_cmd = f"python {os.environ['DAGS_PATH']}dags/dependencies/dataflow_scripts/qalert_requests_dataflow.py"
+in_cmd = f" --input gs://{os.environ['GCS_PREFIX']}_qalert/requests/backfill/data" \
+  "/requests_backfill_data_backfilled_requests_formatted.json"
+out_cmd = f" --avro_output gs://{os.environ['GCS_PREFIX']}_qalert/requests/backfill/data/avro_output/"
+df_cmd_str = py_cmd + in_cmd + out_cmd
 qalert_requests_dataflow = BashOperator(
         task_id = 'qalert_dataflow',
         bash_command = df_cmd_str,
         dag = dag
 )
 
-
 # Load AVRO data produced by dataflow_script into BQ temp table
 qalert_requests_bq = GoogleCloudStorageToBigQueryOperator(
         task_id = 'qalert_bq',
-        destination_project_dataset_table = f"{os.environ['GCLOUD_PROJECT']}:qalert.new_req",
-        bucket = f"{os.environ['GCS_PREFIX']}_qalert",
-        source_objects = [f"requests/avro_output/{date_direc}/{ts}/*.avro"],
+        destination_project_dataset_table = f"{os.environ['GCLOUD_PROJECT']}:qalert.backfill_req",
+        bucket = f"gs://{os.environ['GCS_PREFIX']}_qalert/requests/backfill/data/avro_output/",
+        source_objects = [f"requests/backfill/data/avro_output/*.avro"],
         write_disposition = 'WRITE_TRUNCATE',
         create_disposition = 'CREATE_IF_NEEDED',
         source_format = 'AVRO',
         autodetect = True,
         dag = dag
 )
-
 
 # Update geo coords with lat/long cast as floats (dataflow/AVRO glitch forces them to be output as strings; the
 # source of the error is instrinsic to dataflow and may not be fixable). Also, dedupe the results (someties the same
@@ -107,7 +110,7 @@ WITH formatted AS
         CAST(anon_lat AS FLOAT64) AS anon_lat,
         CAST(anon_long AS FLOAT64) AS anon_long,
     FROM 
-        {os.environ['GCLOUD_PROJECT']}.qalert.new_req
+        {os.environ['GCLOUD_PROJECT']}.qalert.backfill_req
     )
 SELECT {COLS_IN_ORDER} FROM formatted
 """
@@ -115,14 +118,13 @@ qalert_requests_format_dedupe = BigQueryOperator(
         task_id = 'qalert_dedupe_and_format',
         sql = query_format,
         use_legacy_sql = False,
-        destination_dataset_table = f"{os.environ['GCLOUD_PROJECT']}:qalert.new_req",
+        destination_dataset_table = f"{os.environ['GCLOUD_PROJECT']}:qalert.backfill_req",
         write_disposition = 'WRITE_TRUNCATE',
         dag = dag
 )
 
-
 # Query new tickets to determine if they are in the city limits
-query_city_lim = build_city_limits_query('qalert', 'new_req', 'pii_lat', 'pii_long')
+query_city_lim = build_city_limits_query('qalert', 'backfill_req', 'pii_lat', 'pii_long')
 qalert_requests_city_limits = BigQueryOperator(
         task_id = 'qalert_city_limits',
         sql = query_city_lim,
@@ -130,11 +132,10 @@ qalert_requests_city_limits = BigQueryOperator(
         dag = dag
 )
 
-
 # TODO: investigate (and if necessary fix) the unknown source of duplicates in the geojoin query (see util function
 #  for clearer explanation)
 # Join all the geo information (e.g. DPW districts, etc) to the new data
-query_geo_join = build_revgeo_time_bound_query('qalert', 'new_req', 'create_date_est', 'id', 'pii_lat',
+query_geo_join = build_revgeo_time_bound_query('qalert', 'backfill_req', 'create_date_est', 'id', 'pii_lat',
                                                'pii_long', COLS_IN_ORDER)
 qalert_requests_geojoin = BigQueryOperator(
         task_id = 'qalert_geojoin',
@@ -143,16 +144,15 @@ qalert_requests_geojoin = BigQueryOperator(
         dag = dag
 )
 
-
 # Append the geojoined and de-duped new_req to all_requests (replace table after append to order by ID. BQ does
 # not allow this in INSERT statements (2021-10-01)
 query_append = f"""
-INSERT INTO {os.environ['GCLOUD_PROJECT']}.qalert.all_actions
+INSERT INTO {os.environ['GCLOUD_PROJECT']}.qalert.backfill_all_actions
 SELECT 
     {COLS_IN_ORDER} 
 FROM 
-    `{os.environ['GCLOUD_PROJECT']}.qalert.new_geo_enriched_deduped`;
-    
+    `{os.environ['GCLOUD_PROJECT']}.qalert.backfill_geo_enriched_deduped`;
+
 CREATE OR REPLACE TABLE {os.environ['GCLOUD_PROJECT']}.qalert.all_requests AS
 SELECT 
     {COLS_IN_ORDER} 
@@ -166,7 +166,6 @@ qalert_requests_merge_new_tickets = BigQueryOperator(
         use_legacy_sql = False,
         dag = dag
 )
-
 
 # Split new tickets by parent/child status
 query_split_parent = f"""
@@ -187,7 +186,6 @@ qalert_requests_split_new_parents = BigQueryOperator(
         dag = dag
 )
 
-
 query_split_child = f"""
 CREATE OR REPLACE TABLE {os.environ['GCLOUD_PROJECT']}.qalert.new_children AS
 SELECT 
@@ -202,7 +200,6 @@ qalert_requests_split_new_children = BigQueryOperator(
         use_legacy_sql = False,
         dag = dag
 )
-
 
 # Add new parents in all_linked_requests
 query_append_new_parents = f"""
@@ -219,9 +216,7 @@ qalert_requests_append_new_parent_tickets = BigQueryOperator(
         dag = dag
 )
 
-
-
-#TODO: STILL NEED TO UPDATE THE CLOSED STATUS, LAST ACTION, etc.
+# TODO: STILL NEED TO UPDATE THE CLOSED STATUS, LAST ACTION, etc.
 # Solution?: get the status/last action of the YOUNGEST child in the inner with block
 # Select * except (above fields ^) from alr in the outer with block Instead, select the above fields (^) from the
 # inner WITH block
@@ -231,17 +226,17 @@ query_update_parent = f"""
 -- Create/Replace table after all nested WITH operators
 CREATE OR REPLACE TABLE {os.environ['GCLOUD_PROJECT']}.qalert.all_linked_requests AS
 
-    
+
     -- Outer WITH operator (all_appended): 
     WITH all_appended AS
     (
-            
+
         -- inner with operator (child_combined): returns an id and total number of child requests from the 
         -- new_children table that correspond to a parent ticket.  
         -- (the count of child tickets in each batch will always be >= 1 for each ticket, and has no upper bound)
         -- also concatenates all the pii_comments and pii_private_notes for each group of children together
         WITH child_combined AS 
-        
+
         (
         SELECT
             nc.parent_ticket_id AS match_id,
@@ -252,7 +247,7 @@ CREATE OR REPLACE TABLE {os.environ['GCLOUD_PROJECT']}.qalert.all_linked_request
             {os.environ['GCLOUD_PROJECT']}.qalert.new_children nc
         GROUP BY match_id
         )
-    
+
         -- Outer WITH operator (all_appended):
         -- these results represent all of the information in all_linked_requests 
         -- prior to the append job, that was then updated with total request counts and comment/note concatentation         
@@ -287,7 +282,6 @@ qalert_requests_update_parent_tickets = BigQueryOperator(
         dag = dag
 )
 
-
 # Create a table from all_linked_requests that has all columns EXCEPT those that have potential PII. This table is
 # subsequently exported to WPRDC. BQ will not currently (2021-10-01) allow data to be pushed from a query and it must
 # be stored in a table prior to the push. Thus, this is a 2 step process also involving the operator below.
@@ -305,7 +299,6 @@ qalert_requests_drop_pii_for_export = BigQueryOperator(
         dag = dag
 )
 
-
 # Export table as CSV to WPRDC bucket
 qalert_wprdc_export = BigQueryToCloudStorageOperator(
         task_id = 'qalert_wprdc_export',
@@ -313,7 +306,6 @@ qalert_wprdc_export = BigQueryToCloudStorageOperator(
         destination_cloud_storage_uris = [f"gs://{os.environ['GCS_PREFIX']}_wprdc/qalert_requests_" + "{{ ds }}.csv"],
         dag = dag
 )
-
 
 # TODO: Data Services team has IAM issues preventing access to Web Team BQ. Uncomment when this is resolved.
 # Export to Web Dev team BQ table
@@ -332,15 +324,14 @@ qalert_wprdc_export = BigQueryToCloudStorageOperator(
 # )
 
 
-#TODO: this code block was added as part of the dubugging process and it is unclear if it is needed. Will keep/remove
+# TODO: this code block was added as part of the dubugging process and it is unclear if it is needed. Will keep/remove
 # (remember to remove the import statement also)
 # when that is more clear.
 delete_new_req = BigQueryTableDeleteOperator(
-    task_id="delete_new_req",
-    deletion_dataset_table=f"{os.environ['GCLOUD_PROJECT']}.qalert.new_req",
-    dag = dag
+        task_id = "delete_new_req",
+        deletion_dataset_table = f"{os.environ['GCLOUD_PROJECT']}.qalert.new_req",
+        dag = dag
 )
-
 
 # Clean up
 qalert_beam_cleanup = BashOperator(
@@ -349,12 +340,10 @@ qalert_beam_cleanup = BashOperator(
         dag = dag
 )
 
-
-
 # DAG execution:
-qalert_requests_gcs >> qalert_requests_dataflow >> qalert_requests_bq >> qalert_requests_format_dedupe >> \
+# qalert_requests_gcs >> \
+qalert_requests_dataflow >> qalert_requests_bq >> qalert_requests_format_dedupe >> \
 qalert_requests_city_limits >> qalert_requests_geojoin >> qalert_requests_merge_new_tickets >> \
 qalert_requests_split_new_parents >> qalert_requests_split_new_children >> \
 qalert_requests_append_new_parent_tickets >> qalert_requests_update_parent_tickets >> \
 qalert_requests_drop_pii_for_export >> qalert_wprdc_export >> delete_new_req >> qalert_beam_cleanup
-
