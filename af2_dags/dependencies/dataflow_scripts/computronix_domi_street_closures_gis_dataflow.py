4c3b93d9cf303514bc15ce20a28a37f493e59005
Jesse Wood <woodjt.mail@gmail.com>
2022-05-20 08:19:02 -0400

    this represents a complete version of the dataflow script. It has not been thoroughly tested and refactors are likely. The commit here is to allow a merge back into the CX mainline

--- af2_dags/dependencies/dataflow_scripts/computronix_domi_street_closures_gis_dataflow.py
+++ af2_dags/dependencies/dataflow_scripts/computronix_domi_street_closures_gis_dataflow.py
@@ -2,35 +2,65 @@ from __future__ import absolute_import
 
 import logging
 import os
+from abc import ABC
 
 import apache_beam as beam
 from apache_beam.io import ReadFromText
 from apache_beam.io.avroio import WriteToAvro
 
 import dataflow_utils
-from dataflow_utils.dataflow_utils import JsonCoder, SwapFieldNames, ChangeDataTypes, ConvertBooleans, \
-    FilterOutliers, StandardizeTimes, GoogleMapsClassifyAndGeocode, generate_args
+from dataflow_utils.dataflow_utils import JsonCoder, SwapFieldNames, ConvertBooleans, StandardizeTimes, generate_args
 
 
 # The CX data contains fields that are nested. We need to extract that information, which is accomplished by this
-# function. Additionally, the new field names that we derive are formatted in snake case.
-class UnNestFields(beam.DoFn, ABC):
-    def __init__(self, un_nest):
-        """
-        :param un_nest: list of tuples; each tuple consists of the 1) the parent field we want to un_nest from
-        2) a list of the new field names for the unnested child fields
-        """
-        self.un_nest = un_nest
+# function. Additionally, the new field names that we derive are formatted in snake case. Several IDs for segments of
+# street closures are convert to strings explicity. The CX system returns some IDs as strings and others as INTs,
+# so they are standarized here.
+
+# Sometimes many rows created from one. If there is more than one segment of street closed, a separate row is
+# created for eacah. For each of the multiple segments All values, except the segment IDs are identical and redundant
+# (all fields other than the IDs). Though this is inefficient from a data storage perspective, it is needed by the
+# GIS team for compliance with their software as of 05/2022
 
 
-# TODO: what needs to happen here is we need to unnest all fields and give them a new new. however,
-# the problem is that there are sometimes going to be many rows created from 1. if there is more than one carte id
-# you have to make that many rows
-# so pass in the field (parent and child)
-# then loop thru each val and each time create a new row with copied values redundant and the new stuff in carte id etc
-# to do this- create a new var ("datum 2") and return that. this will basically take the place of datum original
-# don't have to return input datum it's not useful;
-# carte id is the only field we care about for determing new rows
+class UnNestRenameFields(beam.DoFn, ABC):
+    def __init__(self, nested_data):
+        """
+
+        """
+        self.nested_data = nested_data
+
+    def process(self, datum):
+        old_nest_keys = self.nested_data[0]
+        new_unnest_keys = self.nested_data[1]
+
+        # if there are values to unnest
+        if datum["street_closure"]:
+            # iterate through all nested fields and extract them
+            for (onk, nuk) in zip(old_nest_keys, new_unnest_keys):
+                datum[nuk] = datum["street_closure"][0][onk]
+
+            # there can be multiple segments per ticket; each segment needs to be made a separate row,
+            # and all information needs to be present in each row, with the only difference being the segment. Thus,
+            # two segments from the same record will have redundant information, with only the segment information
+            # being unique. this is easy to accomplish by first extracting the segments to a local var, then looping
+            # over them, each time simply adding the segment info to the existing copy of datum (preserving the data)
+            segs = datum["street_segment"]
+
+            # after this extraction, these columns have no content and can be deleted. this needs to be done outside
+            # of the loop to prevent a key error
+            del datum["street_closure"]
+            del datum["street_segment"]
+
+            # loop through the segments
+            for s in segs:
+                datum["closure_id"] = str(s["UNIQUEID"])
+                datum["carte_id"] = str(s["CARTEID"])
+                # yield will return datum without exiting the loop
+                yield datum
+        else:
+            del datum["street_closure"]
+            yield datum
 
 
 def run(argv = None):
@@ -42,34 +72,37 @@ def run(argv = None):
     )
 
     with beam.Pipeline(options = pipeline_options) as p:
-        un_nest_fields = [
-                ["SPECIALINSTRUCTIONS", "FROMDATE", "TODATE", "WEEKDAYWORKHOURS", "WEEKENDWORKHOURS", "PRIMARYSTREET",
-                 "FROMSTREET", "TOSTREET", "FULLCLOSURE", "TRAVELLANE", "PARKINGLANE", "METEREDPARKING", "SIDEWALK",
-                 "VALIDATED"],
-                ["UNIQUEID", "CARTEID"]
-        ]
+        name_swaps = [("EXTERNALFILENUM", "ext_file_num"), ("PERMITTYPEPERMITTYPE", "permit_type"),
+                      ("WORKDESCRIPTION", "work_desc"), ("TYPEOFWORKDESCRIPTION", "type_work_desc"),
+                      ("APPLICANTCUSTOMFORMATTEDNAME", "applicant_name"),
+                      ("ALLCONTRACTORSNAME", "contractor_name"), ("CREATEDDATE", "create_date"),
+                      ("DOMISTREETCLOSURE", "street_closure")
+                      ]
 
-        new_names = [
-                ["special_instructions", "from_date", "to_date", "weekday_hours", "weekend_hours", "primary_street",
-                 "from_street", "to_street", "full_closure", "travel_lane", "parking_lane", "metered_parking",
-                 "sidewalk", "validated"],
-                ["unique_id", "carte_id"]
-        ]
+        un_nest_names = ["SPECIALINSTRUCTIONS", "FROMDATE", "TODATE", "WEEKDAYWORKHOURS", "WEEKENDWORKHOURS",
+                         "PRIMARYSTREET", "FROMSTREET", "TOSTREET", "FULLCLOSURE", "TRAVELLANE", "PARKINGLANE",
+                         "METEREDPARKING", "SIDEWALK", "VALIDATED", "STREETCLOSUREDOMISTREETSEGMENT"]
+        new_names = ["special_instructions", "from_date", "to_date", "weekday_hours", "weekend_hours", "primary_street",
+                     "from_street", "to_street", "full_closure", "travel_lane", "parking_lane", "metered_parking",
+                     "sidewalk", "validated", "street_segment"]
+        nested = [un_nest_names, new_names]
 
-        un_nest_data = [
-                ('DOMISTREETCLOSURE', un_nest_fields[0], new_names[0]),
-                ("STREETCLOSUREDOMISTREETSEGMENT", un_nest_fields[1], new_names[1])
-        ]
+        times = [("create_date", "EST"), ("from_date", "EST"), ("to_date", "EST")]
+
+        bool_convs = [("full_closure", "Y", "N", None), ("travel_lane", "Y", "N", None),
+                      ("parking_lane", "Y", "N", None), ("metered_parking", "Y", "N", None),
+                      ("sidewalk", "Y", "N", None), ("validated", "Y", "N", None)]
 
         lines = p | ReadFromText(known_args.input, coder = JsonCoder())
 
         load = (
                 lines
-                | beam.ParDo(UnNestFields(un_nest_data))
-                # | beam.ParDo(SwapFieldNames(field_name_swaps))
+                | beam.ParDo(SwapFieldNames(name_swaps))
+                | beam.ParDo(UnNestRenameFields(nested))
+                | beam.ParDo(StandardizeTimes(times, True))
+                | beam.ParDo(ConvertBooleans(bool_convs, True))
                 | WriteToAvro(known_args.avro_output, schema = avro_schema, file_name_suffix = '.avro',
                               use_fastavro = True))
-
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
