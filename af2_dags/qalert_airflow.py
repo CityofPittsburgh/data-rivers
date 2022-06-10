abb983e04928fb11672785254330b25ecd831ac7
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2022-06-10 17:42:21 -0400

    Fix Column Ordering and Set Default Project for WPRDC Export

--- af2_dags/qalert_airflow.py
+++ af2_dags/qalert_airflow.py
@@ -19,7 +19,7 @@ from dependencies.airflow_utils import get_ds_year, get_ds_month, get_ds_day, de
 
 COLS_IN_ORDER = """id, parent_ticket_id, child_ticket, dept, status_name, status_code, request_type_name, 
 request_type_id, origin, pii_comments, pii_private_notes, create_date_est, create_date_utc, create_date_unix, 
-last_action_est, last_action_unix, last_action_utc, closed_date_est, closed_date_utc, closed_date_unix, 
+last_action_est, last_action_utc, last_action_unix, closed_date_est, closed_date_utc, closed_date_unix, 
 pii_street_num, street, cross_street, street_id, cross_street_id, city, pii_input_address, 
 pii_google_formatted_address, address_type, anon_google_formatted_address, neighborhood_name, 
 council_district, ward, police_zone, fire_zone, dpw_streets, dpw_enviro, dpw_parks, input_pii_lat, input_pii_long, 
@@ -470,6 +470,7 @@ wprdc_export = BigQueryToCloudStorageOperator(
         task_id = 'wprdc_export',
         source_project_dataset_table = f"{os.environ['GCLOUD_PROJECT']}:qalert.data_export_scrubbed",
         destination_cloud_storage_uris = [f"gs://{os.environ['GCS_PREFIX']}_wprdc/qalert_requests_{path}.csv"],
+        bigquery_conn_id='google_cloud_default',
         dag = dag
 )
 
