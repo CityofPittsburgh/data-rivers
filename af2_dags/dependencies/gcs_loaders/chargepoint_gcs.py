d4c2f03f5445fa5b263d9b302686344b6e0f9cf7
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2022-07-20 16:40:09 -0400

    Change Backfill JSON File Naming Convention to Use Date Instead of Record Number

--- af2_dags/dependencies/gcs_loaders/chargepoint_gcs.py
+++ af2_dags/dependencies/gcs_loaders/chargepoint_gcs.py
@@ -54,52 +54,54 @@ interval = 1
 start = '</responseText>'
 end = '</ns1:getChargingSessionDataResponse>'
 
-while datetime.strptime(run_stop_win, "%Y-%m-%dT%H:%M:%SZ") <= datetime.strptime(args['lookback_date'], "%Y-%m-%d"):
-    run_start_win, first_run = find_last_successful_run(bucket, "successful_run_log/log.json", DEFAULT_RUN_START)
-    #end_time = run_start_win
-    more = True
-    while more is True:
-        # API call to get data
-        response = requests.post(BASE_URL, data=generate_xml(run_start_win, interval), headers=headers)
-        vals = response.text[response.text.find(start) + len(start):response.text.rfind(end)]
-        vals = '<root>' + vals + '</root>'
-        xml_dict = xmltodict.parse(xml_input=vals, encoding='utf-8')
-        if xml_dict['root']['MoreFlag']:
-            more = (xml_dict['root']['MoreFlag'] == '1')
-        else:
-            more = False
-        records = xml_dict['root']['ChargingSessionData']
-        #end_time = records[len(records)-1]['endTime']
-        interval += INCREMENT_RECORDS
+#while datetime.strptime(run_stop_win, "%Y-%m-%dT%H:%M:%SZ") <= datetime.strptime(args['lookback_date'], "%Y-%m-%d"):
+run_start_win, first_run = find_last_successful_run(bucket, "successful_run_log/log.json", DEFAULT_RUN_START)
+start_time = run_start_win
+more = True
+while more is True:
+    # API call to get data
+    response = requests.post(BASE_URL, data=generate_xml(run_start_win, interval), headers=headers)
+    vals = response.text[response.text.find(start) + len(start):response.text.rfind(end)]
+    vals = '<root>' + vals + '</root>'
+    xml_dict = xmltodict.parse(xml_input=vals, encoding='utf-8')
+    if xml_dict['root']['MoreFlag']:
+        more = (xml_dict['root']['MoreFlag'] == '1')
+    else:
+        more = False
+    records = xml_dict['root']['ChargingSessionData']
+    end_time = records[len(records)-1]['endTime']
+    interval += INCREMENT_RECORDS
 
-        # write the successful run information (used by each successive run to find the backfill start date)
-        successful_run = {
-            "requests_retrieved": len(records),
-            "since": run_start_win,
-            "current_run": run_stop_win,
-            "note": "Data retrieved between the time points listed above"
-        }
-        json_to_gcs("successful_run_log/log.json", [successful_run],
-                    bucket)
+    # write the successful run information (used by each successive run to find the backfill start date)
+    successful_run = {
+        "requests_retrieved": len(records),
+        "since": run_start_win,
+        "current_run": run_stop_win,
+        "note": "Data retrieved between the time points listed above"
+    }
+    json_to_gcs("successful_run_log/log.json", [successful_run],
+                bucket)
 
-        # each run of data extracted from the API will be appended to a growing JSON and saved as an individual JSON
-        append_target_path = f"energy/backfill/{args['lookback_date']}/backfilled_records.json"
-        curr_run_target_path = f"energy/backfill/{args['lookback_date']}/-{interval}_records.json"
-        temp_target_path = f"energy/backfill/{args['lookback_date']}/temp_uploaded_blob.json"
+    # each run of data extracted from the API will be appended to a growing JSON and saved as an individual JSON
+    append_target_path = f"energy/backfill/{args['lookback_date']}/backfilled_records.json"
+    curr_run_target_path = f"energy/backfill/{args['lookback_date']}/{start_time}-{end_time}_records.json"
+    temp_target_path = f"energy/backfill/{args['lookback_date']}/temp_uploaded_blob.json"
 
-        # load each run's data as a unique file
-        json_to_gcs(curr_run_target_path, records, bucket)
-        if first_run:
-            # load the initial run that will be appended
-            json_to_gcs(append_target_path, records, bucket)
-        else:
-            # load the current run's data to gcs as a temp blob to be appended next
-            json_to_gcs(temp_target_path, records, bucket)
+    # load each run's data as a unique file
+    json_to_gcs(curr_run_target_path, records, bucket)
+    if first_run:
+        # load the initial run that will be appended
+        json_to_gcs(append_target_path, records, bucket)
+    else:
+        # load the current run's data to gcs as a temp blob to be appended next
+        json_to_gcs(temp_target_path, records, bucket)
 
-            # append temp_uploaded_blob.json to a growing json of all data in backfill. As of 11/2021 GCP will not combine
-            # more than 32 files, so this operation is performed inside the loop. If fewer files were created overall,
-            # this operation could be moved outside the loop.
-            bucket_obj = storage_client.bucket(bucket)
-            output_file_blob = bucket_obj.blob(append_target_path)
-            output_file_blob.content_type = 'application/ndjson'
-            output_file_blob.compose([bucket_obj.get_blob(append_target_path), bucket_obj.get_blob(temp_target_path)])
+        # append temp_uploaded_blob.json to a growing json of all data in backfill. As of 11/2021 GCP will not combine
+        # more than 32 files, so this operation is performed inside the loop. If fewer files were created overall,
+        # this operation could be moved outside the loop.
+        bucket_obj = storage_client.bucket(bucket)
+        output_file_blob = bucket_obj.blob(append_target_path)
+        output_file_blob.content_type = 'application/ndjson'
+        output_file_blob.compose([bucket_obj.get_blob(append_target_path), bucket_obj.get_blob(temp_target_path)])
+
+    start_time = end_time
