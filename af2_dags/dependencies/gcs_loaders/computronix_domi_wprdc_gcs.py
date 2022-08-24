b5057b0825d98fadbd6c8d4e3b74ec18251825ee
Jesse Wood <woodjt.mail@gmail.com>
2022-08-24 13:26:30 -0400

    next phase of initial draft, has not been tested and may not run fully

--- af2_dags/dependencies/gcs_loaders/computronix_domi_wprdc_gcs.py
+++ af2_dags/dependencies/gcs_loaders/computronix_domi_wprdc_gcs.py
@@ -1,8 +1,28 @@
 import os
 import argparse
+import requests
 
-from gcs_utils import json_to_gcs, select_expand_odata
+from gcs_utils import json_to_gcs
+    #, select_expand_odata
 
+
+def hit_cx_odata_api(odata_url):
+    records = []
+    more_links = True
+    while more_links:
+        res = requests.get(odata_url)
+        records.extend(res.json()['value'])
+
+        if res.status_code != 200:
+            print("API call failed")
+            print(f"Status Code:  {res.status_code}")
+
+        if '@odata.nextLink' in res.json().keys():
+            odata_url = res.json()['@odata.nextLink']
+        else:
+            more_links = False
+
+    return records
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--output_arg', dest = 'out_loc', required = True,
@@ -16,24 +36,26 @@ bucket = f"{os.environ['GCS_PREFIX']}_computronix"
 url = 'https://staff.onestoppgh.pittsburghpa.gov/pghprod/odata/odata/'
 
 
+# PLI permit tables
 bases = ['BUILDINGPERMIT', 'ELECTRICALPERMIT', 'MECHANICALPERMIT']
+
+# nested expansion tables (expansion from each base table)
+xref_1 = "JOBPARCELXREF"
+nested_table_1 = "PARCEL"
+xref_2 = "PARCELPARCELOWNERXREF"
+nested_table_2 = "PARCELOWNER"
+
+# unnested expansion (only on base table)
+unnested_table_1 = "SHADOWJOB"
+
+# fields to select from each table (nt = nested table; unt = unnested table) (there are differently named ID fields
+# for each permit type [ID fields must appear in the same order as the base table])
 fds_base = 'ISSUEDATE, ALLCONTRACTORSNAME, TYPEOFWORKDESCRIPTION, COMMERCIALORRESIDENTIAL, WORKDESCRIPTION, ' \
-            'TOTALPROJECTVALUE' # PERMITNUMBER,
-fds_parcel = 'FORMATTEDPARCELNUMBER, ADDRESSABLEOBJEFORMATTEDADDRES'
-fds_shadowjob = 'SNP_NEIGHBORHOOD, SNP_WARD'
-fds_owner = 'OWNERNAME'
-
-
-
-
-
-
-
-
-
-
-
-
+            'TOTALPROJECTVALUE'
+fds_id = ["PERMITNUMBER", "EXTERNALFILENUM", "EXTERNALFILENUM"]
+fds_nt1 = 'FORMATTEDPARCELNUMBER, ADDRESSABLEOBJEFORMATTEDADDRES'
+fds_nt2 = 'OWNERNAME'
+fds_unt1 = 'SNP_NEIGHBORHOOD, SNP_WARD'
 
 
 # should we do this for eac permit type?
@@ -45,45 +67,36 @@ fds_owner = 'OWNERNAME'
 # the real value from PermitTYpePeermitTYpe
 #
 
+odata_url_base_fields = F"$select={fds_base}"
 
+odata_url_tail = F"&$expand={xref_1}" \
+    "(" \
+        F"$select={nested_table_1},; $expand={nested_table_1}" \
+            "(" \
+                F"$select={fds_nt1},; $expand={xref_2}" \
+                    "(" \
+                        F"$select={nested_table_2},; $expand={nested_table_2}" \
+                            F"($select={fds_nt2})" \
+                    ")" \
+            ")" \
+    ")"
 
-#
-# tables = [
-#              (b, False, [fds_base]),
-#             ("JOBPARCELXREF", ["PARCEL"], ["PARCEL", fds_parcel]),
-#             ("SHADOWJOB", False, [sel_fields[2]]),
-#             ("OWNERNAME", False, [sel_fields[3]])
-#     ]
+for (b, i) in zip(bases, fds_id):
+    odata_url = F"{url}{b}?{odata_url_base_fields}, {i}, {odata_url_tail}"
+    permits = hit_cx_odata_api(odata_url)
+    for p in permits:
+        p.update({"permit_type": b.split("PERMIT")[0]})
 
+odata_url = F"{url}GENERALPERMIT?{odata_url_base_fields}, PERMITTYPEPERMITTYPE, EXTERNALFILENUM, {odata_url_tail}"
+gen_permits = hit_cx_odata_api(odata_url)
+for g in gen_permits:
+    g.update({"permit_type": g["PERMITTYPEPERMITTYPE"]})
+    g.pop("PERMITTYPEPERMITTYPE")
 
-# ("JOBPARCELXREF", ["PARCEL"], ["PARCEL", parcel_fds]),
-
-
-permits = []
-for b in bases:
-    # first tuple refers to the base table & subsequent tuples are expansions
-    tables = [
-            (b, False, [fds_base]),
-            ("JOBPARCELXREF", ["PARCEL"], ["PARCEL", fds_parcel]),
-
-    ]
-
-            #            ("SHADOWJOB", False, [fds_shadowjob])
-
-# ]
-
-
-
-
-
-
-
-    # extract the data from ODATA API
-    permits.append(select_expand_odata(url, tables, limit_results = False))
-
-
+permits.extend(gen_permits)
 
 # load data into GCS
 # out loc = <dataset>/<full date>/<run_id>_str_closures.json
-json_to_gcs(args["out_loc"], permits, bucket)
+# json_to_gcs(args["out_loc"], permits, bucket)
+
 
