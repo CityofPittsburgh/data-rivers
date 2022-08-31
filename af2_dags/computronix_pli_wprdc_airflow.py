0b7cddc5ac76b4f13ff8066b24674d1a972ff762
Jesse Wood <woodjt.mail@gmail.com>
2022-08-31 07:35:55 -0400

    renamed df and gcs loader as well as first draft of af script that hasn't been tested yet

---
+++ af2_dags/computronix_pli_wprdc_airflow.py
@@ -0,0 +1,82 @@
+from __future__ import absolute_import
+
+import os
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
+from airflow.contrib.operators.bigquery_to_gcs import BigQueryToCloudStorageOperator
+
+from dependencies import airflow_utils
+from dependencies.airflow_utils import get_ds_year, get_ds_month, get_ds_day, default_args
+
+# TODO: When Airflow 2.0 is released, upgrade the package, sub in DataFlowPythonOperator for BashOperator,
+# and pass the argument 'py_interpreter=python3'
+
+dag = DAG(
+    'computronix_pli_wprdc',
+    default_args=default_args,
+    schedule_interval='@daily',
+    user_defined_filters={'get_ds_month': get_ds_month, 'get_ds_year': get_ds_year, 'get_ds_day': get_ds_day}
+)
+
+
+# initialize gcs locations
+bucket = f"gs://{os.environ['GCS_PREFIX']}_computronix"
+dataset = "pli_permits_wprdc"
+path = "{{ ds|get_ds_year }}/{{ ds|get_ds_month }}/{{ ds|get_ds_day }}/{{ run_id }}"
+json_loc = f"{path}_pli_permits.json"
+avro_loc = f"avro_output/{path}/"
+
+
+# Run gcs_loader
+exec_gcs = f"python {os.environ['GCS_LOADER_PATH']}/computronix_pli_permits_wprdc.py"
+gcs_loader = BashOperator(
+        task_id = 'gcs_loader',
+        bash_command = f"{exec_gcs} --output_arg {dataset}/{json_loc}",
+        dag = dag
+)
+
+
+exec_df = f"python {os.environ['DATAFLOW_SCRIPT_PATH']}/computronix_pli_permits_wprdc_dataflow.py"
+dataflow = BashOperator(
+        task_id = 'dataflow',
+        bash_command = f"{exec_df} --input {bucket}/{dataset}/{json_loc} --avro_output {bucket}/{dataset}/{avro_loc}",
+        dag = dag
+)
+
+
+# Load AVRO data produced by dataflow_script into BQ temp table
+gcs_to_bq = GoogleCloudStorageToBigQueryOperator(
+        task_id = 'gcs_to_bq',
+        destination_project_dataset_table = f"{os.environ['GCLOUD_PROJECT']}:computronix.pli_permits",
+        bucket = f"{os.environ['GCS_PREFIX']}_computronix",
+        source_objects = [f"{dataset}/{avro_loc}*.avro"],
+        write_disposition = 'WRITE_TRUNCATE',
+        create_disposition = 'CREATE_IF_NEEDED',
+        source_format = 'AVRO',
+        autodetect = True,
+        dag = dag
+)
+
+
+# Export table as CSV to WPRDC bucket
+# file name is the date. path contains the date info
+csv_file_name = f"{path}"
+dest_bucket = f"gs://{os.environ['GCS_PREFIX']}_wprdc/pli/permits/"
+wprdc_export = BigQueryToCloudStorageOperator(
+        task_id = 'wprdc_export',
+        source_project_dataset_table = f"{os.environ['GCLOUD_PROJECT']}.computronix.pli_permits",
+        destination_cloud_storage_uris = [f"{dest_bucket}{csv_file_name}.csv"],
+        dag = dag
+)
+
+
+beam_cleanup = BashOperator(
+    task_id='beam_cleanup',
+    bash_command=airflow_utils.beam_cleanup_statement(f"{os.environ['GCS_PREFIX']}_computronix"),
+    dag=dag
+)
+
+gcs_loader >> dataflow >> gcs_to_bq >> wprdc_export >> beam_cleanup
+
