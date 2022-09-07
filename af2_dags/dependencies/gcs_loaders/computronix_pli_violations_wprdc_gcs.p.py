c878f72849b5f06e220e925da11f9b2f94fa6e91
Jesse Wood <woodjt.mail@gmail.com>
2022-09-07 10:00:26 -0400

    non working code...just making progress

--- af2_dags/dependencies/gcs_loaders/computronix_pli_violations_wprdc_gcs.p.py
+++ af2_dags/dependencies/gcs_loaders/computronix_pli_violations_wprdc_gcs.p.py
@@ -5,11 +5,11 @@ import requests
 from gcs_utils import json_to_gcs
 
 
-def hit_cx_odata_api(url):
+def hit_cx_odata_api(targ_url):
     records = []
     more_links = True
     while more_links:
-        res = requests.get(url)
+        res = requests.get(targ_url)
         records.extend(res.json()['value'])
 
         if res.status_code != 200:
@@ -35,52 +35,35 @@ bucket = f"{os.environ['GCS_PREFIX']}_computronix"
 # CX ODATA API URL base
 url = 'https://staff.onestoppgh.pittsburghpa.gov/pghprod/odata/odata/'
 
-# PLI permit tables
-bases = ["CASEFILE"]
-
-# nested expansion tables (expansion from each base table)
-            # xref_1 = "Y"
-            # nested_table_1 = "Z"
+# PLI tables
+base = "CASEFILE"
 
 # unnested expansion (only on base table)
 unnested_table_1 = "INVESTIGATION"
+unnested_table_2 = "VIOLATION"
+unnested_table_3 = "CASEFILETYPE"
 
 # fields to select from each table (nt = nested table; unt = unnested table)
-fds_base = "FD1"
-            # fds_nt1 = "FD2"
-fds_unt1 = "FD3"
+fds_base = "EXTERNALFILENUM, STATUSDESCRIPTION, ADDRESSFORMATTEDADDRESS, PARCELPARCELNUMBER"
+fds_unt1 = "DATECOMPLETED, OUTCOME, FINDINGS"
+fds_unt2 = "CODESECTION, CODESECTIONTITLE, DESCRIPTION, SPECIALINSTRUCTIONS"
+fds_unt3 = "NAME"
 
-odata_url_date_filter = F"$filter=ISSUEDATE gt 2020-06-01T00:00:00Z"
-
+# build url components
+odata_url_date_filter = F"$filter=DATECOMPLETED gt 2020-06-01T00:00:00Z"
 odata_url_base_fields = F"$select={fds_base}"
 
-odata_url_tail = F"&$expand={xref_1}" \
-    "(" \
-        F"$select={nested_table_1},; $expand={nested_table_1}" \
-            "(" \
-                F"$select={fds_nt1},; $expand={xref_2}" \
-                    "(" \
-                        F"$select={nested_table_2},; $expand={nested_table_2}" \
-                            F"($select={fds_nt2})" \
-                    ")" \
-            ")" \
-    ")"
+odata_url_expansions = F"$expand={unnested_table_1}($select={fds_unt1}),{odata_url_date_filter}, " \
+                 F"expand={unnested_table_2}($select={fds_unt2}), " \
+                 F"expand={unnested_table_3}($select={fds_unt3})"
 
-all_permits = []
-for (b, i) in zip(bases, fds_id):
-    odata_url = F"{url}{b}?{odata_url_date_filter}&{odata_url_base_fields}, {i}, {odata_url_tail}"
-    permits = hit_cx_odata_api(odata_url)
-    for p in permits:
-        p.update({"permit_type": b.split("PERMIT")[0]})
-    if b == bases[0]:
-        for p in permits:
-            p.update({"EXTERNALFILENUM": p["PERMITNUMBER"]})
-            p.pop("PERMITNUMBER")
+odata_url = F"{url}{base}?{odata_url_base_fields},{odata_url_expansions}"
 
-    all_permits.extend(permits)
+# get violations from API
+violations = hit_cx_odata_api(odata_url)
 
 
 # load data into GCS
 # out loc = <dataset>/<full date>/<run_id>_str_closures.json
-json_to_gcs(args["out_loc"], all_violations, bucket)
+json_to_gcs(args["out_loc"], violations, bucket)
 
