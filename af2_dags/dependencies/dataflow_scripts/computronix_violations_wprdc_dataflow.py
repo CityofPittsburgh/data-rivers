15dfb697afa9ef0793c14cea3db6185c41627b13
Jesse Wood <woodjt.mail@gmail.com>
2022-09-09 16:10:06 -0400

    first draft of df script. not tested yet

---
+++ af2_dags/dependencies/dataflow_scripts/computronix_violations_wprdc_dataflow.py
@@ -0,0 +1,58 @@
+from __future__ import absolute_import
+
+import logging
+import os
+
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io.avroio import WriteToAvro
+
+# import util modules.
+# util modules located one level down in directory (./dataflow_util_modules/datflow_utils.py)
+from dataflow_utils.dataflow_utils import JsonCoder, SwapFieldNames, StandardizeTimes, ConvertStringCase, generate_args
+
+DEFAULT_DATAFLOW_ARGS = [
+        '--save_main_session',
+        f"--project={os.environ['GCLOUD_PROJECT']}",
+        f"--service_account_email={os.environ['SERVICE_ACCT']}",
+        f"--region={os.environ['REGION']}",
+        f"--subnetwork={os.environ['SUBNET']}"
+]
+
+
+# run function is called at the bottom of the script and the entire operation is defined within
+# generate_args will initialize all options/args needed to execute the pipeline. known_args contains the runtime
+# params passed in from DAG (input/output). pipeline_options contains all the flags that are initialized by default (
+# project/service_acct/etc.). The schema is loaded as a dict
+def run(argv = None):
+    known_args, pipeline_options, avro_schema = generate_args(
+            job_name = 'computronix-pli-wprdc',
+            bucket = '{}_computronix'.format(os.environ['GCS_PREFIX']),
+            argv = argv,
+            schema_name = 'computronix_pli_wprdc',
+            default_arguments = DEFAULT_DATAFLOW_ARGS
+    )
+
+    with beam.Pipeline(options = pipeline_options) as p:
+
+        str_convs = [("ext_file_num", "upper"), ("investigation_outcome", "upper"), ("investigation_date", "upper"),
+                     ("violation_desc", "upper"), ("status", "upper"), ("investigation_findings", "upper"),
+                     ("violation_code_title", "upper"), ("violation_code_section", "upper"),
+                     ("violation_special_instructions", "upper"), ("work_desc", "upper"), ("parcel_num", "upper"),
+                     ("address", "upper")]
+
+        times = [("investigation_date", "EST")]
+
+        lines = p | ReadFromText(known_args.input, coder = JsonCoder())
+
+        load = (
+                lines
+                | beam.ParDo(ConvertStringCase(str_convs))
+                | beam.ParDo(StandardizeTimes(times, t_format = "%m/%d/%Y"))
+                | WriteToAvro(known_args.avro_output, schema = avro_schema, file_name_suffix = '.avro',
+                              use_fastavro = True))
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()
