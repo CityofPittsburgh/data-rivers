21b12d562a3964785fbdcea85a76129948952bcc
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2022-09-23 11:32:05 -0400

    Handle Ceridian URL Redirects in API Request

--- af2_dags/dependencies/gcs_loaders/ceridian_gcs.py
+++ af2_dags/dependencies/gcs_loaders/ceridian_gcs.py
@@ -3,7 +3,6 @@ import argparse
 import pendulum
 from datetime import datetime, timedelta
 import requests
-from urllib.request import urlopen
 from requests.auth import HTTPBasicAuth
 from google.cloud import storage
 
@@ -22,7 +21,7 @@ args = vars(parser.parse_args())
 yesterday = datetime.combine(datetime.now(tz = pendulum.timezone("utc")) - timedelta(1),
                              datetime.min.time()).strftime("%Y-%m-%d %H:%M:%S")
 
-BASE_URL = f"{os.environ['CERIDIAN_URL_PREFIX']}.dayforcehcm.com/Api/{os.environ['CERIDIAN_ORG_ID']}/V1/Reports/APIINFO"
+BASE_URL = f"https://www.dayforcehcm.com/Api/{os.environ['CERIDIAN_ORG_ID']}/V1/Reports/APIINFO"
 
 auth = HTTPBasicAuth(os.environ['CERIDIAN_USER'], os.environ['CERIDIAN_PW'])
 payload = {'pageSize': API_LIMIT}
@@ -34,17 +33,20 @@ run_start_win, first_run = find_last_successful_run(bucket, "employees/successfu
 
 all_records = []
 url = BASE_URL
+s = requests.session()
 more = True
 while more is True:
     # API call to get data
-    response = requests.get(url, auth=auth, params=payload)
+    response_1 = s.get(url, auth=auth, params=payload)
+    redir_url = response_1.url
+    response_2 = s.get(redir_url, auth=auth, params=payload)
     curr_run = datetime.now(tz = pendulum.timezone('UTC')).strftime("%Y-%m-%d %H:%M:%S")
-    print("Response at " + str(curr_run) + ": " + str(response.status_code))
+    print("Response at " + str(curr_run) + ": " + str(response_2.status_code))
 
-    employees = response.json()['Data']['Rows']
+    employees = response_2.json()['Data']['Rows']
     # continue looping through records until the API has a MoreFlag value of 0
-    if response.json()['Paging']['Next']:
-        url = response.json()['Paging']['Next']
+    if response_2.json()['Paging']['Next']:
+        url = response_2.json()['Paging']['Next']
     else:
         more = False
     # append list of API results to growing all_records list (should not need more than initial API call)
