e78aab0b3b392e83863aa990c4768c0502d23dc0
jasonfic <43254577+jasonfic@users.noreply.github.com>
2022-10-10 11:11:35 -0400

    Backfill Missing 311 Records and Add in PII Comments Field (#213)
    * Add Stages of Qalert Backfill DAG that Avoid Incurring Google Maps API Costs
    
    * Parse Datetime from String:
    
    * Re-Integrate PII Comments into Qalert Analysis Tables
    
    * Remove References to Google Maps Fields from Backfill Dataflow
    
    * Add Temporary Hardcoded Bucket Path for Qalert Backfill
    
    Co-authored-by: Jason Ficorilli <ip.analytics@pittsburghpa.gov>

--- af2_dags/qalert_backfill_airflow.py
+++ af2_dags/qalert_backfill_airflow.py
@@ -21,17 +21,17 @@ COLS_IN_ORDER = """id, parent_ticket_id, child_ticket, dept, status_name, status
 request_type_id, origin, pii_comments, anon_comments, pii_private_notes, create_date_est, create_date_utc, 
 create_date_unix, last_action_est, last_action_utc, last_action_unix, closed_date_est, closed_date_utc, 
 closed_date_unix, pii_street_num, street, cross_street, street_id, cross_street_id, city, pii_input_address, 
-pii_google_formatted_address, anon_google_formatted_address, address_type, neighborhood_name, 
-council_district, ward, police_zone, fire_zone, dpw_streets, dpw_enviro, dpw_parks, input_pii_lat, input_pii_long, 
-google_pii_lat, google_pii_long, input_anon_lat, input_anon_long, google_anon_lat, google_anon_long"""
+address_type, neighborhood_name, council_district, ward, police_zone, fire_zone, dpw_streets, dpw_enviro, 
+dpw_parks, input_pii_lat, input_pii_long, input_anon_lat, input_anon_long"""
 
 LINKED_COLS_IN_ORDER = """status_name, status_code, dept, 
 request_type_name, request_type_id, origin, pii_comments, anon_comments, pii_private_notes, create_date_est, 
 create_date_utc, create_date_unix, last_action_est, last_action_utc, last_action_unix, closed_date_est, closed_date_utc, 
 closed_date_unix, pii_street_num, street, cross_street, street_id, cross_street_id, city, pii_input_address, 
-pii_google_formatted_address, anon_google_formatted_address, address_type, neighborhood_name, 
+NULL AS pii_google_formatted_address, NULL AS anon_google_formatted_address, address_type, neighborhood_name, 
 council_district, ward, police_zone, fire_zone, dpw_streets, dpw_enviro, dpw_parks, input_pii_lat, input_pii_long, 
-google_pii_lat, google_pii_long, input_anon_lat, input_anon_long, google_anon_lat, google_anon_long"""
+NULL AS google_pii_lat, NULL AS google_pii_long, input_anon_lat, input_anon_long, 
+NULL AS google_anon_lat, NULL AS google_anon_long"""
 
 EXCLUDE_TYPES = """'Hold - 311', 'Graffiti, Owner Refused DPW Removal', 'Medical Exemption - Tote', 
 'Snow Angel Volunteer', 'Claim form (Law)','Snow Angel Intake', 'Application Request', 'Reject to 311', 'Referral', 
@@ -46,77 +46,63 @@ dpw_enviro, dpw_parks, input_anon_lat, input_anon_long, google_anon_lat, google_
 
 
 dag = DAG(
-        'qalert_requests',
+        'qalert_requests_backfill',
         default_args = default_args,
-        schedule_interval = '@hourly',
-        start_date = datetime(2021, 1, 21),
-        catchup = False,
+        schedule_interval = None,
         user_defined_filters = {'get_ds_month': get_ds_month, 'get_ds_year': get_ds_year,
-                                'get_ds_day': get_ds_day},
-        max_active_runs = 1
+                                'get_ds_day': get_ds_day}
 )
 
-
-# initialize gcs locations
-bucket = f"gs://{os.environ['GCS_PREFIX']}_qalert"
-dataset = "requests"
-
-path = "{{ ds|get_ds_year }}/{{ ds|get_ds_month }}/{{ ds|get_ds_day }}/{{ run_id }}"
-
-json_loc = f"{path}_requests.json"
-avro_loc = f"avro_output/{path}/"
-
+path = "{{ ds|get_ds_year }}-{{ ds|get_ds_month }}-07"
 
 # Run gcs_loader
-exec_gcs = f"python {os.environ['GCS_LOADER_PATH']}/qalert_requests_gcs.py"
 gcs_loader = BashOperator(
-        task_id = 'gcs_loader',
-        bash_command = f"{exec_gcs} --output_arg {dataset}/{json_loc}",
-        dag = dag
+    task_id='gcs_loader',
+    bash_command=F"python {os.environ['DAGS_PATH']}/dependencies/gcs_loaders/qalert_backfill_gcs.py",
+    dag=dag
 )
 
-
-exec_df = f"python {os.environ['DATAFLOW_SCRIPT_PATH']}/qalert_requests_dataflow.py"
+# Run dataflow_script
+py_cmd = f"python {os.environ['DAGS_PATH']}/dependencies/dataflow_scripts/qalert_backfill_dataflow.py"
+in_cmd = \
+    f" --input gs://{os.environ['GCS_PREFIX']}_qalert/requests/backfill/{path}/backfilled_requests.json"
+out_cmd = f" --avro_output gs://{os.environ['GCS_PREFIX']}_qalert/requests/backfill/{path}/avro_output/"
+df_cmd_str = py_cmd + in_cmd + out_cmd
 dataflow = BashOperator(
-        task_id = 'dataflow',
-        bash_command = f"{exec_df} --input {bucket}/{dataset}/{json_loc} --avro_output {bucket}/{dataset}/{avro_loc}",
-        dag = dag
+    task_id='dataflow',
+    bash_command=df_cmd_str,
+    dag=dag
 )
 
 # Load AVRO data produced by dataflow_script into BQ temp table
 gcs_to_bq = GoogleCloudStorageToBigQueryOperator(
-        task_id = 'gcs_to_bq',
-        destination_project_dataset_table = f"{os.environ['GCLOUD_PROJECT']}:qalert.incoming_actions",
-        bucket = f"{os.environ['GCS_PREFIX']}_qalert",
-        source_objects = [f"{dataset}/{avro_loc}*.avro"],
-        write_disposition = 'WRITE_TRUNCATE',
-        create_disposition = 'CREATE_IF_NEEDED',
-        source_format = 'AVRO',
-        autodetect = True,
-        bigquery_conn_id='google_cloud_default',
-        dag = dag
+    task_id = 'gcs_to_bq',
+    destination_project_dataset_table = f"{os.environ['GCLOUD_PROJECT']}:qalert.incoming_backfill",
+    bucket = f"{os.environ['GCS_PREFIX']}_qalert",
+    source_objects=[f"requests/backfill/{path}/avro_output/*.avro"],
+    write_disposition = 'WRITE_TRUNCATE',
+    create_disposition = 'CREATE_IF_NEEDED',
+    source_format = 'AVRO',
+    autodetect = True,
+    bigquery_conn_id='google_cloud_default',
+    dag = dag
 )
 
 # Update geo coords with lat/long cast as floats (dataflow/AVRO glitch forces them to be output as strings; the
 # source of the error is instrinsic to dataflow and may not be fixable). Also, dedupe the results (someties the same
 # ticket appears in the computer system more than 1 time (a QAlert glitch)
 query_format_dedupe = f"""
-CREATE OR REPLACE TABLE `{os.environ['GCLOUD_PROJECT']}.qalert.incoming_actions` AS
+CREATE OR REPLACE TABLE `{os.environ['GCLOUD_PROJECT']}.qalert.incoming_backfill` AS
 WITH formatted  AS 
     (
     SELECT 
-        DISTINCT * EXCEPT (input_pii_lat, input_pii_long, google_pii_lat, google_pii_long, 
-                           input_anon_lat, input_anon_long, google_anon_lat, google_anon_long),
+        DISTINCT * EXCEPT (input_pii_lat, input_pii_long, input_anon_lat, input_anon_long),
         CAST(input_pii_lat AS FLOAT64) AS input_pii_lat,
         CAST(input_pii_long AS FLOAT64) AS input_pii_long,
-        CAST(google_pii_lat AS FLOAT64) AS google_pii_lat,
-        CAST(google_pii_long AS FLOAT64) AS google_pii_long,
         CAST(input_anon_lat AS FLOAT64) AS input_anon_lat,
-        CAST(input_anon_long AS FLOAT64) AS input_anon_long,
-        CAST(google_anon_lat AS FLOAT64) AS google_anon_lat,
-        CAST(google_anon_long AS FLOAT64) AS google_anon_long,
+        CAST(input_anon_long AS FLOAT64) AS input_anon_long
     FROM 
-        {os.environ['GCLOUD_PROJECT']}.qalert.incoming_actions
+        {os.environ['GCLOUD_PROJECT']}.qalert.incoming_backfill
     )
 -- drop the final column through slicing the string (-13). final column is added in next query     
 SELECT 
@@ -133,7 +119,7 @@ format_dedupe = BigQueryOperator(
 )
 
 # Query new tickets to determine if they are in the city limits
-query_city_lim = build_city_limits_query('qalert', 'incoming_actions', 'input_pii_lat', 'input_pii_long')
+query_city_lim = build_city_limits_query('qalert', 'incoming_backfill', 'input_pii_lat', 'input_pii_long')
 city_limits = BigQueryOperator(
         task_id = 'city_limits',
         sql = query_city_lim,
@@ -146,7 +132,7 @@ city_limits = BigQueryOperator(
 #  for clearer explanation)
 # FINAL ENRICHMENT OF NEW DATA
 # Join all the geo information (e.g. DPW districts, etc) to the new data
-query_geo_join = build_revgeo_time_bound_query('qalert', 'incoming_actions', 'incoming_enriched',
+query_geo_join = build_revgeo_time_bound_query('qalert', 'incoming_backfill', 'backfill_enriched',
                                                'create_date_utc', 'id', 'input_pii_lat', 'input_pii_long')
 geojoin = BigQueryOperator(
         task_id = 'geojoin',
@@ -176,7 +162,7 @@ SELECT
     {LINKED_COLS_IN_ORDER}
 
 FROM
-    `{os.environ['GCLOUD_PROJECT']}.qalert.incoming_enriched`
+    `{os.environ['GCLOUD_PROJECT']}.qalert.backfill_enriched`
 WHERE id NOT IN (SELECT id FROM `{os.environ['GCLOUD_PROJECT']}.qalert.all_tickets_current_status`)
 AND child_ticket = False
 AND request_type_name NOT IN ({EXCLUDE_TYPES})
@@ -207,7 +193,7 @@ along with the other child tickets
 CREATE OR REPLACE TABLE `{os.environ['GCLOUD_PROJECT']}.qalert.temp_prev_parent_now_child` AS
 SELECT 
     id AS fp_id, parent_ticket_id, anon_comments, pii_private_notes
-FROM `{os.environ['GCLOUD_PROJECT']}.qalert.incoming_enriched`
+FROM `{os.environ['GCLOUD_PROJECT']}.qalert.backfill_enriched`
 WHERE id IN (SELECT 
                 group_id
              FROM`{os.environ['GCLOUD_PROJECT']}.qalert.all_linked_requests`)
@@ -251,7 +237,7 @@ CREATE OR REPLACE TABLE `{os.environ['GCLOUD_PROJECT']}.qalert.temp_child_combin
     SELECT
         id, parent_ticket_id, anon_comments, pii_private_notes
     FROM
-        `{os.environ['GCLOUD_PROJECT']}.qalert.incoming_enriched` new_c
+        `{os.environ['GCLOUD_PROJECT']}.qalert.backfill_enriched` new_c
     WHERE new_c.id NOT IN (SELECT id FROM `{os.environ['GCLOUD_PROJECT']}.qalert.all_tickets_current_status`)
     AND new_c.child_ticket = TRUE
     AND new_c.request_type_name NOT IN ({EXCLUDE_TYPES})
@@ -355,7 +341,7 @@ SELECT
     closed_date_est, closed_date_utc,closed_date_unix,
     last_action_est, last_action_utc,last_action_unix,
     status_name, status_code
-FROM  `{os.environ['GCLOUD_PROJECT']}.qalert.incoming_enriched`
+FROM  `{os.environ['GCLOUD_PROJECT']}.qalert.backfill_enriched`
 
 WHERE id IN (SELECT id FROM `{os.environ['GCLOUD_PROJECT']}.qalert.all_tickets_current_status`)
 AND child_ticket = FALSE
@@ -431,11 +417,11 @@ analysis as the linkages between tickets are not taken into account.
 */
 
 DELETE FROM `{os.environ['GCLOUD_PROJECT']}.qalert.all_tickets_current_status`
-WHERE id IN (SELECT id FROM `{os.environ['GCLOUD_PROJECT']}.qalert.incoming_enriched`);
+WHERE id IN (SELECT id FROM `{os.environ['GCLOUD_PROJECT']}.qalert.backfill_enriched`);
 INSERT INTO `{os.environ['GCLOUD_PROJECT']}.qalert.all_tickets_current_status`
 SELECT 
     {COLS_IN_ORDER}
-FROM `{os.environ['GCLOUD_PROJECT']}.qalert.incoming_enriched`;
+FROM `{os.environ['GCLOUD_PROJECT']}.qalert.backfill_enriched`;
 """
 delete_old_insert_new_records = BigQueryOperator(
         task_id = 'delete_old_insert_new_records',
@@ -443,6 +429,53 @@ delete_old_insert_new_records = BigQueryOperator(
         bigquery_conn_id='google_cloud_default',
         use_legacy_sql = False,
         dag = dag
+)
+
+query_add_pii_comments = f"""
+CREATE OR REPLACE TABLE `{os.environ['GCLOUD_PROJECT']}.qalert.all_linked_requests` AS
+SELECT 
+    alr.group_id, alr.child_ids, alr.num_requests, alr.parent_closed, alr.status_name, 
+    alr.status_code, alr.dept, alr.request_type_name, alr.request_type_id, alr.origin,
+    bck.pii_comments, alr.anon_comments, alr.pii_private_notes, alr.create_date_est, 
+    alr.create_date_utc, alr.create_date_unix, alr.last_action_est, alr.last_action_utc, 
+    alr.last_action_unix, alr.closed_date_est, alr.closed_date_utc, alr.closed_date_unix, 
+    alr.pii_street_num, alr.street, alr.cross_street, alr.street_id, alr.cross_street_id, 
+    alr.city, alr.pii_input_address, alr.pii_google_formatted_address, 
+    alr.anon_google_formatted_address, alr.address_type, alr.neighborhood_name, 
+    alr.council_district, alr.ward, alr.police_zone, alr.fire_zone, alr.dpw_streets, 
+    alr.dpw_enviro, alr.dpw_parks, alr.pii_lat AS google_pii_lat, alr.pii_long AS google_pii_long, 
+    alr.anon_lat AS google_anon_lat, alr.anon_long AS google_anon_long,
+    alr.input_pii_lat AS input_pii_lat, alr.input_pii_long AS input_pii_long,
+    alr.input_anon_lat AS input_anon_lat, alr.input_anon_long AS input_anon_long
+FROM `{os.environ['GCLOUD_PROJECT']}.qalert.all_linked_requests` alr
+JOIN `{os.environ['GCLOUD_PROJECT']}.qalert.backfill_enriched` bck
+ON alr.group_id = bck.id;
+
+CREATE OR REPLACE TABLE `{os.environ['GCLOUD_PROJECT']}.qalert.all_tickets_current_status` AS
+SELECT 
+    atcs.id, atcs.parent_ticket_id, atcs.child_ticket, atcs.dept, atcs.status_name, 
+    atcs.status_code, atcs.dept, atcs.request_type_name, atcs.request_type_id, atcs.origin,
+    bck.pii_comments, atcs.anon_comments, atcs.pii_private_notes, atcs.create_date_est, 
+    atcs.create_date_utc, atcs.create_date_unix, atcs.last_action_est, atcs.last_action_utc, 
+    atcs.last_action_unix, atcs.closed_date_est, atcs.closed_date_utc, atcs.closed_date_unix, 
+    atcs.pii_street_num, alr.street, atcs.cross_street, atcs.street_id, atcs.cross_street_id, 
+    atcs.city, alr.pii_input_address, atcs.pii_google_formatted_address, 
+    atcs.anon_google_formatted_address, atcs.address_type, atcs.neighborhood_name, 
+    atcs.council_district, alr.ward, atcs.police_zone, atcs.fire_zone, atcs.dpw_streets, 
+    atcs.dpw_enviro, alr.dpw_parks, atcs.pii_lat AS google_pii_lat, atcs.pii_long AS google_pii_long, 
+    atcs.anon_lat AS google_anon_lat, atcs.anon_long AS google_anon_long,
+    atcs.input_pii_lat AS input_pii_lat, atcs.input_pii_long AS input_pii_long,
+    atcs.input_anon_lat AS input_anon_lat, atcs.input_anon_long AS input_anon_long
+FROM `{os.environ['GCLOUD_PROJECT']}.qalert.all_tickets_current_status` atcs
+JOIN `{os.environ['GCLOUD_PROJECT']}.qalert.backfill_enriched` bck
+ON atcs.id = bck.id;
+"""
+add_pii_comments = BigQueryOperator(
+    task_id='add_pii_comments',
+    sql=query_add_pii_comments,
+    bigquery_conn_id='google_cloud_default',
+    use_legacy_sql=False,
+    dag=dag
 )
 
 # Create a table from all_linked_requests that has all columns EXCEPT those that have potential PII. This table is
@@ -486,4 +519,4 @@ beam_cleanup = BashOperator(
 # DAG execution:
 gcs_loader >> dataflow >> gcs_to_bq >> format_dedupe >> city_limits >> geojoin >> insert_new_parent >> \
 remove_false_parents >> integrate_children >> replace_last_update >> delete_old_insert_new_records >> \
-drop_pii_for_export >> wprdc_export >> beam_cleanup
+add_pii_comments >> drop_pii_for_export >> wprdc_export >> beam_cleanup
