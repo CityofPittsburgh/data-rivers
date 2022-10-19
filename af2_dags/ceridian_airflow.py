f71239384c06963f844f66111ea41887bbe9ad26
jasonfic <43254577+jasonfic@users.noreply.github.com>
2022-10-19 13:08:58 -0400

    Reformat Department and Ethnicity Values for Ceridian DAG (#226)
    * Create Ceridian Dayforce Employee DAG with Functioning API Call
    
    * Add Dataflow Script to Make Field Names More Readable
    
    * Alter GCS and BigQuery Locations for Ceridian Data for Ease of Use
    
    * Fix API URL and Loop Mechanics
    
    * Add Small Dataflow Task to Strip Date from Hire Date Timestamp
    
    * Add Tasks to Create SQL Tables for Dashburgh
    
    This commit adds two new bash commands to the airflow script of the
    Ceridian DAG. These commands execute a pair of SQL scripts that generate
    new tables in BigQuery - one to compare the gender demographics of the
    city government workforce to that of the overall city, and another table
    that does the same for racial demographics. These tables are being created
    because data manipulation in BigQuery is easier to work with than data
    manipulation in Power BI.
    
    * Add Mistakenly Excluded SELECT Statement to Gender Comp Query
    
    * Use Environmental Variable to Dynamically Update Ceridian API URL
    
    * Handle Ceridian
    URL Redirects in API Request
    
    * Add Default Start Date to Help Schedule Ceridian DAG
    
    * Add Specialized Util for Ceridian Department Mapping
    
    * Add ParDo Classes to Clean Up Department and Ethnicity Names
    
    * Push Back Ceridian Start Date, Remove SQL Scripts
    
    Co-authored-by: Jason Ficorilli <ip.analytics@pittsburghpa.gov>

--- af2_dags/ceridian_airflow.py
+++ af2_dags/ceridian_airflow.py
@@ -23,7 +23,7 @@ dag = DAG(
     'ceridian',
     default_args=default_args,
     schedule_interval='@weekly',
-    start_date=datetime(2022, 10, 7),
+    start_date=datetime(2022, 10, 21),
     user_defined_filters={'get_ds_month': get_ds_month, 'get_ds_year': get_ds_year,
                           'get_ds_day': get_ds_day}
 )
