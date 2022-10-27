a18769b9d788a181974ba9bdbf29e1268f594522
Jesse Wood <woodjt.mail@gmail.com>
2022-10-27 11:42:49 -0400

    first draft to run in drt

--- af2_dags/qalert_gis_airflow.py
+++ af2_dags/qalert_gis_airflow.py
@@ -8,9 +8,6 @@ from dependencies import airflow_utils
 from dependencies.airflow_utils import get_ds_month, get_ds_year, default_args
 
 
-
-
-
 # The goal of this DAG is to push a copy of the 311 all_linked_requests to data-bridGIS for the GIS team to build
 # maps off the pipeline
 
@@ -26,11 +23,15 @@ dag = DAG(
 push_table = BigQueryOperator(
         task_id = 'format_street_tix',
         sql = F"""
-        CREATE OR REPLACE TABLE `data-bridgis.qalert.all_linked_requests` AS
-        SELECT
-            * EXCEPT (input_pii_long, input_pii_lat),
-            ST_GEOGPOINT(input_pii_long, input_pii_lat) as geography
-        FROM `{os.environ['GLCOUD_PROJECT']}.qalert.all_linked_requests`
+       CREATE OR REPLACE TABLE `data-bridgis.qalert.all_linked_requests` AS
+        SELECT 
+                group_id, child_ids, num_requests, parent_closed, status_name, dept,  request_type_name, origin, address_type,
+                neighborhood_name, council_district, ward, police_zone, fire_zone, dpw_streets, dpw_enviro, dpw_parks,
+                DATE(create_date_est) AS create_date,
+                DATE(last_action_est) AS last_update,
+                DATE(closed_date_est) AS closed_date,
+                ST_GEOGPOINT(google_pii_long, google_pii_lat) AS geo
+        FROM `data-rivers.qalert.all_linked_requests`
         """,
         bigquery_conn_id='google_cloud_default',
         use_legacy_sql = False,
