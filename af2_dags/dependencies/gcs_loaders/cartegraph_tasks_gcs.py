2a53ccb9940a960bbc90c34b927d52f6682262ba
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2022-10-27 14:32:16 -0400

    Set Cap At 50K Cartegraph Records to Prevent Crashes, Use Ticking Offset Rather than Date for New DAG Runs

--- af2_dags/dependencies/gcs_loaders/cartegraph_tasks_gcs.py
+++ af2_dags/dependencies/gcs_loaders/cartegraph_tasks_gcs.py
@@ -8,7 +8,11 @@ from google.cloud import storage
 
 from gcs_utils import json_to_gcs, find_last_successful_run
 
-API_LIMIT = 5000
+# the first recorded entry date
+DEFAULT_RUN_START = "2015-08-12 12:00:00 AM"
+
+API_LIMIT = 1000
+OFFSET_CAP = 50000
 
 storage_client = storage.Client()
 bucket = f"{os.environ['GCS_PREFIX']}_cartegraph"
@@ -18,47 +22,50 @@ parser.add_argument('--output_arg', dest='out_loc', required=True,
                     help='fully specified location to upload the combined ndjson file')
 args = vars(parser.parse_args())
 
-yesterday = datetime.combine(datetime.now(tz = pendulum.timezone("utc")) - timedelta(1),
-                             datetime.min.time()).strftime("%Y-%m-%d %H:%M:%S")
-
-BASE_URL = f"https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/classes/cgTasksClass"
-
-auth = HTTPBasicAuth(os.environ['CARTEGRAPH_USER'], os.environ['CARTEGRAPH_PW'])
-sort = 'EntryDateField:asc'
-
 # find the last successful DAG run (needs to be specified in UTC YYYY-MM-DD HH:MM:SS)
 # if there was no previous good run, default to yesterday's date.
 # this is used to initialize the payload below
 offset, first_run = find_last_successful_run(bucket, "tasks/successful_run_log/log.json", 0)
 
+BASE_URL = f"https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/classes/cgTasksClass"
+
+auth = HTTPBasicAuth(os.environ['CARTEGRAPH_USER'], os.environ['CARTEGRAPH_PW'])
+sort = 'EntryDateField:asc'
+fields = 'Oid,DepartmentField,StatusField,EntryDateField,StartDateActualField,StopDateActualField,LaborCostActualField,' \
+         'EquipmentCostActualField,MaterialCostActualField,LaborHoursActualField,RequestIssueField,RequestDepartmentField,' \
+         'RequestLocationField,cgAssetIDField,cgAssetTypeField,TaskDescriptionField,NotesField,CgShape'
+
 all_records = []
 more = True
 while more is True:
-    payload = {'sort': sort, 'limit': API_LIMIT, 'offset': offset}
+    payload = {'fields': fields, 'sort': sort, 'limit': API_LIMIT, 'offset': offset}
     # API call to get data
     response = requests.get(BASE_URL, auth=auth, params=payload)
-    curr_run = datetime.now(tz = pendulum.timezone('UTC')).strftime("%Y-%m-%d %H:%M:%S")
-    print("Response at " + str(curr_run) + ": " + str(response.status_code))
+    curr_run = datetime.now(tz = pendulum.timezone('US/Eastern')).strftime("%Y-%m-%d %I:%M:%S %p")
+    print("Response at " + str(curr_run) + " with offset value " + str(offset) + ": " + str(response.status_code))
 
     tasks = response.json()['cgTasksClass']
     total = int(response.json()['_metadata']['totalCount'])
     diff = total - int(offset)
-    # continue looping through records until we have captured the total count of records
-    if diff < API_LIMIT:
-        more = False
-    else:
-        offset = int(offset) + API_LIMIT
+
     # append list of API results to growing all_records list
     all_records += tasks
 
-    # write the successful run information (used by each successive run to find the backfill start date)
-    successful_run = {
-        "requests_retrieved": len(tasks),
-        "since": curr_run,
-        "current_run": offset,
-        "note": "Final chunk of data retrieved using the offset value listed above"
-    }
-    json_to_gcs("tasks/successful_run_log/log.json", [successful_run],
-                bucket)
+    # continue looping through records until we have captured the total count of records
+    if (diff <= API_LIMIT) or (len(all_records) >= OFFSET_CAP):
+        more = False
+    else:
+        offset = int(offset) + API_LIMIT
+
+
+# write the successful run information (used by each successive run to find the backfill start date)
+successful_run = {
+    "requests_retrieved": len(all_records),
+    "run_date": curr_run,
+    "current_run": offset,
+    "note": "Data extraction left off with the offset value listed above"
+}
 
 json_to_gcs(f"{args['out_loc']}", all_records, bucket)
+
+json_to_gcs("tasks/successful_run_log/log.json", [successful_run], bucket)
