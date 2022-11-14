4ee028dde686d216557d7dfe71a3eb855d08e688
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2022-11-14 16:33:54 -0500

    Use New Department Column Name in Deletion Query

--- af2_dags/ceridian_airflow.py
+++ af2_dags/ceridian_airflow.py
@@ -67,8 +67,7 @@ ceridian_bq_load = GoogleCloudStorageToBigQueryOperator(
 # their benefits administered by the City of Pittsburgh and should not be reflected in any employee data
 query_remove_rows = f"""
 DELETE FROM `{os.environ['GCLOUD_PROJECT']}.ceridian.active_employees` 
-WHERE department = '' OR department IS NULL OR department = 'Historical' OR department = 'Terminated' 
-OR department = 'Non-Employee Benefits'"""
+WHERE dept = '' OR dept IS NULL OR dept = 'Historical' OR dept = 'Terminated' OR dept = 'Non-Employee Benefits'"""
 remove_non_employees = BigQueryOperator(
         task_id = 'remove_non_employees',
         sql = query_remove_rows,
