a262164c3b0f6123c9be5c6d7c88edfcb2aa42b3
Jesse Wood <woodjt.mail@gmail.com>
2022-11-28 12:01:48 -0500

    first draft of gcs loader and dag. not tested yet

---
+++ af2_dags/dependencies/gcs_loaders/computronix_shadow_jobs_gcs.py
@@ -0,0 +1,26 @@
+import os
+import argparse
+
+from gcs_utils import call_odata_api, json_to_gcs
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output_arg', dest = 'out_loc', required = True,
+                    help = 'fully specified location to upload the json file')
+args = vars(parser.parse_args())
+
+bucket = f"{os.environ['GCS_PREFIX']}_computronix"
+
+
+# There is currently (11/22) an error in the CX system where the SHADOWJOB entity has no navigation property
+# connecting to the various permit tables. Since several fields in the SHADOWJOB entity need to be joined into the
+# other CX tables, the entire table will be pulled here and inner joined in BigQuery in a subsequent step
+# CX ODATA API URL base
+url = 'https://staff.onestoppgh.pittsburghpa.gov/pghprod/odata/odata/'
+shadow_url = F"{url}SHADOWJOB?)"
+shadow_jobs = call_odata_api(shadow_url)
+
+
+# load data into GCS
+# out loc = <dataset>/<full date>/<run_id>_shadow_jobs.json
+json_to_gcs(args["out_loc"], shadow_jobs, bucket)
