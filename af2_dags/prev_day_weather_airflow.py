746f37032ab655e643c761b9ee05e74a8811a7f2
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2022-12-29 15:11:08 -0500

    XCommercialize Weather DAG

--- af2_dags/prev_day_weather_airflow.py
+++ af2_dags/prev_day_weather_airflow.py
@@ -5,7 +5,7 @@ import os
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 from dependencies import airflow_utils
 from dependencies.airflow_utils import get_prev_ds_month, get_prev_ds_year, default_args
@@ -25,35 +25,48 @@ dag = DAG(
     user_defined_filters={'get_prev_ds_month': get_prev_ds_month, 'get_prev_ds_year': get_prev_ds_year}
 )
 
-# initialize gcs locations
-dataset = "weather"
-bucket = f"{os.environ['GCS_PREFIX']}_{dataset}"
-path = "prev_day_weather/{{ prev_ds|get_prev_ds_year }}/{{ prev_ds|get_prev_ds_month }}"
+def init_cmds_xcomm(**kwargs):
+    # initialize gcs locations
+    dataset = "weather"
+    kwargs['ti'].xcom_push(key="prev_day_weather_gsc", value=f"python {os.environ['GCS_LOADER_PATH']}"
+                 "/prev_day_weather_gcs.py --lookback_date {{ prev_ds }}")
+    kwargs['ti'].xcom_push(key="dest_proj_ds_table", value=f"{os.environ['GCLOUD_PROJECT']}.{dataset}.daily_weather")
+    bucket = f"{os.environ['GCS_PREFIX']}_{dataset}"
+    kwargs['ti'].xcom_push(key="bucket", value=bucket)
+    path = "prev_day_weather/{{ prev_ds|get_prev_ds_year }}/{{ prev_ds|get_prev_ds_month }}"
+    kwargs['ti'].xcom_push(key="source_objects", value=[f"{path}/"+"{{ prev_ds }}_weather_report.avro"])
+    kwargs['ti'].xcom_push(key="beam_cleanup", value=airflow_utils.beam_cleanup_statement(f"{os.environ['GCS_PREFIX']}_{dataset}"))
+
+
+push_xcom = PythonOperator(
+    task_id='push_xcom',
+    python_callable=init_cmds_xcomm,
+    dag=dag
+)
 
 prev_day_weather_gcs = BashOperator(
     task_id='prev_day_weather_gcs',
-    bash_command=f"python {os.environ['GCS_LOADER_PATH']}"
-                 "/prev_day_weather_gcs.py --lookback_date {{ prev_ds }}",
+    bash_command=str("{{ ti.xcom_pull(task_ids='push_xcom', key='prev_day_weather_gsc') }}"),
     dag=dag
 )
 
 prev_day_weather_bq_load = GoogleCloudStorageToBigQueryOperator(
-        task_id = 'prev_day_weather_bq_load',
-        destination_project_dataset_table = f"{os.environ['GCLOUD_PROJECT']}.{dataset}.daily_weather",
-        bucket = bucket,
-        source_objects = [f"{path}/"+"{{ prev_ds }}_weather_report.avro"],
-        bigquery_conn_id='google_cloud_default',
-        write_disposition='WRITE_APPEND',
-        create_disposition='CREATE_IF_NEEDED',
-        source_format = 'AVRO',
-        autodetect = True,
-        dag = dag
+    task_id='prev_day_weather_bq_load',
+    destination_project_dataset_table=str("{{ ti.xcom_pull(task_ids='push_xcom', key='dest_proj_ds_table') }}"),
+    bucket=str("{{ ti.xcom_pull(task_ids='push_xcom', key='bucket') }}"),
+    source_objects=str("{{ ti.xcom_pull(task_ids='push_xcom', key='source_objects') }}"),
+    bigquery_conn_id='google_cloud_default',
+    write_disposition='WRITE_APPEND',
+    create_disposition='CREATE_IF_NEEDED',
+    source_format='AVRO',
+    autodetect=True,
+    dag=dag
 )
 
 beam_cleanup = BashOperator(
-        task_id = 'prev_day_weather_beam_cleanup',
-        bash_command = airflow_utils.beam_cleanup_statement(f"{os.environ['GCS_PREFIX']}_prev_day_weather"),
-        dag = dag
+    task_id = 'prev_day_weather_beam_cleanup',
+    bash_command=str("{{ ti.xcom_pull(task_ids='push_xcom', key='beam_cleanup') }}"),
+    dag = dag
 )
 
-prev_day_weather_gcs >> prev_day_weather_bq_load >> beam_cleanup
+push_xcom >> prev_day_weather_gcs >> prev_day_weather_bq_load >> beam_cleanup
