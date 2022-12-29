489e8bc2c17d34d34bf42635812a760aeb83e769
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2022-12-29 15:37:09 -0500

    Account For Airflow Template Variables Being Inaccessible in Xcom Definition

--- af2_dags/prev_day_weather_airflow.py
+++ af2_dags/prev_day_weather_airflow.py
@@ -29,13 +29,11 @@ def init_cmds_xcomm(**kwargs):
     # initialize gcs locations
     dataset = "weather"
     kwargs['ti'].xcom_push(key="prev_day_weather_gsc", value=f"python {os.environ['GCS_LOADER_PATH']}"
-                 "/prev_day_weather_gcs.py --lookback_date {{ prev_ds }}")
+                 "/prev_day_weather_gcs.py --lookback_date")
     kwargs['ti'].xcom_push(key="dest_proj_ds_table", value=f"{os.environ['GCLOUD_PROJECT']}.{dataset}.daily_weather")
     bucket = f"{os.environ['GCS_PREFIX']}_{dataset}"
     kwargs['ti'].xcom_push(key="bucket", value=bucket)
-    path = "prev_day_weather/{{ prev_ds|get_prev_ds_year }}/{{ prev_ds|get_prev_ds_month }}"
-    kwargs['ti'].xcom_push(key="source_objects", value=[f"{path}/"+"{{ prev_ds }}_weather_report.avro"])
-    kwargs['ti'].xcom_push(key="beam_cleanup", value=airflow_utils.beam_cleanup_statement(f"{os.environ['GCS_PREFIX']}_{dataset}"))
+    kwargs['ti'].xcom_push(key="beam_cleanup", value=airflow_utils.beam_cleanup_statement(bucket))
 
 
 push_xcom = PythonOperator(
@@ -46,7 +44,7 @@ push_xcom = PythonOperator(
 
 prev_day_weather_gcs = BashOperator(
     task_id='prev_day_weather_gcs',
-    bash_command=str("{{ ti.xcom_pull(task_ids='push_xcom', key='prev_day_weather_gsc') }}"),
+    bash_command=str("{{ ti.xcom_pull(task_ids='push_xcom', key='prev_day_weather_gsc') }}") + " {{ prev_ds }}",
     dag=dag
 )
 
@@ -54,7 +52,8 @@ prev_day_weather_bq_load = GoogleCloudStorageToBigQueryOperator(
     task_id='prev_day_weather_bq_load',
     destination_project_dataset_table=str("{{ ti.xcom_pull(task_ids='push_xcom', key='dest_proj_ds_table') }}"),
     bucket=str("{{ ti.xcom_pull(task_ids='push_xcom', key='bucket') }}"),
-    source_objects=str("{{ ti.xcom_pull(task_ids='push_xcom', key='source_objects') }}"),
+    source_objects=["prev_day_weather/{{ prev_ds|get_prev_ds_year }}/{{ prev_ds|get_prev_ds_month }}/" +
+                    "{{ prev_ds }}_weather_report.avro"],
     bigquery_conn_id='google_cloud_default',
     write_disposition='WRITE_APPEND',
     create_disposition='CREATE_IF_NEEDED',
