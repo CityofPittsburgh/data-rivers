c9da85fe689009f365b1560147cd07b166f689ba
Jesse Wood <woodjt.mail@gmail.com>
2023-05-15 12:48:08 -0400

    added support for geojoining

--- af2_dags/eproperty_vacant_properties_gis_wprdc_airflow.py
+++ af2_dags/eproperty_vacant_properties_gis_wprdc_airflow.py
@@ -10,7 +10,8 @@ from airflow.contrib.operators.bigquery_to_gcs import BigQueryToCloudStorageOper
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 
 from dependencies import airflow_utils
-from dependencies.airflow_utils import get_ds_year, get_ds_month, get_ds_day, default_args
+from dependencies.airflow_utils import get_ds_year, get_ds_month, get_ds_day, default_args, \
+    build_revgeo_time_bound_query
 
 # TODO: When Airflow 2.0 is released, upgrade the package, sub in DataFlowPythonOperator for BashOperator,
 # and pass the argument 'py_interpreter=python3'
@@ -18,7 +19,7 @@ from dependencies.airflow_utils import get_ds_year, get_ds_month, get_ds_day, de
 dag = DAG(
     'eprop_vacant_gis_wprdc',
     default_args=default_args,
-    schedule_interval='@weekly',
+    schedule_interval='@daily',
     user_defined_filters={'get_ds_month': get_ds_month, 'get_ds_year': get_ds_year, 'get_ds_day': get_ds_day},
     start_date=datetime(2023, 5, 1),
     catchup = False
@@ -27,41 +28,49 @@ dag = DAG(
 
 # initialize gcs locations
 bucket = f"gs://{os.environ['GCS_PREFIX']}_eproperty"
-dataset = "eprop_vacant_props_gis_wprdc"
 path = "{{ ds|get_ds_year }}/{{ ds|get_ds_month }}/{{ ds|get_ds_day }}/{{ run_id }}"
-json_loc = f"{path}_eproperty_vacant_properties.json"
-avro_loc = f"avro_output/{path}/"
+json_loc = "{{ ds|get_ds_month }}/{{ ds|get_ds_day }}" + "_vacant_properties.json"
 
 
 # Run gcs_loader
 exec_gcs = f"python {os.environ['GCS_LOADER_PATH']}/eprop_vacant_properties_gis_wprdc_gcs.py"
 gcs_loader = BashOperator(
         task_id = 'gcs_loader',
-        bash_command = f"{exec_gcs} --output_arg {dataset}/{json_loc}",
+        bash_command = f"{exec_gcs} --json_output_arg {json_loc}",
         dag = dag
 )
 
 
-exec_df = f"python {os.environ['DATAFLOW_SCRIPT_PATH']}/eprop_vacant_properties_gis_wprdc_dataflow.py"
-dataflow = BashOperator(
-        task_id = 'dataflow',
-        bash_command = f"{exec_df} --input {bucket}/{dataset}/{json_loc} --avro_output {bucket}/{dataset}/{avro_loc}",
-        dag = dag
-)
-
-
-# Load AVRO data produced by dataflow_script into BQ temp table
+# Load AVRO data produced by gcs_loader (which creates avro directly) into BQ table
 # TODO: use AF2 operator when we convert --> gcs_to_bq = GCSToBigQueryOperator(
 gcs_to_bq = GoogleCloudStorageToBigQueryOperator(
         task_id = 'gcs_to_bq',
         destination_project_dataset_table = f"{os.environ['GCLOUD_PROJECT']}:eproperty.vacant_properties",
-        bucket = f"{os.environ['GCS_PREFIX']}_eproperty",
-        source_objects = [f"{dataset}/{avro_loc}*.avro"],
+        bucket = f"{os.environ['GCS_PREFIX']}_hot_metal",
+        source_objects = ["eproperties.avro"],
         write_disposition = 'WRITE_TRUNCATE',
         create_disposition = 'CREATE_IF_NEEDED',
         source_format = 'AVRO',
         autodetect = True,
-        time_partitioning={'type': 'MONTH'},
+        dag = dag
+)
+
+# reverse geocode
+query_geo_join = build_revgeo_time_bound_query('eproperty', 'vacant_properties', 'vacant_properties_enriched',
+                                             'status_date', 'id', 'latitude', 'longitude')
+geojoin = BigQueryOperator(
+        task_id = 'geojoin',
+        sql = query_geo_join,
+        bigquery_conn_id='google_cloud_default',
+        use_legacy_sql = False,
+        dag = dag
+)
+
+
+# delete the temporary avro in hot storage
+delete_avro = BashOperator(
+        task_id = 'delete_avro',
+        bash_command = F"gsutil rm -r gs://{os.environ['GCS_PREFIX']}_hot_metal/eproperties*",
         dag = dag
 )
 
@@ -80,7 +89,7 @@ wprdc_export = BigQueryToCloudStorageOperator(
 
 # push table of ALL properties data-bridGIS BQ
 query_push_gis = F"""
-CREATE OR REPLACE TABLE `data-bridgis.eproperty.gis_vacant_properties` AS 
+CREATE OR REPLACE TABLE `data-bridgis.eproperty.gis_vacant_properties_enriched` AS 
 SELECT 
   *
 FROM `{os.environ["GCLOUD_PROJECT"]}.eproperty.vacant_properties` 	
@@ -102,7 +111,8 @@ beam_cleanup = BashOperator(
 
 
 # branching DAG splits after the gcs_to_bq stage and converges back at beam_cleanup
-gcs_loader >> dataflow >> gcs_to_bq
-gcs_to_bq >> wprdc_export >> beam_cleanup
-gcs_to_bq >> push_gis >> beam_cleanup
+gcs_loader >> gcs_to_bq >> geojoin
+geojoin >> delete_avro >> beam_cleanup
+geojoin >> wprdc_export >> beam_cleanup
+geojoin >> push_gis >> beam_cleanup
 
