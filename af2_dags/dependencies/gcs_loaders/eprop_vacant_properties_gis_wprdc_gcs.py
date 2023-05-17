37c8b5594690ce9515ab9b71752d9ec83d7d506c
woodjt <37873653+woodjt@users.noreply.github.com>
2023-05-17 09:01:21 -0400

    Bq upload and null handling (#315)
    * intermediate checkout
    
    * adding new ability to push df straight into BQ and skipping AVRO  upload. also changed col names and adjusted dag accordingly
    
    * cleaning up code and extra comments. this has been tested and appears to run well. a future branch will add schema specification for upload into BQ

--- af2_dags/dependencies/gcs_loaders/eprop_vacant_properties_gis_wprdc_gcs.py
+++ af2_dags/dependencies/gcs_loaders/eprop_vacant_properties_gis_wprdc_gcs.py
@@ -2,27 +2,25 @@ import os
 import argparse
 import json
 import requests
-import re
 import pandas as pd
 
-from gcs_utils import json_to_gcs, avro_to_gcs
+from gcs_utils import json_to_gcs
 
 # API_LIMIT controls pagination of API request. As of May 2023 it seems that the request cannot be limited to
 # selected fields. The unwanted fields are removed later and the required fields are specified here in the namees
 # returned directly from the API. Results are uploaded into long-term storage as a json. This is placed in an
 # auto-class bucket, so that data that is not touched (essentially each json) for 30 days is moved to colder storage.
-# The script also uploads AVRO files for BQ ingestion. These files are never used after ingestion and can be
-# recovered from the json quite easily. Each DAG, including this one, will load the AVRO into a single hot bucket and
-# then delete it when it is uploaded into BQ
+# The script also uploads the recrods from a dataframe into BQ. No AVRO is loaded into GCS for later movement into BQ
 
 API_LIMIT = 10000
-FIELDS = ["id", "parcelNumber", "propertyAddress1", "currentOwners", "parcelSquareFootage", "acquisitionMethod",
-          "acquisitionDate", "propertyClass", "censusTract", "latitude", "longitude",
-          "inventoryType", "zonedAs", "currentStatus", "statusDate"]
-AVRO_SCHEMA = "eproperty_vacant_property.avsc"
+FIELDS = {"id": "id", "parcelNumber": "parc", "propertyAddress1": "address",
+          "currentOwners": "owner", "parcelSquareFootage": "parc_sq_ft", "acquisitionMethod": "acquisition_method",
+          "acquisitionDate": "acquisition_date", "propertyClass": "class", "censusTract": "census_tract",
+          "latitude": "lat", "longitude": "long", "inventoryType": "inventory_type", "zonedAs": "zoned_as",
+          "currentStatus": "current_status", "statusDate": "status_date_utc"}
+
 
 json_bucket = f"{os.environ['GCS_PREFIX']}_eproperty"
-hot_bucket = F"{os.environ['GCS_PREFIX']}_hot_metal"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--json_output_arg', dest = 'json_out_loc', required = True,
@@ -71,30 +69,22 @@ while more is True:
 # However, as of May 2023, this does not seem likely to occur
 df_records = pd.DataFrame(all_records)
 
-drops = [f for f in df_records.columns.to_list() if f not in FIELDS]
+drops = [f for f in df_records.columns.to_list() if f not in FIELDS.keys()]
 df_records.drop(drops, axis = 1, inplace = True)
 
-# insert an underscore between any camel cased characters, convert to lower case, and remove numbers
-# change all column names. reorder column for easy inspection.
-name_changes = {}
-for old in FIELDS:
-    old_conv = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', old)
-    new_name = re.sub('([a-z0-9])([A-Z])', r'\1_\2', old_conv).lower().strip()
-    new_name = re.sub(r'[0-9]', '', new_name)
-    name_changes.update({old: new_name})
+df_records.rename(columns = FIELDS, inplace = True)
+df_records = df_records[FIELDS.values()]
 
-df_records.rename(columns = name_changes, inplace = True)
-df_records = df_records[name_changes.values()]
 
 # convert id, an int, to string to be consistent with our SOP and change NaNs to Null
 df_records["id"] = df_records["id"].astype(str)
-# df_records = df_records.fillna(None)
-df_records = df_records.dropna()
 df_records[["neighborhood_name", "council_district", "ward", "fire_zone", "police_zone",
             "dpw_streets", "dpw_enviro", "dpw_parks"]] = ""
+
+# load into BQ
+df_records.to_gbq("eproperty.vacant_properties", F"{os.environ['GCLOUD_PROJECT']}", if_exists= "replace")
 
 # load API results as a json to GCS autoclass storage and avro to temporary hot storage bucket (deleted after load
 # into BQ)
 list_of_dict_recs = df_records.to_dict(orient = 'records')
 json_to_gcs(args['json_out_loc'], list_of_dict_recs, json_bucket)
-avro_to_gcs('eproperties.avro', list_of_dict_recs, hot_bucket, AVRO_SCHEMA)
