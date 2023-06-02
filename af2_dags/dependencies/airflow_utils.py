e5a1549ebca77330683c9a5ee0583e4e4c6e7626
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-06-02 15:28:40 -0400

    Merge in Latest Coord Query-Builder Function

--- af2_dags/dependencies/airflow_utils.py
+++ af2_dags/dependencies/airflow_utils.py
@@ -675,17 +675,18 @@ def build_city_limits_query(dataset, raw_table, lat_field='lat', long_field='lon
     """
 
 
-def build_geo_coords_from_parcel_query(dest, raw_table, parc_field, lat_field = "latitude", long_field = "longitude"):
+def build_geo_coords_from_parcel_query(raw_table, parc_field, lat_field = "latitude", long_field = "longitude"):
     return F"""
-    WITH {dest} AS
-    (SELECT
+    SELECT
         raw.*,
         ST_Y(ST_CENTROID(p.geometry)) AS {lat_field}, 
         ST_X(ST_CENTROID(p.geometry)) AS {long_field}
     FROM `{raw_table}` raw
     LEFT OUTER JOIN `{os.environ['GCLOUD_PROJECT']}.timebound_geography.parcels` p ON 
-    {parc_field} = p.zone)
+    {parc_field} = p.zone
     """
+
+
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     run()
