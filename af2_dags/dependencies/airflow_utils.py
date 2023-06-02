d6f418b6de828fca2b3c2ddb8df73b931d8a1ad4
jasonfic <43254577+jasonfic@users.noreply.github.com>
2023-06-02 15:45:13 -0400

    Tax delinquency table refactor (#343)
    * Coalesce Neighborhood, Export to GIS Project
    
    * Merge in Latest Coord Query-Builder Function

--- af2_dags/dependencies/airflow_utils.py
+++ af2_dags/dependencies/airflow_utils.py
@@ -169,7 +169,6 @@ def build_revgeo_time_bound_query(dataset, raw_table, new_table, create_date, id
     :param create_date: ticket creation date (string)
     :param lat_field: field in table that identifies latitude value
     :param long_field: field in table that identifies longitude value
-    :param table_or_view: indication of whether the output of the query will be a new table or a view
     :param geo_fields_in_raw: indication of whether the raw source already contains the zone columns
 
     :return: string to be passed through as arg to BigQueryOperator
@@ -685,6 +684,7 @@ def build_geo_coords_from_parcel_query(raw_table, parc_field, lat_field = "latit
     LEFT OUTER JOIN `{os.environ['GCLOUD_PROJECT']}.timebound_geography.parcels` p ON 
     {parc_field} = p.zone
     """
+
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
