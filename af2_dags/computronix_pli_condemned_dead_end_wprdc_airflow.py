5442e948a4522ac69cc798cac4467000b0eb261d
Jesse Wood <woodjt.mail@gmail.com>
2023-06-15 14:20:21 -0400

    adjust worflow to allow for query funcs to be called, remove some code that was declared inline, change branch structure of the dag, and all necessary imports

--- af2_dags/computronix_pli_condemned_dead_end_wprdc_airflow.py
+++ af2_dags/computronix_pli_condemned_dead_end_wprdc_airflow.py
@@ -12,9 +12,8 @@ from airflow.contrib.operators.bigquery_to_gcs import BigQueryToCloudStorageOper
 from dependencies import airflow_utils
 from dependencies.airflow_utils import get_ds_year, get_ds_month, get_ds_day, default_args
 
+from bq_queries import computronix_pli_condemned_dead_end_properties_queries as q
 
-# TODO: When Airflow 2.0 is released, upgrade the package, sub in DataFlowPythonOperator for BashOperator,
-# and pass the argument 'py_interpreter=python3'
 
 dag = DAG(
     'computronix_pli_condemned_dead_end_properties',
@@ -52,7 +51,7 @@ dataflow = BashOperator(
 )
 
 
-# Load AVRO data produced by dataflow_script into BQ temp table
+# Load AVRO data produced by dataflow_script into BQ staging table
 gcs_to_bq = GoogleCloudStorageToBigQueryOperator(
         task_id = 'gcs_to_bq',
         destination_project_dataset_table = F"{os.environ['GCLOUD_PROJECT']}:computronix.incoming_pli_program_inspection_properties",
@@ -66,54 +65,30 @@ gcs_to_bq = GoogleCloudStorageToBigQueryOperator(
 )
 
 
-# seperate the condemned properties into a table for PLI
-query_condemned = F"""
-CREATE OR REPLACE TABLE 
-`{os.environ['GCLOUD_PROJECT']}.computronix.pli_condemned_properties` AS
-SELECT 
-    *      
-FROM `{os.environ['GCLOUD_PROJECT']}.computronix.pli_program_inspection_properties`
-WHERE insp_type_desc LIKE 'Condemned Property' AND insp_status NOT LIKE 'Inactive'
-"""
-seperate_condemned = BigQueryOperator(
-        task_id = 'seperate_condemned',
-        sql = query_condemned,
+# union all  new records with the existing records that did not get pulled in if an api request error occurred
+combine_incoming_existing = BigQueryOperator(
+        task_id = 'combine_incoming_existing',
+        sql = q.combine_incoming_existing_recs(),
         bigquery_conn_id='google_cloud_default',
         use_legacy_sql = False,
         dag = dag
 )
 
 
-# seperate the dead end properties into a table for PLI
-query_dead_end = F"""
-CREATE OR REPLACE TABLE 
-`{os.environ['GCLOUD_PROJECT']}.computronix.pli_dead_end_properties` AS
-SELECT 
-    * 
-FROM `{os.environ['GCLOUD_PROJECT']}.computronix.pli_program_inspection_properties`
-WHERE insp_type_desc LIKE 'Dead End Property' AND insp_status NOT LIKE 'Inactive'
-"""
-seperate_dead_end = BigQueryOperator(
-        task_id = 'seperate_dead_end',
-        sql = query_dead_end,
+# seperate all condemened and dead end records for pli export (2 tables)
+seperate_pli_con_dead_end = BigQueryOperator(
+        task_id = 'seperate_con_dead_end',
+        sql = q.create_pli_exp_active_tables(),
         bigquery_conn_id='google_cloud_default',
         use_legacy_sql = False,
         dag = dag
 )
 
 
-# seperate the dead end and condemned properties (both) into a table with inactive and active records
-query_wprdc_exp = F"""
-CREATE OR REPLACE TABLE 
-`{os.environ['GCLOUD_PROJECT']}.computronix.pli_cde_properties_wprdc_exp` AS
-SELECT 
-    *
-FROM `{os.environ['GCLOUD_PROJECT']}.computronix.pli_program_inspection_properties`
-WHERE insp_type_desc LIKE 'Dead End Property' OR insp_type_desc LIKE 'Condemned Property'
-"""
-seperate_wprdc_exp = BigQueryOperator(
-        task_id = 'seperate_wprdc',
-        sql = query_wprdc_exp,
+# seperate the dead end and condemned properties (both together) into a table with inactive and active records
+create_wprdc_exp = BigQueryOperator(
+        task_id = 'create_wprdc_exp',
+        sql = q.create_wprdc_exp_table(),
         bigquery_conn_id='google_cloud_default',
         use_legacy_sql = False,
         dag = dag
@@ -156,43 +131,20 @@ pli_export_dead_end = BigQueryToCloudStorageOperator(
 )
 
 
-# push table of the latest updates of ALL permits (not just active) data-bridGIS BQ
-query_push_gis_latest_cde = F"""
-CREATE OR REPLACE TABLE `data-bridgis.computronix.cde_properties_latest_update` AS
-SELECT 
-  cde.*,
-  ROW_NUMBER () OVER (ORDER BY create_date_UNIX) as parc_unique_id
-FROM `{os.environ['GCLOUD_PROJECT']}.computronix.pli_cde_properties_wprdc_exp` cde
-JOIN
-(SELECT 
-  MAX(create_date_unix) as max_date,
-  parc_num
-FROM `{os.environ['GCLOUD_PROJECT']}.computronix.pli_cde_properties_wprdc_exp`
-GROUP BY parc_num
-) AS max_vals
-ON cde.create_date_UNIX = max_vals.max_date AND cde.parc_num = max_vals.parc_num
-ORDER BY cde.create_date_UNIX
-"""
-
+# push table of the latest updates of ALL cde permits (not just active) data-bridGIS BQ
 push_gis_latest_cde = BigQueryOperator(
         task_id = 'push_gis_latest_cde',
-        sql = query_push_gis_latest_cde,
+        sql = q.push_gis_latest_updates(),
         bigquery_conn_id='google_cloud_default',
         use_legacy_sql = False,
         dag = dag
 )
 
 
-# Push all records to GIS
-query_push_gis_cde = F"""
-CREATE OR REPLACE TABLE `data-bridgis.computronix.cde_properties` AS
-SELECT 
-  cde.*,
-FROM `{os.environ['GCLOUD_PROJECT']}.computronix.pli_cde_properties_wprdc_exp` cde
-"""
+# Push all cde records to GIS
 push_gis_cde = BigQueryOperator(
         task_id = 'push_gis_cde',
-        sql = query_push_gis_cde,
+        sql = q.push_gis_all_recs(),
         bigquery_conn_id='google_cloud_default',
         use_legacy_sql = False,
         dag = dag
@@ -206,9 +158,12 @@ beam_cleanup = BashOperator(
 )
 
 
-# branching DAG splits after the gcs_to_bq stage and converges back at beam_cleanup
-gcs_loader >> dataflow >> gcs_to_bq
-gcs_to_bq >> seperate_condemned >> pli_export_condemned >> beam_cleanup
-gcs_to_bq >> seperate_dead_end >> pli_export_dead_end >> beam_cleanup
-gcs_to_bq >> seperate_wprdc_exp >> wprdc_export >> beam_cleanup
-gcs_to_bq >> push_gis_latest_cde >> push_gis_cde >> beam_cleanup
+gcs_loader >> dataflow >> gcs_to_bq >> combine_incoming_existing
+combine_incoming_existing >> seperate_pli_con_dead_end
+combine_incoming_existing >> create_wprdc_exp
+seperate_pli_con_dead_end >> pli_export_condemned >> beam_cleanup
+seperate_pli_con_dead_end >> pli_export_dead_end >> beam_cleanup
+create_wprdc_exp >> wprdc_export >> beam_cleanup
+create_wprdc_exp >> push_gis_latest_cde >> beam_cleanup
+create_wprdc_exp  >> push_gis_cde >> beam_cleanup
+
