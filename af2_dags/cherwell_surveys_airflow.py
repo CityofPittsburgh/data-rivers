ad0106a83f627029a4f380d0addc5ba4219d8a9c
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-06-30 14:33:40 -0400

    Move from Dataflow to Pandas Processing for Cherwell Surveys

--- af2_dags/cherwell_surveys_airflow.py
+++ af2_dags/cherwell_surveys_airflow.py
@@ -23,7 +23,7 @@ COLS = """id, incident_id, created_date_EST, created_date_UTC, created_date_UNIX
 
 
 dag = DAG(
-    'cherwell_surveys',
+    'cherwell_surveys_pandas',
     default_args=default_args,
     schedule_interval='@daily',
     start_date=datetime(2023, 5, 8),
@@ -36,67 +36,50 @@ dag = DAG(
 # initialize gcs locations and store endpoint names in variables
 source = "cherwell"
 bucket = f"gs://{os.environ['GCS_PREFIX']}_{source}"
+hot_bucket = f"{os.environ['GCS_PREFIX']}_hot_metal"
 dataset = "surveys"
 exec_date = "{{ ds }}"
 path = "{{ ds|get_ds_year }}/{{ ds|get_ds_month }}/{{ run_id }}"
 json_loc = f"{dataset}/{path}_{dataset}.json"
-avro_loc = f"{dataset}/avro_output/{path}"
-table = "customer_satisfaction_survey_responses"
+avro_loc = "survey_responses"
+table = "customer_satisfaction_survey_responses_pandas"
 id_col = "id"
-date_fields = ['created_date_EST', 'created_date_UTC',
-               'submitted_date_EST', 'submitted_date_UTC',
-               'last_modified_date_EST', 'last_modified_date_UTC']
+cast_fields = [{'field': 'created_date_EST', 'type': 'DATETIME'},
+               {'field': 'created_date_UTC', 'type': 'DATETIME'},
+               {'field': 'submitted_date_EST', 'type': 'DATETIME'},
+               {'field': 'submitted_date_UTC', 'type': 'DATETIME'},
+               {'field': 'submitted_date_UNIX', 'type': 'INT64'},
+               {'field': 'last_modified_date_EST', 'type': 'DATETIME'},
+               {'field': 'last_modified_date_UTC', 'type': 'DATETIME'}]
 
 
-cherwell_surveys_gcs = BashOperator(
-    task_id='cherwell_surveys_gcs',
+cherwell_surveys_pandas_gcs = BashOperator(
+    task_id='cherwell_surveys_pandas_gcs',
     bash_command=f"python {os.environ['GCS_LOADER_PATH']}/cherwell_surveys_gcs.py --output_arg {json_loc}",
     dag=dag
 )
 
-cherwell_surveys_dataflow = BashOperator(
-    task_id='cherwell_surveys_dataflow',
-    bash_command=f"python {os.environ['DATAFLOW_SCRIPT_PATH']}/cherwell_surveys_dataflow.py "
-                 f"--input {bucket}/{json_loc} --avro_output {bucket}/{avro_loc}",
+cherwell_surveys_pandas = BashOperator(
+    task_id='cherwell_surveys_pandas',
+    bash_command=f"python {os.environ['DATAFLOW_SCRIPT_PATH']}/cherwell_surveys_pandas.py --input {json_loc}",
     dag=dag
 )
 
-cherwell_surveys_bq_load = GoogleCloudStorageToBigQueryOperator(
-    task_id='cherwell_surveys_bq_load',
-    destination_project_dataset_table=f"{os.environ['GCLOUD_PROJECT']}.{source}.{table}",
-    bucket=f"{os.environ['GCS_PREFIX']}_{source}",
-    source_objects=[f"{avro_loc}*.avro"],
-    write_disposition='WRITE_TRUNCATE',
-    create_disposition='CREATE_IF_NEEDED',
-    source_format='AVRO',
-    autodetect=True,
-    bigquery_conn_id='google_cloud_default',
-    dag=dag
-)
-
-format_date_query = build_format_dedup_query(source, table, 'DATETIME', date_fields, COLS,
-                                             datestring_fmt="%m/%d/%Y %I:%M:%S %p")
-format_dates = BigQueryOperator(
-    task_id='format_dates',
+format_date_query = build_format_dedup_query(source, table, cast_fields, COLS, datestring_fmt="%Y-%m-%d %H:%M:%S")
+pandas_format_dates = BigQueryOperator(
+    task_id='pandas_format_dates',
     sql=format_date_query,
     bigquery_conn_id='google_cloud_default',
     use_legacy_sql=False,
     dag=dag
 )
 
-dedup_table = BigQueryOperator(
-    task_id='dedup_table',
+pandas_dedup_table = BigQueryOperator(
+    task_id='pandas_dedup_table',
     sql=build_dedup_old_updates(source, table, id_col, 'last_modified_date_UNIX'),
     bigquery_conn_id='google_cloud_default',
     use_legacy_sql=False,
     dag=dag
 )
 
-beam_cleanup = BashOperator(
-    task_id='beam_cleanup',
-    bash_command=airflow_utils.beam_cleanup_statement(f"{os.environ['GCS_PREFIX']}_{source}"),
-    dag=dag
-)
-
-cherwell_surveys_gcs >> cherwell_surveys_dataflow >> cherwell_surveys_bq_load >> format_dates >> \
-    dedup_table >> beam_cleanup
+cherwell_surveys_pandas_gcs >> cherwell_surveys_pandas >> pandas_format_dates >> pandas_dedup_table
--- af2_dags/dependencies/dataflow_scripts/cherwell_surveys_pandas.py
+++ af2_dags/dependencies/dataflow_scripts/cherwell_surveys_pandas.py
@@ -1,141 +1,20 @@
 from __future__ import absolute_import
 
-import logging
 import os
 import argparse
-import json
-import ndjson
-import re
 import io
-import pytz
-import dateutil
 
 import pandas as pd
 from google.cloud import storage
 
-import avro.schema
-from avro.datafile import DataFileWriter
-from avro.io import DatumWriter
+from dataflow_utils.pandas_utils import change_data_type, conv_avsc_to_bq_schema, standardize_times #, strip_char_pattern
 
-
-def avro_to_gcs(file_name, avro_object_list, bucket_name, schema_name):
-    """
-    take list of dicts in memory and upload to GCS as AVRO
-    :params
-    :file_name: str file name ending in ".avro" example-> "test.avro"
-    :avro_object_list: a list of dictionaries. each dictionary is a single row of all fields
-    :bucket_name: str of destination bucket. for avro files to be pushed into BQ and then deleted (the most common
-    use case) this will be F"{os.environ['GCS_PREFIX']}_hot_metal"
-    :schema_name: str containing the schema name example -> "test_schema.avsc"
-    """
-    avro_bucket = F"{os.environ['GCS_PREFIX']}_avro_schemas"
-    blob = storage.Blob(
-        name=schema_name,
-        bucket=storage_client.get_bucket(avro_bucket),
-    )
-
-    schema_text = blob.download_as_string()
-    schema = avro.schema.Parse(schema_text)
-    writer = DataFileWriter(open(file_name, "wb"), DatumWriter(), schema)
-    for item in avro_object_list:
-       writer.append(item)
-    writer.close()
-
-    upload_file_gcs(bucket_name, file_name)
-
-
-def change_data_type(df, convs):
-    df = df.astype(convs)
-    return df
-
-
-def json_linter(ndjson: str):
-    result_ndjson = []
-    for i, line in enumerate(ndjson.split('\n')):
-        try:
-            json.loads(line)
-            result_ndjson.append(line)
-        except:
-            json_split = line.split('}{')
-            for idx in range(len(json_split)):
-                if idx == 0:
-                    result_ndjson.append(json_split[idx] + '}')
-                elif idx == (len(json_split)-1):
-                    result_ndjson.append('{' + json_split[idx])
-                else:
-                    result_ndjson.append('{' + json_split[idx] + '}')
-
-    return '\n'.join(result_ndjson)
-
-
-def json_to_gcs(path, json_object_list, bucket_name):
-    """
-    take list of dicts in memory and upload to GCS as newline JSON
-    """
-    blob = storage.Blob(
-        name=path,
-        bucket=storage_client.get_bucket(bucket_name),
-    )
-    try:
-        blob.upload_from_string(
-            # dataflow needs newline-delimited json, so use ndjson
-            data=ndjson.dumps(json_object_list),
-            content_type='application/json',
-            client=storage_client,
-        )
-    except json.decoder.JSONDecodeError:
-        print("Error uploading data to GCS bucket, linting and trying again")
-        str_requests = ndjson.dumps(json_object_list)
-        linted = "[" + json_linter(str_requests) + "]"
-        linted_requests = ndjson.loads(linted)[0]
-        json_to_gcs(path, linted_requests, bucket_name)
-
-    logging.info('Successfully uploaded blob %r to bucket %r.', path, bucket_name)
-
-    print('Successfully uploaded blob {} to bucket {}'.format(path, bucket_name))
-
-
-def standardize_times(df, time_changes):
-    result = {}
-
-    for time_change in time_changes:
-        if df[time_change[0]] is not None and df[time_change[0]] != '':
-            parse_dt = dateutil.parser.parse(df[time_change[0]])
-            clean_dt = parse_dt.replace(tzinfo=None)
-            try:
-                pytz.all_timezones.index(time_change[1])
-            except ValueError:
-                pass
-            else:
-                loc_time = pytz.timezone(time_change[1]).localize(clean_dt, is_dst=None)
-                utc_conv = loc_time.astimezone(tz=pytz.utc)
-                east_conv = loc_time.astimezone(tz=pytz.timezone('US/Eastern'))
-                unix_conv = utc_conv.timestamp()
-                result['{}_UTC'.format(time_change[0])] = utc_conv.strftime('%Y-%m-%d %H:%M:%S')
-                result['{}_EST'.format(time_change[0])] = east_conv.strftime('%Y-%m-%d %H:%M:%S')
-                result['{}_UNIX'.format(time_change[0])] = int(unix_conv)
-        else:
-            result['{}_UTC'.format(time_change[0])] = None
-            result['{}_EST'.format(time_change[0])] = None
-            result['{}_UNIX'.format(time_change[0])] = None
-
-    return pd.Series(result)
-
-
-def upload_file_gcs(bucket_name, file):
-    """
-    Uploads a file to the bucket.
-    param bucket_name:str = "your-bucket-name" where the file will be placed
-    param source_file:str = "local/path/to/file"
-    param destination_blob_name:str = "storage-object-name"
-    """
-
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(file)
-    blob.upload_from_filename(file)
-
-    os.remove(file)
-
+FINAL_COLS = ['id', 'incident_id', 'created_date_EST', 'created_date_UTC', 'created_date_UNIX', 'submitted_by',
+              'submitted_date_EST', 'submitted_date_UTC', 'submitted_date_UNIX', 'survey_complete',
+              'q1_timely_resolution', 'q2_handled_professionally', 'q3_clear_communication', 'q4_overall_satisfaction',
+              'q5_request_handled_first_time', 'q6_improvement_suggestions', 'q7_contact_me', 'q8_additional_comments',
+              'survey_score', 'avg_survey_score', 'owned_by', 'last_modified_date_EST', 'last_modified_date_UTC',
+              'last_modified_date_UNIX', 'last_modified_by']
 
 storage_client = storage.Client()
 bucket = storage_client.bucket(f"{os.environ['GCS_PREFIX']}_cherwell")
@@ -144,8 +23,6 @@ hot_bucket = f"{os.environ['GCS_PREFIX']}_hot_metal"
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', dest='input', required=True,
                     help='fully specified GCS location from which to retrieve the source json file')
-# parser.add_argument('--json_output_arg', dest = 'json_out_loc', required = True,
-#                     help = 'fully specified location to upload the processed json file for long-term storage')
 args = vars(parser.parse_args())
 
 blob = bucket.blob(args['input'])
@@ -177,15 +54,20 @@ df = df.drop(columns=time_cols).join(time_df)
 df['submitted_date_UNIX'] = df['submitted_date_UNIX'].fillna(0)
 convs = {'id': str, 'incident_id': str, 'q1_timely_resolution': int,
          'q2_handled_professionally': int, 'q3_clear_communication': int,
-         'q4_overall_satisfaction': int,  'submitted_date_UNIX': int,
+         'q4_overall_satisfaction': int, 'created_date_UNIX': int,
+         'submitted_date_UNIX': int, 'last_modified_date_UNIX': int,
          'survey_score': float, 'avg_survey_score': float}
 df = change_data_type(df, convs)
-df['submitted_date_UNIX'] = df['submitted_date_UNIX'].mask(df['submitted_date_UNIX'] == 0, None)
+# df['submitted_date_UNIX'] = df['submitted_date_UNIX'].mask(df['submitted_date_UNIX'] == 0, None)
 df['survey_complete'] = df['survey_complete'].map({'True': True, 'False': False})
 
 # convert all different Null types to a single type (None)
+df = df.applymap(lambda x: None if isinstance(x, str) and x == '' else x)
 df = df.where(df.notnull(), None)
+# df = strip_char_pattern(df, ['submitted_date_UNIX'], "(?<=\d)\.0$")
+df = df[FINAL_COLS]
 
-out_data = df.to_dict(orient='records')
-# json_to_gcs(args['json_out_loc'], out_data, bucket)
-avro_to_gcs('survey_responses.avro', out_data, hot_bucket, "cherwell_surveys.avsc")
+#  read in AVRO schema and load into BQ
+schema = conv_avsc_to_bq_schema(F"{os.environ['GCS_PREFIX']}_avro_schemas", "cherwell_surveys.avsc")
+df.to_gbq("cherwell.customer_satisfaction_survey_responses_pandas", project_id=f"{os.environ['GCLOUD_PROJECT']}",
+          if_exists="replace") #, table_schema=schema)
