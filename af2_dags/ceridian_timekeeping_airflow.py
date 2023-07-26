466a778dc4a9011826a6fac6b8e4e0d84d42ea78
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-07-26 12:25:27 -0400

    Upload Table from Avro Bucket, Insert into Master Table

--- af2_dags/ceridian_timekeeping_airflow.py
+++ af2_dags/ceridian_timekeeping_airflow.py
@@ -9,16 +9,12 @@ from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 from airflow.contrib.operators.bigquery_to_gcs import BigQueryToCloudStorageOperator
 from dependencies import airflow_utils
-from dependencies.airflow_utils import get_ds_month, get_ds_year, get_ds_day, \
-    default_args, build_percentage_table_query
+from dependencies.airflow_utils import get_ds_month, get_ds_year, get_ds_day, default_args, \
+    build_insert_new_records_query
 
-# The goal of this DAG is to perform a daily pull of basic demographic information for each
+# The goal of this DAG is to perform a monthly pull of timesheet entries for all
 # City of Pittsburgh employee via the Ceridian Dayforce API. This  data will be stored securely
-# in Data Rivers and used for a few projects. For one, we will use the list of timekeeping returned
-# by the API to determine who has left the City payroll so that we can stop keeping track of
-# their COVID vaccination status. Additionally, we will use de-identified race, sex, and union
-# membership totals to display on Dashburgh. This will give the public insight on the demographics
-# of the city government and how it compares to the demographics of the city as a whole.
+# in Data Rivers and visualized in a Looker Studio dashboard for internal usage.
 
 dag = DAG(
     'ceridian_timekeeping',
@@ -31,8 +27,10 @@ dag = DAG(
 
 # initialize gcs locations
 bucket = f"gs://{os.environ['GCS_PREFIX']}_ceridian"
+hot_bucket = f"gs://{os.environ['GCS_PREFIX']}_hot_metal"
 dataset = "timekeeping"
-# path = "{{ ds + macros.dateutil.relativedelta.relativedelta(months=-1, day=1) }}"
+
+# create one single json file per month that gets overwritten each day
 today = datetime.today()
 first_curr_month = today.replace(day=1)
 prev_month_last = first_curr_month - timedelta(days=1)
@@ -40,7 +38,7 @@ prev_month = str(prev_month_last.date().strftime("%m-%Y"))
 path = "{{ ds|get_ds_year }}"
 
 json_loc = f"{dataset}/{path}/{prev_month}_timekeeping.json"
-avro_loc = f"{dataset}/avro_output/{path}/" + "{{ run_id }}"
+avro_loc = "employee_timekeeping"
 
 ceridian_gcs = BashOperator(
     task_id='ceridian_gcs',
@@ -50,21 +48,41 @@ ceridian_gcs = BashOperator(
 
 ceridian_dataflow = BashOperator(
     task_id='ceridian_dataflow',
-    bash_command=f"python {os.environ['DATAFLOW_SCRIPT_PATH']}/ceridian_timekeeping_dataflow.py --input {bucket}/{json_loc} "
-                 f"--avro_output {bucket}/{avro_loc}",
+    bash_command=f"python {os.environ['DATAFLOW_SCRIPT_PATH']}/ceridian_timekeeping_dataflow.py "
+                 f"--input {bucket}/{json_loc} --avro_output {hot_bucket}/{avro_loc}*.avro",
     dag=dag
 )
 
 ceridian_bq_load = GoogleCloudStorageToBigQueryOperator(
     task_id='ceridian_bq_load',
     destination_project_dataset_table=f"{os.environ['GCLOUD_PROJECT']}.ceridian.monthly_timesheet_report",
-    bucket=f"{os.environ['GCS_PREFIX']}_ceridian",
+    bucket=hot_bucket,
     source_objects=[f"{avro_loc}*.avro"],
     write_disposition='WRITE_TRUNCATE',
     create_disposition='CREATE_IF_NEEDED',
     source_format='AVRO',
     autodetect=True,
     bigquery_conn_id='google_cloud_default',
+    dag=dag
+)
+
+write_append_query = F"""
+CREATE OR REPLACE TABLE `{os.environ['GCLOUD_PROJECT']}.ceridian.historic_timekeeping` AS
+    SELECT * FROM `{os.environ['GCLOUD_PROJECT']}.ceridian.historic_timekeeping`
+    UNION DISTINCT
+    SELECT * FROM `{os.environ['GCLOUD_PROJECT']}.ceridian.monthly_timesheet_report`
+"""
+insert_monthly_data = BigQueryOperator(
+    task_id='insert_monthly_data',
+    sql=write_append_query,
+    bigquery_conn_id='google_cloud_default',
+    use_legacy_sql=False,
+    dag=dag
+)
+
+delete_avro = BashOperator(
+    task_id='delete_avro',
+    bash_command=f"gsutil rm -r gs://{hot_bucket}/{avro_loc}*.avro",
     dag=dag
 )
 
