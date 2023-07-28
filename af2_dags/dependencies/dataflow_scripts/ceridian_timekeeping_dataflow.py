cfd2982464341638a24f16c84b2cf36ca4a2ab53
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-07-28 17:00:45 -0400

    Use New Utils in Timekeeping Script, Convert Date to Datetime

--- af2_dags/dependencies/dataflow_scripts/ceridian_timekeeping_dataflow.py
+++ af2_dags/dependencies/dataflow_scripts/ceridian_timekeeping_dataflow.py
@@ -9,7 +9,8 @@ from apache_beam.io import ReadFromText
 from apache_beam.io.avroio import WriteToAvro
 
 from dataflow_utils import dataflow_utils
-from dataflow_utils.dataflow_utils import JsonCoder, SwapFieldNames, generate_args, ChangeDataTypes, FilterFields
+from dataflow_utils.dataflow_utils import JsonCoder, SwapFieldNames, generate_args, ChangeDataTypes, FilterFields, \
+    CrosswalkDeptNames, StripBeforeDelim, StandardizeTimes
 from google.cloud import storage
 
 DEFAULT_DATAFLOW_ARGS = [
@@ -19,56 +20,6 @@ DEFAULT_DATAFLOW_ARGS = [
     f"--region={os.environ['REGION']}",
     f"--subnetwork={os.environ['SUBNET']}"
 ]
-
-USER_DEFINED_CONST_BUCKET = "user_defined_data"
-
-
-class CrosswalkDeptNames(beam.DoFn):
-    def __init__(self, place_id_bucket, crosswalk_file):
-        """
-        :param place_id_bucket - Name of GCS bucket the crosswalk file will be extracted from
-        :param crosswalk_file - Environmental variable containing the name of a JSON file that maps department names
-        returned by the Ceridian API to department names sourced from the City's Department of Human Resources
-        """
-        self.place_id_bucket = place_id_bucket
-        self.crosswalk_file = crosswalk_file
-
-    def process(self, datum):
-        storage_client = storage.Client()
-        bucket = storage_client.get_bucket(self.place_id_bucket)
-        blob = bucket.get_blob(self.crosswalk_file)
-        cw = blob.download_as_string()
-        crosswalk = json.loads(cw.decode('utf-8'))
-        datum['dept'] = None
-        datum['dept_desc'] = None
-        datum['office'] = None
-        datum['corporation'] = None
-        for dict in crosswalk:
-            if datum['Department_LongName'] == dict['Ceridian Department Name']:
-                datum['dept'] = dict['Department']
-                datum['dept_desc'] = dict['Department Description']
-                datum['office'] = dict['Office']
-                datum['corporation'] = dict['Corporation']
-        if not datum['dept']:
-            split_dept = datum['Department_LongName'].split("-")
-            datum['dept'] = split_dept[0]
-            try:
-                datum['dept_desc'] = split_dept[1]
-                try:
-                    datum['office'] = split_dept[2]
-                except:
-                    datum['office'] = split_dept[0]
-            except:
-                datum['dept_desc'] = split_dept[0]
-            datum['corporation'] = 'City of Pittsburgh'
-        yield datum
-
-
-class StripDate(beam.DoFn):
-    def process(self, datum):
-        if datum['EmployeePaySummary_BusinessDate']:
-            datum['EmployeePaySummary_BusinessDate'] = datum['EmployeePaySummary_BusinessDate'].split('T')[0]
-        yield datum
 
 
 def run(argv=None):
@@ -87,10 +38,12 @@ def run(argv=None):
     )
 
     with beam.Pipeline(options=pipeline_options) as p:
+        date_fields = ['EmployeePaySummary_BusinessDate']
+        times = [date_fields[0], 'EST']
         field_name_swaps = [('EmployeeEmploymentStatus_EmployeeNumber', 'employee_num'),
                             ('Employee_DisplayName', 'display_name'),
                             ('Job_ShortName', 'job_title'),
-                            ('EmployeePaySummary_BusinessDate', 'pay_period'),
+                            ('EmployeePaySummary_BusinessDate_EST', 'work_period'),
                             ('PayAdjCode_ShortName', 'pay_code'),
                             ('PayCategory_CodeName', 'pay_category'),
                             ('PayCategory_LongName', 'pay_category_description'),
@@ -98,15 +51,17 @@ def run(argv=None):
                             ('LaborMetricsCode0_XRefCode', 'labor_metrics_code')]
         type_changes = [('employee_num', 'str'), ('net_hours', 'float')]
         drop_fields = ['EmployeePaySummary_PayAmountSum', 'EmployeePaySummary_Rate', 'OrgUnit_ShortName',
-                       'LaborMetricsCode0_LedgerCode', 'Department_LongName']
+                       'LaborMetricsCode0_LedgerCode', 'Department_LongName', 'EmployeePaySummary_BusinessDate_UTC',
+                       'EmployeePaySummary_BusinessDate_UNIX']
 
         lines = p | ReadFromText(known_args.input, coder=JsonCoder())
 
         load = (
                 lines
-                | beam.ParDo(StripDate())
+                | beam.ParDo(StripBeforeDelim(date_fields, delim='T'))
+                | beam.ParDo(StandardizeTimes(times, "%m/%d/%Y"))
                 | beam.ParDo(SwapFieldNames(field_name_swaps))
-                | beam.ParDo(CrosswalkDeptNames(USER_DEFINED_CONST_BUCKET, os.environ['CERIDIAN_DEPT_FILE']))
+                | beam.ParDo(CrosswalkDeptNames('Department_LongName'))
                 | beam.ParDo(ChangeDataTypes(type_changes))
                 | beam.ParDo(FilterFields(drop_fields, exclude_target_fields=True))
                 | WriteToAvro(known_args.avro_output, schema=avro_schema, file_name_suffix='.avro',
