e4229b7e5d897915f24a3ed1b3bf0830833f00b7
Jesse Wood <woodjt.mail@gmail.com>
2023-08-01 13:41:44 -0400

    intermediate checkout for testing

--- af2_dags/dependencies/gcs_loaders/eprop_vacant_properties_gis_wprdc_extract.py
+++ af2_dags/dependencies/gcs_loaders/eprop_vacant_properties_gis_wprdc_extract.py
@@ -14,40 +14,104 @@ from gcs_utils import json_to_gcs, conv_avsc_to_bq_schema
 # auto-class bucket, so that data that is not touched (essentially each json) for 30 days is moved to colder storage.
 # The script also uploads the recrods from a dataframe into BQ. No AVRO is loaded into GCS for later movement into BQ
 
-
 def normalize_block_lot(x):
-    # convert all block lots to the standard format of dehyphenated 16 chars
-    # for each section of the incorrectly formatted block/lot, there may not be the correct length of characters as
-    # specified in part_len. these to be zero padded on the left and those section may not be there at all.
-    if x is not np.nan:
-        parts = x.split("-")
-        if len(parts) == 1:
-            # print("bad value")
+    """
+    function to standardize the format of parcel numbers/block lots to the county format. this is intended to be run on
+    single input values and is designed to work in parallel processing applications (e.g. dataflow or pandas.apply etc)
+
+    the terminology block/lot and parcel number are interchangeable
+
+    the only acceptable format is the same the county uses- a 16 char string (no special chars) with the 5th
+    character being a letter and all others numeric.
+
+    each of the 5 components of the parcel number string are fixed in length (4, 1, 5, 4, and 2 characters, respectively)
+    and no characters can be omitted (e.g. 1234X12345123412 is correct) Zero padding the components is very common (
+    i.e. each component frequently contains several "0") (0001X00001000101 is correctly formatted).
+
+    The city frequently inserts hyphens between components and does not zero pad them. As an example, this parcel
+    number: 0001X00001000101 (when correctly formatted) could appear like this in the city's datasets: 01-X-01-1-1.
+    Further, when all components of the parcel number are "0"s the city frequently omits them. As an example: 01-X-01
+    would represent 0001X00001000000.
+
+    This function will dehypenate, zero pad, and verify correctness
+
+    :param x: string input value representing the parcel number
+    :return out: a string of the formatted parcel number
+    """
+
+
+    # default parcel number component values (number of chars per component, boolean if alphabetical vals allowed,
+    # boolean if the component is required (sometimes the final two components (6 chars) are ommitted)
+
+    components = {"len": [4, 1, 5, 4, 2],
+                  "alpha_char" : [False, True, False, False, False],
+                  "required": [True, True, True , False, False]}
+
+    # skip Nulls
+    if x is not None and x is not np.nan:
+
+        # strip white spaces
+        x = x.strip().upper()
+
+        #all values must be a hyphen or alphanumeric (no special chars)
+        for char_val in x:
+            if not char_val.isalnum() and char_val != "-":
+                return None
+
+        # either the string contains hyphens (and can be variable length) or it is exactly 16 chars
+        if x.__contains__("-"):
+            parts = x.split("-")
+            if len(parts) > len(components["required"]):
+                return None
+
+        elif len(x) == 16:
+            parts = []
+            for l in components["len"]:
+                sel_string = x[:l]
+                x = x[l:]
+                parts.append(sel_string)
+
+        # fails above conditional logic
+        else:
             return None
 
-        part_len = [4, 1, 5, 4, 2]
+        # all processing below assumes that the input is exactly 16 chars or hyphenated, and without special chars.
+        # all fully inspected vals will overwrite the elements of conv_vals. The final 2 components of the string are
+        # often ommitted by the city if they are all zeros. In this case, the zeros pre populated in conv_vals will
+        # take their place
+        conv_vals = ["","","","0000","00"]
 
-        p = 0
-        out = ""
-        while p < len(part_len):
-            # don't convert alpha only chars
-            if len(parts) >= (p + 1) and parts[p].isalpha():
-                out = out + parts[p].rjust(part_len[p], "0")
-                p += 1
-                continue
+        for i in range(len(parts)):
 
-            # if the current part is missing make it all zeros
-            if len(parts) >= (p+1):
-                parts[p] = parts[p].rjust(part_len[p], "0")
-            else:
-                parts.append("".rjust(part_len[p], "0"))
+            # verify all chars in component are correctly alpha or non alpha components (as of August 2023,
+            # only component #2 is allowed to be a letter) (special char containing strings already bypassed- this
+            # checks that each char within each component is correctly alpha or numeric (e.g not like "12A4")
+            for c_num in parts[i]:
+                if c_num.isalpha() != components["alpha_char"][i]:
+                    return None
 
-            # append it all together
-            out = out + parts[p]
-            p += 1
-        return out
+            # verify component is not longer than allowed (if the string was 16 chars then this has to be correct,
+            # so this only really is useful if the input was hyphenated. input string hyphen locations are variable)
+            if len(parts[i]) > components["len"][i]:
+                return None
+
+            # pad all string parts with zeros if they are shorter than required. this code results in no changes if
+            # 1) the input string was 16 chars in length or 2) the part is already the correct length
+            conv_vals[i] = parts[i].rjust(components["len"][i],"0")
+
+        out = "".join(conv_vals)
+
+        # the final output must be 16 chars (this is essentially guaranteed by this point and this is a final
+        # safeguard and there must be a minimum of 2 unique characters (one letter and one number). The reality is
+        # that there should be approx 4-5 unique characters as the absolute minimum. This is not well constrained in
+        # the parcel number system. The minimum of 2 unique characters prevents test strings like "0000A00000000000"
+        # from passing through. This is, however, only a weak safeguard.
+        if len(out) == 16 and len(set(out)) > 2:
+            return out
+        else:
+            return None
     else:
-        return np.nan
+        return None
 
 
 API_LIMIT = 10000
@@ -59,10 +123,10 @@ FIELDS = {"id": "id", "parcelNumber": "parc", "propertyAddress1": "address",
 
 json_bucket = f"{os.environ['GCS_PREFIX']}_eproperty"
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--json_output_arg', dest='json_out_loc', required=True,
-                    help='fully specified location to upload the processed json file for long-term storage')
-args = vars(parser.parse_args())
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--json_output_arg', dest='json_out_loc', required=True,
+#                     help='fully specified location to upload the processed json file for long-term storage')
+# args = vars(parser.parse_args())
 
 # Build the API request components
 URL_BASE = F"https://api.epropertyplus.com/landmgmt/api/property/"
@@ -118,22 +182,8 @@ df_records["id"] = df_records["id"].astype(str)
 df_records["address"].apply(lambda x: x.upper())
 
 # clean the parcel numbers
-df_records["parc"] = df_records["parc"].apply(lambda x: str(x.strip().upper()))
-regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
-df_records.drop(
-        df_records[df_records["parc"].apply(lambda x: regex.search(x)) == None].index.to_list(),
-inplace = True
-)
-df_records.drop(
-        df_records[df_records["parc"].apply(lambda x: len(x) > 16)].index.to_list(),
-        inplace = True
-)
-df_clean = df_records[df_records["parc"].apply(lambda x: len(x) == 16)]
-df_bad_parc = df_records[df_records["parc"].apply(lambda x: len(x) < 16)]
-df_bad_parc["parc"] = df_bad_parc["parc"].apply(lambda x: normalize_block_lot(x))
-df_bad_parc = df_bad_parc[   df_bad_parc["parc"].apply(lambda x: x != None)]
+df_records["parc"] = df_records["parc"].apply(lambda x: normalize_block_lot(x))
 
-df_final = df_clean.append(df_bad_parc)
 
 
 # load into BQ
