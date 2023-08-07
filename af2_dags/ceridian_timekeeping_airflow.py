d4f108cb8c4e1d2c67daf28bdcbcb7a9c8a32465
jasonfic <43254577+jasonfic@users.noreply.github.com>
2023-08-07 08:18:32 -0400

    Timekeeping Backfill Fix (#389)
    * Allow Full Backfill of Timekeeping Data
    
    * Only Include Distinct Rows in Final Table
    
    * Loop in 15 Day Chunks for API Extract
    
    * Add Debugging to Timekeeping GCS Loader
    
    * Fix Typo, Use Correct Destination for Final Table

--- af2_dags/ceridian_timekeeping_airflow.py
+++ af2_dags/ceridian_timekeeping_airflow.py
@@ -66,9 +66,9 @@ ceridian_timekeeping_bq_load = GoogleCloudStorageToBigQueryOperator(
 
 write_append_query = F"""
 CREATE OR REPLACE TABLE `{os.environ['GCLOUD_PROJECT']}.ceridian.historic_timekeeping` AS
-    SELECT * FROM `{os.environ['GCLOUD_PROJECT']}.ceridian.historic_timekeeping`
+    SELECT DISTINCT * FROM `{os.environ['GCLOUD_PROJECT']}.ceridian.historic_timekeeping`
     UNION DISTINCT
-    SELECT * FROM `{os.environ['GCLOUD_PROJECT']}.ceridian.monthly_timesheet_report`
+    SELECT DISTINCT * FROM `{os.environ['GCLOUD_PROJECT']}.ceridian.monthly_timesheet_report`
 """
 insert_monthly_data = BigQueryOperator(
     task_id='insert_monthly_data',
--- af2_dags/dependencies/gcs_loaders/ceridian_timekeeping_gcs.py
+++ af2_dags/dependencies/gcs_loaders/ceridian_timekeeping_gcs.py
@@ -17,14 +17,29 @@ parser.add_argument('--output_arg', dest='out_loc', required=True,
                     help='fully specified location to upload the combined ndjson file')
 args = vars(parser.parse_args())
 
+
+# adapted from https://stackoverflow.com/a/43088
+def last_day_of_month(datestring):
+    input_date = datetime.strptime(datestring, "%m/%d/%Y %H:%M:%S %p")
+    if input_date.month == 12:
+        return input_date.replace(day=31)
+    date_val = input_date.replace(month=input_date.month+1, day=1) - timedelta(days=1)
+    return str(date_val.date().strftime("%m/%d/%Y")) + " 11:59:59 PM"
+
+
+def set_url(first_of_month, last_of_month):
+    return f"https://www.dayforcehcm.com/Api/{os.environ['CERIDIAN_ORG_ID']}/V1/Reports/TIMESHEETREPORTAPI" \
+                f"?5b93e411-2928-463d-b048-03460269d416={first_of_month}&" \
+                f"bb93e411-2928-463d-b048-03460269d416={last_of_month}"
+
+
 # adapted from https://pynative.com/python-get-last-day-of-month/#h-get-last-day-of-a-previous-month
 input_dt = datetime.today()
-first_curr_month = input_dt.replace(day=1)
-
-prev_month_last = first_curr_month - timedelta(days=1)
-prev_month_first = prev_month_last.replace(day=1)
-
-prev_month_last = str(prev_month_last.date().strftime("%m/%d/%Y")) + " 11:59:59 PM"
+curr_month_first = input_dt.replace(day=1)
+# store the initial first day of the previous month in a variable so the backfill knows when to stop
+curr_month_first_str = str(curr_month_first.date().strftime("%m/%d/%Y %H:%M:%S %p"))
+month_last = curr_month_first - timedelta(days=1)
+month_first = month_last.replace(day=1)
 
 run_start_win, first_run = find_last_successful_run(bucket, "timekeeping/successful_run_log/log.json",
                                                     "01/01/2023 00:00:00 AM")
@@ -32,22 +47,28 @@ run_start_win, first_run = find_last_successful_run(bucket, "timekeeping/success
 # if this is the first time the DAG has ran, start pulling data from the beginning of the year
 # otherwise, only get the previous month's data
 if first_run:
-    prev_month_first = run_start_win
+    month_first = run_start_win
 else:
-    prev_month_first = str(prev_month_first.date().strftime("%m/%d/%Y %H:%M:%S %p"))
-print(f'Previous month date range: {prev_month_first}-{prev_month_last}')
+    month_first = str(month_first.date().strftime("%m/%d/%Y %H:%M:%S %p"))
 
-BASE_URL = f"https://www.dayforcehcm.com/Api/{os.environ['CERIDIAN_ORG_ID']}/V1/Reports/TIMESHEETREPORTAPI"
+# The Ceridian API can only return up to 100K records per request, and each month of data contains about
+# 120K rows. This code splits the month in half so all the data can be retrrieved
+month_last = (datetime.strptime(month_first, "%m/%d/%Y %H:%M:%S %p")).replace(day=15)
+month_last = str(month_last.strftime("%m/%d/%Y")) + " 11:59:59 PM"
+
+# (DAG should never pull data from the current month)
+print(f'Initial request date range: {month_first}-{month_last}')
+
 auth = HTTPBasicAuth(os.environ['CERIDIAN_USER'], os.environ['CERIDIAN_PW'])
 
 # set filters on API request - data should encompass the entire previous month
-url = BASE_URL + f"?5b93e411-2928-463d-b048-03460269d416={prev_month_first}&" \
-                 f"bb93e411-2928-463d-b048-03460269d416={prev_month_last}"
+url = set_url(month_first, month_last)
 
 # Make use of session to retain authorization header when a URL redirect happens
 s = requests.session()
 more = True
 all_records = []
+
 while more is True:
     # API call to get data
     response = s.get(url, auth=auth)
@@ -57,20 +78,36 @@ while more is True:
         # Retry API request with same parameters but new URL
         response = s.get(redir_url, auth=auth)
     curr_run = datetime.now(tz=pendulum.timezone('UTC')).strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Response at {str(curr_run)}: {str(response.status_code)}")
+    print(f"Response for time range {month_first} through {month_last}: {str(response.status_code)}")
 
     time_data = response.json()['Data']['Rows']
     # continue looping through records until the API has a MoreFlag value of 0
     if response.json()['Paging']['Next']:
         url = response.json()['Paging']['Next']
+    # just because there is no MoreFlag value does not necessarily mean there is no more data
+    # continue looping by moving time window up ~15 days until we catch up to the current month
+    elif month_first != curr_month_first_str:
+        month_first = datetime.strptime(month_last, "%m/%d/%Y %H:%M:%S %p").date() + timedelta(days=1)
+        if '/15/' in month_last:
+            month_last = last_day_of_month(month_last)
+        else:
+            month_last = month_first.replace(day=15)
+            month_last = str(month_last.strftime("%m/%d/%Y")) + " 11:59:59 PM"
+        month_first = str(month_first.strftime("%m/%d/%Y %H:%M:%S %p"))
+        if month_first != curr_month_first_str:
+            url = set_url(month_first, month_last)
+        else:
+            more = False
     else:
         more = False
+
     # append list of API results to growing all_records list (should require several loops)
     all_records += time_data
 
+print(f"Total extract length: {len(all_records)}")
 json_to_gcs(f"{args['out_loc']}", all_records, bucket)
 
-successful_run = [{"since": prev_month_first,
-                   "current_run": prev_month_last,
+successful_run = [{"since": month_first,
+                   "current_run": month_last,
                    "note": "Timekeeping data retrieved between the time points listed above"}]
 json_to_gcs("timekeeping/successful_run_log/log.json", successful_run, bucket)
