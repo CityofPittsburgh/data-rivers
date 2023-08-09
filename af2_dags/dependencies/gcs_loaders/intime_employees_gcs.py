88551d274edc38179780cfa0bd2d3662b47a0002
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-08-09 16:26:42 -0400

    Use New XML Generation Util in InTime Employee GCS Loader

--- af2_dags/dependencies/gcs_loaders/intime_employees_gcs.py
+++ af2_dags/dependencies/gcs_loaders/intime_employees_gcs.py
@@ -1,13 +1,11 @@
 import os
-import time
 import argparse
 import pendulum
-from datetime import datetime, timedelta
-import requests
+from datetime import datetime
 from requests.auth import HTTPBasicAuth
 from google.cloud import storage
 
-from gcs_utils import json_to_gcs, find_last_successful_run, post_xml
+from gcs_utils import json_to_gcs, find_last_successful_run, generate_xml, post_xml
 
 # the date the first employee record was entered
 DEFAULT_RUN_START = "2020-06-11"
@@ -20,33 +18,12 @@ parser.add_argument('--output_arg', dest='out_loc', required=True,
                     help='fully specified location to upload the combined ndjson file')
 args = vars(parser.parse_args())
 
-today = datetime.now(tz = pendulum.timezone("utc")).strftime("%Y-%m-%d")
+today = datetime.now(tz=pendulum.timezone("utc")).strftime("%Y-%m-%d")
 
 BASE_URL = 'https://intime2.intimesoft.com/ise/employee/v3/EmployeeAccess'
 auth = HTTPBasicAuth(os.environ['INTIME_USER'], os.environ['INTIME_PW'])
-
-
-def generate_xml(branch, from_time, to_time):
-    """
-    :param branch: string to identify the department employee information will be pulled from
-    :param from_time: date string in %Y-%m-%d format that identifies the start window for when employee data
-    should start being pulled. This date is either the date of the first-ever entry of data into
-    the InTime system, or the date of the last successful run of this data pipeline
-    :param to_time: date string in %Y-%m-%d format that identifies the end window for when employee data
-    should stop being pulled. Should always be the current date
-    """
-    return F"""
-    <S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v3="http://v3.employeeaccess.rise.intimesoft.com/">
-        <S:Body>
-            <v3:getEmployeeDataList>
-                <branchRef>{branch}</branchRef>
-                <startDate>{from_time}</startDate>
-                <endDate>{to_time}</endDate>
-            </v3:getEmployeeDataList>
-        </S:Body>
-    </S:Envelope>
-    """
-
+soap_url = 'employeeaccess'
+request = 'getEmployeeDataList'
 headers = {'Content-Type': 'application/xml'}
 # these variables allow us to parse the record information from the XML text returned by the API
 start = '<ns2:getEmployeeDataListResponse xmlns:ns2="http://v3.employeeaccess.rise.intimesoft.com/">'
@@ -55,12 +32,14 @@ end = '</ns2:getEmployeeDataListResponse>'
 # find the last successful DAG run (needs to be specified in UTC YYYY-MM-DD HH:MM:SS) if there was no previous good
 # run default to the date of the first entry of InTime data (allows for complete backfill).
 # this is used to initialize the payload below
-run_start_win, first_run = find_last_successful_run(bucket, "successful_run_log/log.json", DEFAULT_RUN_START)
+run_start_win, first_run = find_last_successful_run(bucket, "employees/successful_run_log/log.json", DEFAULT_RUN_START)
 from_time = run_start_win.split(' ')[0]
 
+xml_test = generate_xml(soap_url, request, 'POLICE', from_time, today)
+
 # API call to get data
-response = post_xml(BASE_URL, envelope=generate_xml('POLICE', from_time, today), auth=auth, headers=headers,
-                        res_start=start, res_stop=end)
+response = post_xml(BASE_URL, envelope=generate_xml(soap_url, request, 'POLICE', from_time, today),
+                    auth=auth, headers=headers,  res_start=start, res_stop=end)
 records = response['root']['return']
 
 # verify the API called returned data (if no new records, then type will be NONE)
@@ -70,7 +49,6 @@ if records is not None:
                        "since": run_start_win,
                        "current_run": today,
                        "note": "Data retrieved between the time points listed above"}]
-    json_to_gcs("successful_run_log/log.json", successful_run, bucket)
-
+    json_to_gcs("employees/successful_run_log/log.json", successful_run, bucket)
 
 json_to_gcs(f"{args['out_loc']}", records, bucket)
