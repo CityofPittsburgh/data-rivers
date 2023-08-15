2351585f2e329d7cb275fdfc2e779cce966c6282
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-08-15 16:13:53 -0400

    Expand Query to Catch Employees Hired Within Past Day

--- af2_dags/dependencies/bq_queries/ceridian/employee_admin.py
+++ af2_dags/dependencies/bq_queries/ceridian/employee_admin.py
@@ -4,10 +4,14 @@ import os
 def extract_new_hires():
     return F"""
     CREATE OR REPLACE TABLE `{os.environ['GCLOUD_PROJECT']}.ceridian.daily_new_hires` AS
-    SELECT employee_num, display_name, dept_desc, hire_date, account_modified_date, pay_class, 
-    IF(job_title LIKE '%Unpaid%', 'Unpaid', 'Paid') AS pay_status 
-    FROM `{os.environ['GCLOUD_PROJECT']}.ceridian.all_employees`
-    WHERE status = 'Active'
-    AND PARSE_DATETIME('%Y-%m-%d', hire_date) > PARSE_DATETIME('%Y-%m-%d', account_modified_date)
-    AND DATE_DIFF(CURRENT_DATETIME(), PARSE_DATETIME('%Y-%m-%d', account_modified_date), DAY) <= 1
+        SELECT employee_num, display_name, dept_desc, hire_date, account_modified_date, pay_class, 
+        IF(job_title LIKE '%Unpaid%', 'Unpaid', 'Paid') AS pay_status 
+        FROM `{os.environ['GCLOUD_PROJECT']}.ceridian.all_employees`
+        WHERE status = 'Active' AND
+        PARSE_DATETIME('%Y-%m-%d', hire_date) > PARSE_DATETIME('%Y-%m-%d', account_modified_date) AND
+        (
+            DATE_DIFF(CURRENT_DATETIME(), PARSE_DATETIME('%Y-%m-%d', account_modified_date), DAY) <= 7
+            OR
+            DATE_DIFF(CURRENT_DATETIME(), PARSE_DATETIME('%Y-%m-%d', hire_date), DAY) <= 7
+        )
     """
