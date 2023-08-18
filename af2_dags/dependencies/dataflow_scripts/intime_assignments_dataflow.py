02788cf63ac3925ab51cd2c6e92cf95fb507abdc
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-08-18 08:42:29 -0400

    Initialize InTime Assignment Dataflow Script

---
+++ af2_dags/dependencies/dataflow_scripts/intime_assignments_dataflow.py
@@ -0,0 +1,61 @@
+from __future__ import absolute_import
+
+import logging
+import os
+import json
+
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io.avroio import WriteToAvro
+
+from dataflow_utils import dataflow_utils
+from dataflow_utils.dataflow_utils import JsonCoder, SwapFieldNames, generate_args, ChangeDataTypes, FilterFields, \
+    CrosswalkDeptNames, StripBeforeDelim, ColumnsCamelToSnakeCase
+
+DEFAULT_DATAFLOW_ARGS = [
+    '--save_main_session',
+    f"--project={os.environ['GCLOUD_PROJECT']}",
+    f"--service_account_email={os.environ['SERVICE_ACCT']}",
+    f"--region={os.environ['REGION']}",
+    f"--subnetwork={os.environ['SUBNET']}"
+]
+
+
+def run(argv=None):
+    # assign the name for the job and specify the AVRO upload location (GCS bucket), arg parser object,
+    # and avro schema to validate data with. Return the arg parser values, PipelineOptions, and avro_schemas (dict)
+
+    known_args, pipeline_options, avro_schema = generate_args(
+        job_name='intime-assignments-dataflow',
+        bucket=f"{os.environ['GCS_PREFIX']}_intime",
+        argv=argv,
+        schema_name='intime_assignments',
+        default_arguments=DEFAULT_DATAFLOW_ARGS,
+        limit_workers=[False, None]
+    )
+
+    with beam.Pipeline(options=pipeline_options) as p:
+        date_fields = ['date']
+        field_name_swaps = [('employee_full_name', 'display_name'),
+                            ('customer_name', 'court_assignment')]
+        type_changes = [('', 'str'), ('', 'float')]
+        drop_fields = []
+
+        lines = p | ReadFromText(known_args.input, coder=JsonCoder())
+
+        load = (
+                lines
+                | beam.ParDo(StripBeforeDelim(date_fields, delim='T'))
+                | beam.ParDo(ColumnsCamelToSnakeCase())
+                | beam.ParDo(SwapFieldNames(field_name_swaps))
+                | beam.ParDo(CrosswalkDeptNames('Department_LongName', os.environ['intime_DEPT_FILE']))
+                | beam.ParDo(ChangeDataTypes(type_changes))
+                | beam.ParDo(FilterFields(drop_fields, exclude_target_fields=True))
+                | WriteToAvro(known_args.avro_output, schema=avro_schema, file_name_suffix='.avro',
+                              use_fastavro=True)
+        )
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()
