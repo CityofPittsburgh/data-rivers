2b9b4e09fa3b090b8ddad165711484bd369cf304
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-08-25 13:29:14 -0400

    Limit Query Output to 2 Rank Values

--- af2_dags/dependencies/bq_queries/police_rms/intime_admin.py
+++ af2_dags/dependencies/bq_queries/police_rms/intime_admin.py
@@ -6,7 +6,7 @@ def extract_current_intime_details():
     CREATE OR REPLACE TABLE `{os.environ['GCLOUD_PROJECT']}.intime.pbp_current_assignments` AS
     SELECT * FROM (
         SELECT e.employee_id AS ceridian_id, e.mpoetc_number, e.mpoetc_username, e.ncic_username,
-               e.first_name, e.last_name, e.display_name, e.email, e.hire_date, e.rank, a.permanent_rank, 
+               e.first_name, e.last_name, e.display_name, e.email, e.hire_date, a.permanent_rank, 
                a.activity_name AS current_activity, a.scheduled_start_time, a.scheduled_end_time, 
                e.unit AS permanent_unit, a.unit AS current_unit
         FROM `{os.environ['GCLOUD_PROJECT']}.intime.employee_data` e 
