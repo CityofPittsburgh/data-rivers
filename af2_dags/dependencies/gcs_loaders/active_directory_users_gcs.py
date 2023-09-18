ce22b8e5e54e8f1ca44d3dd33939d4b3a05c9b03
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-09-18 09:54:15 -0400

    Update Query Filters

--- af2_dags/dependencies/gcs_loaders/active_directory_users_gcs.py
+++ af2_dags/dependencies/gcs_loaders/active_directory_users_gcs.py
@@ -6,7 +6,7 @@ conn = ldap.initialize(f"ldaps://{os.environ['WINDOWS_VM_HOST']}:{os.environ['WI
 conn.set_option(ldap.OPT_REFERRALS, 0)
 conn.simple_bind_s(os.environ['WINDOWS_VM_UN'], os.environ['WINDOWS_VM_PW'])
 
-result = conn.search_s(f"dc={os.environ['WINDOWS_VM_DOMAIN']},dc=com", ldap.SCOPE_SUBTREE,
-                       "mail like '*'", ['EmployeeID', 'GivenName', 'Surname', 'Mail', 'Enabled', 'Title',
-                                         'Description', 'SamAccountName', 'LastLogonDate'])
+result = conn.search_s(f"dc={os.environ['WINDOWS_VM_DOMAIN']},dc=com,ou=Get-ADUser", ldap.SCOPE_SUBTREE,
+                       "(mail=*)", ['EmployeeID', 'GivenName', 'Surname', 'Mail', 'Enabled', 'Title',
+                                    'Description', 'SamAccountName', 'LastLogonDate'])
 print(result)
