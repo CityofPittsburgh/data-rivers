7d53c16717f9617559adee2305c695160e216ff6
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-09-20 10:37:02 -0400

    Fix LDAP Connection

--- af2_dags/dependencies/gcs_loaders/active_directory_users_gcs.py
+++ af2_dags/dependencies/gcs_loaders/active_directory_users_gcs.py
@@ -2,10 +2,9 @@ import os
 import ldap
 
 
-conn = ldap.initialize(f"ldaps://{os.environ['WINDOWS_VM_HOST']}.city.pittsburgh.pa.us:{os.environ['WINDOWS_VM_PORT']}",
-                       bytes_mode=False)
-# conn.set_option(ldap.OPT_REFERRALS, 0)
-conn.simple_bind_s(os.environ['WINDOWS_VM_UN'], os.environ['WINDOWS_VM_PW'])
+conn = ldap.initialize("ldap://DC1.city.pittsburgh.pa.us")
+conn.set_option(ldap.OPT_REFERRALS, 0)
+conn.simple_bind_s(rf"{os.environ['WINDOWS_VM_DOMAIN']}\{os.environ['WINDOWS_VM_UN']}", os.environ['WINDOWS_VM_PW'])
 
 result = conn.search_s(f"dc={os.environ['WINDOWS_VM_DOMAIN']},dc=com,ou=Get-ADUser", ldap.SCOPE_SUBTREE,
                        "(mail=*)", ['EmployeeID', 'GivenName', 'Surname', 'Mail', 'Enabled', 'Title',
