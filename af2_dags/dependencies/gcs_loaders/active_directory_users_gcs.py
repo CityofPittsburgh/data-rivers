350ab116972cda8173026e3e66d9e059b1ff210a
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-09-20 09:13:16 -0400

    Use Fully Specified Host URL

--- af2_dags/dependencies/gcs_loaders/active_directory_users_gcs.py
+++ af2_dags/dependencies/gcs_loaders/active_directory_users_gcs.py
@@ -2,8 +2,9 @@ import os
 import ldap
 
 
-conn = ldap.initialize(f"ldaps://{os.environ['WINDOWS_VM_HOST']}:{os.environ['WINDOWS_VM_PORT']}", bytes_mode=False)
-conn.set_option(ldap.OPT_REFERRALS, 0)
+conn = ldap.initialize(f"ldaps://{os.environ['WINDOWS_VM_HOST']}.city.pittsburgh.pa.us:{os.environ['WINDOWS_VM_PORT']}",
+                       bytes_mode=False)
+# conn.set_option(ldap.OPT_REFERRALS, 0)
 conn.simple_bind_s(os.environ['WINDOWS_VM_UN'], os.environ['WINDOWS_VM_PW'])
 
 result = conn.search_s(f"dc={os.environ['WINDOWS_VM_DOMAIN']},dc=com,ou=Get-ADUser", ldap.SCOPE_SUBTREE,
