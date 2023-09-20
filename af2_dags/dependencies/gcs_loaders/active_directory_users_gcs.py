3107502cf1058cc4ff5e73baa7b0f38ca215f95b
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-09-20 15:57:04 -0400

    Upload AD Data to GCS via LDAP Query

--- af2_dags/dependencies/gcs_loaders/active_directory_users_gcs.py
+++ af2_dags/dependencies/gcs_loaders/active_directory_users_gcs.py
@@ -1,12 +1,47 @@
 import os
+import argparse
 import ldap
+from google.cloud import storage
+from gcs_utils import json_to_gcs
 
 
-conn = ldap.initialize("ldap://DC1.city.pittsburgh.pa.us")
-conn.set_option(ldap.OPT_REFERRALS, 0)
+storage_client = storage.Client()
+bucket = f"{os.environ['GCS_PREFIX']}_active_directory"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output_arg', dest='out_loc', required=True,
+                    help='fully specified location to upload the combined ndjson file')
+args = vars(parser.parse_args())
+
+conn = ldap.initialize(f"ldap://{os.environ['LDAP_URI']}")
 conn.simple_bind_s(rf"{os.environ['WINDOWS_VM_DOMAIN']}\{os.environ['WINDOWS_VM_UN']}", os.environ['WINDOWS_VM_PW'])
 
-result = conn.search_s(f"dc={os.environ['WINDOWS_VM_DOMAIN']},dc=com,ou=Get-ADUser", ldap.SCOPE_SUBTREE,
-                       "(mail=*)", ['EmployeeID', 'GivenName', 'Surname', 'Mail', 'Enabled', 'Title',
-                                    'Description', 'SamAccountName', 'LastLogonDate'])
-print(result)
+
+def set_domain_name(dept_name):
+    return f"OU={dept_name},OU=Users,OU=Next,DC={os.environ['LDAP_URI'].split('.')[1]}," \
+           f"DC={os.environ['LDAP_URI'].split('.')[2]},DC={os.environ['LDAP_URI'].split('.')[3]}," \
+           f"DC={os.environ['LDAP_URI'].split('.')[4]}"
+
+
+ldap_filter = '(&(objectClass=user)(mail=*))'
+ldap_attributes = ['employeeID', 'givenName', 'sn', 'mail', 'sAMAccountName',
+                   'title', 'description', 'userAccountControl']
+
+dept_names = ["BBI", "CHR", "CON", "Controller's Office", "COU",
+              "CPRB", "DCP", "DOMI", "EMA", "EMS", "FIN", "Fire",
+              "I&P", "JTPA", "Law", "Mayor's", "PAR", "Pension",
+              "Personnel", "Police", "PS", "Public Works",  "Recipients",
+              "Resources", "Service Accounts", "Training", "Vendors"]
+all_users = []
+for dept in dept_names:
+    ldap_dn = set_domain_name(dept)
+    result = conn.search_s(ldap_dn, ldap.SCOPE_SUBTREE, ldap_filter, ldap_attributes)
+
+    for dn, entry in result:
+        user_dict = {}
+        for attr in ldap_attributes:
+            user_dict[attr] = entry.get(attr, [None])[0].decode('utf-8') if entry.get(attr) else None
+        all_users.append(user_dict)
+
+conn.unbind()
+json_to_gcs(f"{args['out_loc']}", all_users, bucket)
