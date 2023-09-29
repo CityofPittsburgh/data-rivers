0fd3c55a57ac6a5f00c225bbefa0013e4ebabeb4
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-09-29 12:03:11 -0400

    Using Acting Rank for AD Personas if Present

--- af2_dags/dependencies/bq_queries/employee_admin/ad_admin.py
+++ af2_dags/dependencies/bq_queries/employee_admin/ad_admin.py
@@ -5,11 +5,14 @@ def build_ad_personas_table():
     return F"""
     CREATE OR REPLACE TABLE `{os.environ['GCLOUD_PROJECT']}.active_directory.active_directory_personas` AS
     SELECT c.employee_num AS ceridian_id, i.mpoetc_number, i.badge_number, c.first_name, 
-           c.last_name, c.display_name, a.email, a.sam_account_name, c.hire_date, i.rank, 
-           i.unit, c.dept_desc, c.office, c.job_title, c.status, c.ethnicity, c.gender, i.web_rms_dropdown
+           c.last_name, c.display_name, a.email, a.sam_account_name, c.hire_date, 
+           IFNULL(p.current_rank, i.rank) AS rank, 
+           i.unit, c.dept_desc, c.office, c.job_title, c.status, c.ethnicity, c.gender
     FROM `{os.environ['GCLOUD_PROJECT']}.ceridian.all_employees` c
     LEFT OUTER JOIN `{os.environ['GCLOUD_PROJECT']}.intime.employee_data` i
     ON c.employee_num = i.employee_id
+    LEFT OUTER JOIN `{os.environ['GCLOUD_PROJECT']}.intime.pbp_current_assignments` p
+    ON c.employee_num = p.ceridian_id
     LEFT OUTER JOIN `{os.environ['GCLOUD_PROJECT']}.active_directory.ad_users` a
     ON c.employee_num = a.employee_id
     """
