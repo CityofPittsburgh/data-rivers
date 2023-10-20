784f6fa8cd9b794fced137da64fdc290ad4ad276
Jesse Wood <woodjt.mail@gmail.com>
2023-10-20 13:26:14 -0400

    code runs and the basic outline of the entire extraction plus an unnest (explode) operation is included here. this may be phased out for a dataflow operation during development

--- af2_dags/dependencies/gcs_loaders/computronix_solar_panel_permits_gis_wprdc.py
+++ af2_dags/dependencies/gcs_loaders/computronix_solar_panel_permits_gis_wprdc.py
@@ -1,6 +1,9 @@
 import os
 import argparse
 
+import pandas as pd
+
+
 from gcs_utils import json_to_gcs, call_odata_api_error_handling, \
     write_partial_api_request_results_for_inspection
 
@@ -9,43 +12,70 @@ The permits for solar panels are ingested here.
 These data will be used by GIS for mapping a product that fire (etc.) will use. They will also be published to WPRDC
 """
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--output_arg', dest = 'out_loc', required = True,
-#                     help = 'fully specified location to upload the ndjson file')
-# args = vars(parser.parse_args())
+parser = argparse.ArgumentParser()
+parser.add_argument('--output_arg', dest = 'out_loc', required = True,
+                    help = 'fully specified location to upload the ndjson file')
+args = vars(parser.parse_args())
 
 bucket = f"{os.environ['GCS_PREFIX']}_computronix"
 
 # CX ODATA API URL base
 url = 'https://staff.onestoppgh.pittsburghpa.gov/pghprod/odata/odata/'
 
-# PLI permit tables
-base = 'ELECTRICALPERMIT'
-
-# nested expansion tables (expansion from each base table)
-xref_1 = "PERMITXSCOPEXREF"
-nested_table_1 = "EQUIPMENTTYPE"
-xref_2 = "JOBPARCELXREF"
-nested_table_2 = "PARCEL"
-
-# fields to select from each table (nt = nested table; unt = unnested table)
-fds_base = 'JOBID, EXTERNALFILENUM,STATUS, ISSUEDDATE, COMPLETEDDATE, STATUSDESCRIPTION, COMMERCIALORRESIDENTIAL'
-fds_nt2 = 'PARCELNUMBER, ADDRESSOBJECTDEFFORMATTEDADDRESS'
-
-# filter to reduce records to only those for solar panels
-odata_record_filter = F"$filter=DESCRIPTION like %SOLAR%"
-
-
 # build url
-odata_url = F"{url}{base}?$select={fds_base}" \ 
-F"&$expand={xref_1};" \
-F"($expand={nested_table_1}(${odata_record_filter}))" \
-F"&$expand={xref_2};" \
-F"($expand={nested_table_2}($select{fds_nt2}))"
+# map of table relationships, with a left and right branch to a base table:
+# general desc:    |        LEFT                   |         BASE           |       RIGHT              |
+# variable name:   left_nt          left_xref                 bt           right_xref      right_nt
+# real table name: WORKSCOPE << PERMITWORKSCOPEXREF << *ELECTRICALPERMIT* >> JOBPARCELXREF >> PARCEL
+# >> or << are expansions from the base table or from xref table. direction of arrow indicates the direction of the
+# expansion outward from the base table (bt)
+
+# tables
+tables = {
+        "bt"        : 'ELECTRICALPERMIT',
+        "left_xref" : "PERMITWORKSCOPEXREF",
+        "left_nt"   : "WORKSCOPE",
+        "right_xref": "JOBPARCELXREF",
+        "right_nt"  : "PARCEL"
+}
+
+fds = {
+        "bt"        : 'JOBID, STATUSDESCRIPTION, COMMERCIALORRESIDENTIAL, COMPLETEDDATE, ISSUEDATE, EXTERNALFILENUM',
+        "left_xref" : "JOBID",
+        "left_nt"   : "DESCRIPTION",
+        "right_xref": "JOBID",
+        "right_nt"  : 'PARCELNUMBER, ADDRESSABLEOBJEFORMATTEDADDRES'
+}
+
+# form the url
+odata_url_base = F"{url}{tables['bt']}?"
+odata_url_left = F"&$select={fds['bt']}" \
+                 F"&$expand={tables['left_xref']}" \
+                 F"(" \
+                 F"$select={fds['left_xref']},;" \
+                 F"$expand={tables['left_nt']}" \
+                 F"($select={fds['left_nt']})" \
+                 "),"
+
+odata_url_right = F"{tables['right_xref']}" \
+                  F"(" \
+                  F"$select={fds['right_xref']},;" \
+                  F"$expand={tables['right_nt']}" \
+                  F"($select={fds['right_nt']})" \
+                  F")"
+
+odata_url = odata_url_base + odata_url_left + odata_url_right
 
 # call the API
 pipe_name = F"{os.environ['GCLOUD_PROJECT']} computronix pli solar panel permits"
 permits, error_flag = call_odata_api_error_handling(targ_url = odata_url, pipeline = pipe_name, ct_url = None)
+
+# expand the nested PERMITWORKSCOPEXREF column (there can be more than 1 entry here, though no other fields can have
+# more than 1 entry) (this is significantly more concise than a dataflow operation to accomplish this unnesting. this
+# type of transformation could be done in dataflow in the future, and a BEAM embedded dataframe may be implemented in
+# place of this.
+permit_unnest = pd.DataFrame(permits).explode("PERMITWORKSCOPEXREF")
+permit_unnest["work_desc"] = pd.json_normalize(permit_unnest["PERMITWORKSCOPEXREF"])["WORKSCOPE.DESCRIPTION"]
 
 
 # load data into GCS
