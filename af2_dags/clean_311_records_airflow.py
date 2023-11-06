cdf93b0a05ea6e2a5b6430c07aa68925c66699de
jasonfic <43254577+jasonfic@users.noreply.github.com>
2023-11-06 16:03:36 -0500

    Import Queries from External Scripts in Cleanup DAG (#508)
    * Include Pre-Start Employees in New Hire Export
    
    * Use External Gen Queries Script for Cherwell Surveys DAG?
    
    * Use External Gen Queries Script for Qalert Backfill DAG
    
    * Remove Format/Dedup Query After Migration to Gen Queries Script
    
    * Use External Gen Queries Script for Twilio DAG
    
    * Remove Insert Query After Migration to Gen Queries Script
    
    * Add Staging Table Query to General Queries Script
    
    * Add 311 Synchronization Query to General Queries Script
    
    * Import Queries from External Scripts in Cleanup DAG

--- af2_dags/clean_311_records_airflow.py
+++ af2_dags/clean_311_records_airflow.py
@@ -7,8 +7,9 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 from airflow.contrib.operators.bigquery_table_delete_operator import BigQueryTableDeleteOperator
 from dependencies import airflow_utils
-from dependencies.airflow_utils import get_ds_month, get_ds_year, \
-    default_args, build_sync_staging_table_query, build_sync_update_query, build_dedup_old_updates
+from dependencies.airflow_utils import get_ds_month, get_ds_year, default_args
+from dependencies.bq_queries import general_queries as g_q
+from dependencies.bq_queries.qscend import transform_enrich_requests as t_q
 
 # The goal of this mini-DAG is to fix a recurring issue where 311 ticket data differs
 # between their records in all_tickets_current_status and all_linked_requests in BigQuery
@@ -41,16 +42,14 @@ comp_fields = [{'req_types': ['request_type_name', 'origin']},
                {'geos': ['address_type', 'neighborhood_name', 'council_district',
                          'ward', 'police_zone', 'fire_zone', 'dpw_streets',
                          'dpw_enviro', 'dpw_parks']}]
-query_staging_table = build_sync_staging_table_query(dataset, new_table, upd_table,
-                                                     src_table, is_deduped, upd_id_field,
-                                                     join_id_field, field_groups, comp_fields)
 
 # create a temporary table with the most up-to-date request types and
 # geographic data sourced from all_tickets_current_status where the
 # request type, origin, and geo data does not match that found in all_linked_requests
 create_unsynced_table = BigQueryOperator(
     task_id='create_unsynced_table',
-    sql=query_staging_table,
+    sql=g_q.build_sync_staging_table_query(dataset, new_table, upd_table, src_table, is_deduped, upd_id_field,
+                                           join_id_field, field_groups, comp_fields),
     bigquery_conn_id='google_cloud_default',
     use_legacy_sql=False,
     dag=dag
@@ -60,8 +59,7 @@ create_unsynced_table = BigQueryOperator(
 # "BadRequest: 400 UPDATE/MERGE must match at most one source row for each target row"
 # error can occur
 last_upd_field = 'last_action_unix'
-query_dedup_old = build_dedup_old_updates(dataset, upd_table,
-                                          upd_id_field, last_upd_field)
+query_dedup_old = g_q.build_dedup_old_updates(dataset, upd_table, upd_id_field, last_upd_field)
 dedup_old_updates = BigQueryOperator(
     task_id='dedup_old_updates',
     sql=query_dedup_old,
@@ -70,26 +68,9 @@ dedup_old_updates = BigQueryOperator(
     dag=dag
 )
 
-query_sync_update = F"""
-SELECT * FROM `{os.environ['GCLOUD_PROJECT']}.{dataset}.{upd_table}` base
-WHERE base.{upd_id_field} NOT IN 
-    (SELECT DISTINCT {upd_id_field} FROM `{os.environ['GCLOUD_PROJECT']}.{dataset}.{new_table}`)
-UNION ALL
-SELECT m.{upd_id_field}, alr.child_ids, alr.num_requests, alr.parent_closed, alr.status_name, alr.status_code, alr.dept, 
-       m.request_type_name, m.request_type_id, m.origin, alr.pii_comments, alr.anon_comments, alr.pii_private_notes, 
-       alr.create_date_est, alr.create_date_utc, alr.create_date_unix, alr.last_action_est, alr.last_action_utc, 
-       alr.last_action_unix, alr.closed_date_est, alr.closed_date_utc, alr.closed_date_unix, m.pii_street_num, m.street, 
-       m.cross_street, m.street_id, m.cross_street_id, m.city, m.pii_input_address, m.pii_google_formatted_address, 
-       m.anon_google_formatted_address, m.address_type, m.neighborhood_name, m.council_district, m.ward, m.police_zone, 
-       m.fire_zone, m.dpw_streets, m.dpw_enviro, m.dpw_parks, m.input_pii_lat, m.input_pii_long, m.google_pii_lat, 
-       m.google_pii_long, m.input_anon_lat, m.input_anon_long, m.google_anon_lat, m.google_anon_long
-FROM `{os.environ['GCLOUD_PROJECT']}.{dataset}.{new_table}` m, 
-     `{os.environ['GCLOUD_PROJECT']}.{dataset}.{upd_table}` alr 
-WHERE m.{upd_id_field} = alr.{upd_id_field}
-"""
 update_unsynced_tickets = BigQueryOperator(
     task_id='update_unsynced_tickets',
-    sql=query_sync_update,
+    sql=t_q.sync_311_updates(dataset, upd_table, upd_id_field, new_table),
     bigquery_conn_id='google_cloud_default',
     use_legacy_sql=False,
     dag=dag
