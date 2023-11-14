5b32b0e693c580494fc2246a2bbf7227989b8d83
jasonfic <43254577+jasonfic@users.noreply.github.com>
2023-11-14 08:15:40 -0500

    Add Customer Name to Cherwell Data for New Dashboard View (#515)
    * Include Pre-Start Employees in New Hire Export
    
    * Add Customer Name to Cherwell Extract
    
    * Process Customer Names & Parse Null String/Bool Values
    
    * Use Customer Name Field in Cherwell Queries

--- af2_dags/dependencies/dataflow_scripts/cherwell_incidents_dataflow.py
+++ af2_dags/dependencies/dataflow_scripts/cherwell_incidents_dataflow.py
@@ -8,7 +8,8 @@ from apache_beam.io import ReadFromText
 from apache_beam.io.avroio import WriteToAvro
 
 from dataflow_utils import dataflow_utils
-from dataflow_utils.dataflow_utils import JsonCoder, generate_args, ExtractFieldWithComplexity, StandardizeTimes
+from dataflow_utils.dataflow_utils import JsonCoder, generate_args, ExtractFieldWithComplexity, StandardizeTimes, \
+    ChangeDataTypes
 
 DEFAULT_DATAFLOW_ARGS = [
     '--save_main_session',
@@ -18,7 +19,7 @@ DEFAULT_DATAFLOW_ARGS = [
     f"--subnetwork={os.environ['SUBNET']}"
 ]
 
-FIELD_COUNT = 28
+FIELD_COUNT = 29
 
 
 def run(argv=None):
@@ -41,7 +42,7 @@ def run(argv=None):
                            'priority', 'last_modified_date', 'closed_date', 'assigned_team', 'assigned_to',
                            'assigned_to_manager', 'incident_type', 'respond_by_deadline', 'resolve_by_deadline',
                            'call_source', 'incident_reopened', 'responded_date', 'resolved_date', 'number_of_touches',
-                           'number_of_escalations', 'requester_department', 'requester', 'on_behalf_of',
+                           'number_of_escalations', 'requester_department', 'requester', 'on_behalf_of', 'customer',
                            'initial_assigned_team', 'comments', 'close_description']
         add_nested_fields = [''] * FIELD_COUNT
         search_fields = [{'name': 'IncidentID'}, {'name': 'CreatedDateTime'}, {'name': 'Status'}, {'name': 'Service'},
@@ -52,13 +53,17 @@ def run(argv=None):
                          {'name': 'Stat_IncidentReopened'}, {'name': 'Stat_DateTimeResponded'},
                          {'name': 'Stat_DateTimeResolved'}, {'name': 'Stat_NumberOfTouches'},
                          {'name': 'Stat_NumberOfEscalations'}, {'name': 'RequesterDepartment'},
-                         {'name': 'Requester'}, {'name': 'OnBehalfOf'}, {'name': 'InitialAssignedTeam'},
-                         {'name': 'Comments'}, {'name': 'CloseDescription'}]
+                         {'name': 'Requester'}, {'name': 'OnBehalfOf'}, {'name': 'CustomerDisplayName'},
+                         {'name': 'InitialAssignedTeam'}, {'name': 'Comments'}, {'name': 'CloseDescription'}]
         add_search_vals = [''] * FIELD_COUNT
         times = [('created_date', 'US/Eastern'), ('last_modified_date', 'US/Eastern'),
                  ('closed_date', 'US/Eastern'), ('responded_date', 'US/Eastern'),
                  ('resolved_date', 'US/Eastern'), ('respond_by_deadline', 'US/Eastern'),
                  ('resolve_by_deadline', 'US/Eastern')]
+        type_changes = [('assigned_team', 'nullstr'), ('assigned_to', 'nullstr'), ('assigned_to_manager', 'nullstr'),
+                        ('incident_type', 'nullstr'), ('incident_reopened', 'bool'), ('requester_department', 'nullstr'),
+                        ('requester', 'nullstr'), ('on_behalf_of', 'bool'), ('customer', 'nullstr'),
+                        ('comments', 'nullstr'), ('close_description', 'nullstr'), ('initial_assigned_team', 'nullstr')]
 
         lines = p | ReadFromText(known_args.input, coder=JsonCoder())
 
@@ -67,6 +72,7 @@ def run(argv=None):
                 | beam.ParDo(ExtractFieldWithComplexity(source_fields, nested_fields, new_field_names,
                                                         add_nested_fields, search_fields, add_search_vals))
                 | beam.ParDo(StandardizeTimes(times, "%m/%d/%Y %I:%M:%S %p"))
+                | beam.ParDo(ChangeDataTypes(type_changes))
                 | WriteToAvro(known_args.avro_output, schema=avro_schema, file_name_suffix='.avro',
                               use_fastavro=True)
         )
