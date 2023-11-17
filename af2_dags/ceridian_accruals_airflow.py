1443da67e3e4616e7af940b73dd474f982db08ca
Jason Ficorilli <ip.analytics@pittsburghpa.gov>
2023-11-17 16:01:38 -0500

    Fix Script Name Typos, Execute Queries in Parallel

--- af2_dags/ceridian_accruals_airflow.py
+++ af2_dags/ceridian_accruals_airflow.py
@@ -5,17 +5,19 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 
 from dependencies import airflow_utils
 from dependencies.airflow_utils import get_ds_year, get_ds_month, get_ds_day, default_args
+import dependencies.bq_queries.employee_admin.ceridian_admin as q
 
-# The goal of this DAG is to extract Time Bank accruals from all officers present in the Ceridan system for comparison
+# The goal of this DAG is to extract Time Bank accruals from all officers present in the ceridian system for comparison
 # with the time balances found in InTime. The Ceridian figures should be written to InTime in cases where they differ,
 # as Dayforce serves as the system of record for accrual balances.
 
 dag = DAG(
-    'ceridan_accruals',
+    'ceridian_accruals',
     default_args=default_args,
     schedule_interval='00 10 * * 1',
     start_date=datetime(2023, 11, 17),
@@ -24,7 +26,7 @@ dag = DAG(
     catchup=False
 )
 
-bucket = f"gs://{os.environ['GCS_PREFIX']}_ceridan"
+bucket = f"gs://{os.environ['GCS_PREFIX']}_ceridian"
 dataset = 'accruals'
 path = "{{ ds|get_ds_year }}/{{ ds|get_ds_month }}/{{ run_id }}"
 json_loc = f"{path}_report.json"
@@ -34,14 +36,14 @@ output_name = f"{dataset}_report"
 # Run gcs_loader
 accruals_gcs_loader = BashOperator(
     task_id='accruals_gcs_loader',
-    bash_command=F"python {os.environ['DAGS_PATH']}/dependencies/gcs_loaders/ceridan_accruals_gcs.py "
+    bash_command=F"python {os.environ['DAGS_PATH']}/dependencies/gcs_loaders/ceridian_accruals_gcs.py "
                  F"--output_arg {dataset}/{json_loc}",
     dag=dag
 )
 
-exec_df = f"python {os.environ['DATAFLOW_SCRIPT_PATH']}/ceridan_accruals_dataflow.py"
+exec_df = f"python {os.environ['DATAFLOW_SCRIPT_PATH']}/ceridian_accruals_dataflow.py"
 accruals_dataflow = BashOperator(
-    task_id='ceridan_accruals_dataflow',
+    task_id='ceridian_accruals_dataflow',
     bash_command=f"{exec_df} --input {bucket}/{dataset}/{json_loc} --avro_output {hot_bucket}/{output_name}",
     dag=dag
 )
@@ -49,7 +51,7 @@ accruals_dataflow = BashOperator(
 # Load AVRO data produced by dataflow_script into BQ temp table
 accruals_gcs_to_bq = GoogleCloudStorageToBigQueryOperator(
     task_id='accruals_gcs_to_bq',
-    destination_project_dataset_table=f"{os.environ['GCLOUD_PROJECT']}:ceridan.time_{output_name}",
+    destination_project_dataset_table=f"{os.environ['GCLOUD_PROJECT']}:ceridian.time_{output_name}",
     bucket=f"{os.environ['GCS_PREFIX']}_hot_metal",
     source_objects=[f"{output_name}*.avro"],
     write_disposition='WRITE_TRUNCATE',
@@ -57,6 +59,22 @@ accruals_gcs_to_bq = GoogleCloudStorageToBigQueryOperator(
     source_format='AVRO',
     autodetect=True,
     bigquery_conn_id='google_cloud_default',
+    dag=dag
+)
+
+compare_timebank_balances = BigQueryOperator(
+    task_id='compare_timebank_balances',
+    sql=q.compare_timebank_balances(),
+    bigquery_conn_id='google_cloud_default',
+    use_legacy_sql=False,
+    dag=dag
+)
+
+update_accruals_table = BigQueryOperator(
+    task_id='update_accruals_table',
+    sql=q.update_time_accruals_table(),
+    bigquery_conn_id='google_cloud_default',
+    use_legacy_sql=False,
     dag=dag
 )
 
@@ -68,9 +86,11 @@ delete_accruals_avro = BashOperator(
 
 accruals_beam_cleanup = BashOperator(
     task_id='accruals_beam_cleanup',
-    bash_command=airflow_utils.beam_cleanup_statement(f"{os.environ['GCS_PREFIX']}_ceridan"),
+    bash_command=airflow_utils.beam_cleanup_statement(f"{os.environ['GCS_PREFIX']}_ceridian"),
     dag=dag
 )
 
-accruals_gcs_loader >> accruals_dataflow >> accruals_gcs_to_bq >> delete_accruals_avro >> \
+accruals_gcs_loader >> accruals_dataflow >> accruals_gcs_to_bq >> compare_timebank_balances >> delete_accruals_avro >> \
+    accruals_beam_cleanup
+accruals_gcs_loader >> accruals_dataflow >> accruals_gcs_to_bq >> update_accruals_table >> delete_accruals_avro >> \
     accruals_beam_cleanup
