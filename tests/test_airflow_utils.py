813110a4087dec36ad772a41658179810f14cccb
jasonfic <43254577+jasonfic@users.noreply.github.com>
2023-11-21 14:51:52 -0500

    Ensure Email  Utility Functions Properly (#543)
    * Include Pre-Start Employees in New Hire Export
    
    * Add GCS Script for Ceridian Police Accruals Report
    
    * Deploy Dataflow Script for Ceridian Accruals
    
    * Add Retrieval Date to Ceridian Accrual Data
    
    * Use Standardized Schema for Ceridian Accrual Data
    
    * Use Extend Instead of Addition to Grow Record Lis
    
    * Use New Dataflow Util for General Crosswalks
    
    * Initialize Time Accruals Airflow Script
    
    * Add Query to Get Differences Between Ceridian/InTime Time Balances
    
    * Add Query to Append Accruals Report to Historic Archive
    
    * Use Corrected Table Names & Create Comparison Table
    
    * Fix Script Name Typos, Execute Queries in Parallel
    
    * Export Comparison Table to CSV for Police Timekeeper Reference
    
    * Add Rounding to Comparison Query to Filter Out False Positives
    
    * Add Airflow Util to Send a File from GCS Over Email
    
    * Add Logic to Prevent Emailing Empty Files
    
    * Ensure Email Utility Functions Properly

--- tests/test_airflow_utils.py
+++ tests/test_airflow_utils.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
 # patches unittest.TestCase to be python3 compatible
+import os
 import datetime
 import re
 import unittest
@@ -93,6 +94,15 @@ class TestAirflowUtils(unittest.TestCase):
             output = af2_airflow_utils.find_backfill_date(val['bucket'], val['subfolder'])
             self.assertEqual(output, str(expected[index].date()))
             index += 1
+
+    def test_gcs_to_email(self):
+        bucket = f"{os.environ['GCS_PREFIX']}_pbp"
+        file_path = "data_sharing/time_balance_mismatches.csv"
+        recipient = os.environ['EMAIL']
+        subject = "ALERT: Time Bank Mismatches Detected"
+        message = "Attached is an extract of all time bank balances that differ between the Ceridian and InTime source systems."
+        attachment_name = "time_balance_mismatches"
+        af2_airflow_utils.gcs_to_email(bucket, file_path, recipient, subject, message, attachment_name)
 
     def test_within_city_limits(self):
         datum = [{'address': '414 Grant St, Pittsburgh, PA 15219', 'lat': 40.4382355, 'long': -79.9966742, 'address_type': 'Precise'},
